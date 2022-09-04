2090f2b500e52de070b0f5214e2d308807dc48df
Darosss <Daroing12@gmail.com>
2022-09-04 18:03:59 +0200

    Added node-twitch for api twitch and added some timers, refactored classes

--- configHead.js
+++ configHead.js
@@ -3,7 +3,6 @@ var config = {};
 config.options = {};
 config.options.channels = {};
 config.options.username = "booksarefunsometimes";
-config.options.oauth = "oauth";
 
 config.options.channelsSeparately = true; //write separately folder for each of canal
 
@@ -27,24 +26,56 @@ config.options.bots = [
 config.options.words = ["http", "@", "www.", "sub", "follow"]; // exclude words
 
 config.options.timers = {};
-config.options.timers.words = {};
-// To add a trigger add new object to config.options.timers.words..fe. u want to add new trigger named shit, on triger: shit and send: you are shit:
+
+//CONFIG OPTIONS TIMERS:
+//FOR FOLLOWS, SUBS, CHEERS
+//
+// [Text before data,
+//  data,
+//  Text before name,
+//  name,
+//  End text
+//]
+config.options.timers.follower = {};
+
+config.options.timers.follower.phrases = [
+  [
+    "Long time ago when mosquitos eten tomatoen..about : ",
+    " was a stupid person (",
+    " ) who liked noone, but when he followed this channel he lived more than u can think about it",
+  ],
+  ["TAK ", " koko ", " KO"],
+];
+
+config.options.timers.follower.enabled = true;
+config.options.timers.follower.delay = 10;
+config.options.timers.follower.msg = 0;
+config.options.timers.donation = {};
+config.options.timers.donation.phrases = [
+  "DONACJE TO DONAJCE DAWAJ I DONT CARE",
+  "DONATION ARE ONLY FROM SONG REQUEST",
+];
+config.options.timers.donation.enabled = true;
+config.options.timers.donation.delay = 4;
+config.options.timers.donation.msg = 2;
+// To add a trigger add new object to config.options.trigers..fe. u want to add new trigger named shit, on triger: shit and send: you are shit:
 // You create as below:
 //config.options.timers.words.shit = ["shit", "you are shit"];
 
 //									 [trigger, message, isOn?, delay in seconds];
-config.options.timers.words.zabol = [
+config.options.triggers = {};
+config.options.triggers.kappa = [
   "kappa",
   "Kappa k Kappa a Kappa p Kappa p Kappa a Kappa",
   true,
   30,
 ];
-config.options.timers.words.lul = [
+config.options.triggers.lul = [
   "lul",
   "LUL k LUL a LUL p LUL p LUL a LUL",
   true,
   11,
 ];
 
-config.options.timers.zabolGame = ["szukam :)", 5000];
+// config.options.timers.zabolGame = ["szukam :)", 5000];
 module.exports = config;
--- headBot.js
+++ headBot.js
@@ -1,30 +1,32 @@
 const tmi = require("tmi.js");
-const functionsHead = require("./functionsHead.js");
+const TwitchApi = require("node-twitch").default;
+require("dotenv").config();
+const config_file = require("./configHead.js");
 
-const config = require("./configHead.js");
-const userData = require("./auth.js");
-const username = userData.username;
-const userAuth = userData.auth;
-
-class BotChleb {
+const twApi = new TwitchApi({
+  client_id: process.env.client_id,
+  client_secret: process.env.client_secret,
+});
+// TODO CHANGE ALL FOR LOOP FOREACH WHEN CAN its More readable for me then
+class BotTimer {
   constructor(config) {
     this.config = config;
-    this.triggers = this.config.timers.words;
-    this.chanels = this.config.channels;
-    this.logMsgDir = [];
-    this.logMsg = [];
-    this.usersDir = [];
-    this.usersJson = [];
-    this.#createNonExistingJsons();
+    this.triggers = this.config.triggers;
+    this.timers = this.config.timers;
+    this.msgs_count = [];
+    Object.keys(this.timers).forEach((element) => {
+      this.msgs_count[element] = 0;
+    });
+    // SET EVERY MSG COUNT TO 0 WHEN STARTING BOT
+    console.log(this.msgs_count);
   }
-
   checkTrigger(client, channel, msg) {
     let trig_key = Object.keys(this.triggers);
     for (let i = 0; i < trig_key.length; i++) {
       let trigger = this.triggers[trig_key[i]];
       let trigger_word = trigger[0];
       if (trigger[2]) {
-        console.log(trigger_word);
+        // console.log(trigger_word);
         if (msg.toLowerCase().includes(trigger_word)) {
           this.triggers[trig_key[i]][2] = false;
           // its must be like this, because it should change this.triggers
@@ -40,6 +42,154 @@ class BotChleb {
       }
     }
   }
+  // TODO need to do here sth like config[...]...phrases.format
+  // fe.
+  // format.datediff = true \\ false
+  // means: false == will be normal date fe. 2022/4/9
+  // true == 50 days difference betwen today and this date
+  returnFollowMsg(date = "", name = "") {
+    var random = Math.floor(
+      Math.random() * this.timers.follower.phrases.length
+    );
+    var dat;
+    if (date.length > 0) {
+      var follow_at = date.split("T")[0].split("-");
+      dat = new Date(follow_at[0], follow_at[1], follow_at[2]);
+    }
+    const diffTime = Math.abs(new Date() - dat);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    var msg =
+      this.timers.follower.phrases[random][0] +
+      diffDays +
+      this.timers.follower.phrases[random][1] +
+      name +
+      this.timers.follower.phrases[random][2];
+    return msg;
+  }
+  returnNormalMsg(from) {
+    var random = Math.floor(Math.random() * from.phrases.length);
+    return from.phrases[random];
+  }
+  async getUserId(loginName) {
+    const users = await twApi.getUsers(loginName);
+    const user = users.data[0];
+    const userId = user.id;
+    return userId;
+  }
+  async getRandomFollower(channel) {
+    return await twApi
+      .getFollows({
+        to_id: (await this.getUserId(channel)).toString(),
+      })
+      .then((result) => {
+        var random = Math.floor(Math.random() * result.data.length);
+        return result.data[random];
+      });
+  }
+  async isFollowing(broadcaster, chatter) {
+    return await twApi
+      .getFollows({
+        to_id: (await this.getUserId(broadcaster)).toString(),
+        from_id: await (await this.getUserId(chatter)).toString(),
+      })
+      .then((result) => {
+        if (result.total <= 0) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+  }
+  async checkNormalTimer(client, channel, chatter) {
+    // loop throu config.options.timers...
+    Object.keys(this.timers).forEach(async (element) => {
+      var that = this;
+      var incrMsg = 1;
+      switch (element) {
+        case "follower":
+          await this.isFollowing(channel, chatter).then((result) => {
+            if (result) {
+              console.log("Not follow msg value 3");
+              incrMsg = 3;
+            } else {
+              console.log("Follow msg value 1");
+            }
+          });
+          break;
+        default:
+          console.log("Zliczam normalnego timersa");
+      }
+      if (
+        this.timers[element].enabled &&
+        this.msgs_count[element] >= this.timers[element].msg
+      ) {
+        if (element == "follower") {
+          await this.getRandomFollower(channel).then((result) => {
+            //client.say(channel, this.returnFollowMsg(result.followed_at, result.from_name));
+            console.log(
+              this.returnFollowMsg(result.followed_at, result.from_name)
+            );
+          });
+        } // TODO other else is for subs/ cheers and donation if can
+        else {
+          console.log(this.returnNormalMsg(this.timers[element]));
+        }
+        this.timers[element].enabled = false; // set timer for false
+        this.msgs_count[element] = 0; // reset msgs count to 0, because msg is sent
+
+        setTimeout(function () {
+          that.timers[element].enabled = true; // set time true after period of time(delay)
+          console.log(element, "is up again"); // debug info
+        }, that.timers[element].delay * 1000); // set timeout(f(), delay timer);
+      }
+      this.msgs_count[element] += incrMsg;
+    });
+    console.log(this.msgs_count);
+
+    // --------------------------THATS DEBUG -------------------------------
+    // else {
+    //   if (
+    //     !this.timers[element].enabled &&
+    //     this.msgs_count[element] >= this.timers[element].msg
+    //   ) {
+    //     console.log(
+    //       element,
+    //       "is false, but messages are fine: counted:",
+    //       this.msgs_count[element],
+    //       "min:",
+    //       this.timers[element].msg
+    //     );
+    //   } else if (
+    //     this.timers[element].enabled &&
+    //     !this.msgs_count[element] >= this.timers[element].msg
+    //   ) {
+    //     console.log(
+    //       element,
+    //       "is enabled, but messages aren't high enough counted:",
+    //       this.msgs_count[element],
+    //       "min:",
+    //       this.timers[element].msg
+    //     );
+    //   } else {
+    //     console.log("Nothing is true so do nothing");
+    //   }
+    // }
+    // --------------------------THATS DEBUG -------------------------------
+  }
+  //checkTimerApi for taking data from json api fe. data,
+  //random name follower etc(is following)
+}
+
+class BotLog {
+  constructor(config) {
+    this.config = config;
+    this.chanels = this.config.channels;
+    this.logMsgDir = [];
+    this.logMsg = [];
+    this.usersDir = [];
+    this.usersJson = [];
+    this.#createNonExistingJsons();
+  }
   #createNonExistingJsons() {
     if (this.config.channelsSeparately) {
       //TODO create else condition where == false
@@ -49,7 +199,7 @@ class BotChleb {
       today = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
       var dir = [];
       for (let i = 0; i < this.chanels.length; i++) {
-        dir[i] = `./${config.options.folderName}/${this.chanels[i]}`;
+        dir[i] = `./${this.config.folderName}/${this.chanels[i]}`;
         this.#createUsersJson(dir[i], this.chanels[i], "users");
         this.#createSentencesJson(dir[i], this.chanels[i], today, "sentences");
       }
@@ -66,7 +216,6 @@ class BotChleb {
 
     this.logMsg[channel] = require(this.logMsgDir[channel]);
   }
-
   #createNewJsonFile(folder, userJson) {
     var fs = require("fs");
     if (!fs.existsSync(folder)) {
@@ -132,7 +281,7 @@ class BotChleb {
       this.usersJson[channelName].push({
         name: chatter,
         messages: 1,
-        lastSeen: lastSeenDate,
+        lastSeen: this.#formatDateYMDHMS,
       });
     }
     this.#rewriteJson(this.usersDir[channelName], this.usersJson[channelName]);
@@ -148,8 +297,10 @@ class BotChleb {
     this.#rewriteJson(this.logMsgDir[channelName], this.logMsg[channelName]);
   }
 }
-const botChleb = new BotChleb(config.options);
+const botLog = new BotLog(config_file.options);
+const botTimer = new BotTimer(config_file.options);
 var intervalOnce = true;
+
 const client = new tmi.Client({
   options: { debug: true },
   connection: {
@@ -157,13 +308,12 @@ const client = new tmi.Client({
     reconnect: true,
   },
   identity: {
-    password: userAuth,
-    username: username,
+    password: process.env.password,
+    username: process.env.username,
   },
-  channels: config.options.channels,
+  channels: config_file.options.channels,
 });
 client.connect();
-
 client.on("message", (channel, tags, message, self) => {
   // if(intervalOnce){
   // intervalOnce = false;
@@ -171,11 +321,13 @@ client.on("message", (channel, tags, message, self) => {
   // client.say(channel, config.options.timers.zabolGame[0]);
   // },config.options.timers.zabolGame[1]);
   // }
-  botChleb.countMessages(channel, tags.username);
-  botChleb.logMessages(channel, tags.username, message);
 
-  if (tags["display-name"] == config.options.username) return;
+  // botLog.countMessages(channel, tags.username);
+  // botLog.logMessages(channel, tags.username, message);
+
+  if (tags["display-name"] == config_file.options.username) return;
+  botTimer.checkNormalTimer(client, channel.slice(1), "darkohoiik"); //tags["display-name"]
   // if sender message == bot = return;
-  botChleb.checkTrigger(client, channel, message);
+  botTimer.checkTrigger(client, channel, message);
   // check message for trigger from config
 });
