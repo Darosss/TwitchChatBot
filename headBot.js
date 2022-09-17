d6cad793bbb88b6550246cd1bdc75be9ae20de80
Darosss <Daroing12@gmail.com>
2022-09-17 13:51:02 +0200

    Corrected function to work as intended

--- headBot.js
+++ headBot.js
@@ -23,23 +23,13 @@ const botTimerObj = new BotTimer(client, bot_commands);
 client.connect();
 
 client.on("connected", () => {
-  console.log(clc.notice("CONNECTED"));
-  client.on("join", (channel, username, self) => {
-    if (!self) return;
-    console.log(
-      clc.notice("BOT JOINED"),
-      username,
-      clc.info("- It set the intervals")
-    );
-
+  console.log(clc.notice("CONNECTED", clc.info("- I set the intervals now")));
+  config.channels.forEach((channel) => {
     botTimerObj.initOnJoinToChannel(channel.slice(1));
   });
 });
 client.on("disconnected", (reason) => {
-  if (timer_interval) {
-    console.log(clc.error("DISCONNECTED"), clc.notice("-clearing interval"));
-    clearInterval(timer_interval);
-  }
+  console.log(clc.error("DISCONNECTED"), clc.notice("-clearing interval"));
 });
 
 function logMsg(username, msg) {
@@ -50,7 +40,6 @@ function logMsg(username, msg) {
 
 client.on("message", (channel, tags, message, self) => {
   logMsg(tags.username, message);
-
   if (self) return; //echoed msg from bot
   botLogObj.countMessages(channel.slice(1), tags.username);
   botLogObj.logMessages(channel.slice(1), tags.username, message);
