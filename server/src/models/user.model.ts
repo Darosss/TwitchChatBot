add934f4297813e22080aa881f1f56de2ffd01b7
Darosss <Daroing12@gmail.com>
2023-01-16 13:07:07 +0100

    fix: add lastSeen and messageCount to user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -5,6 +5,8 @@ const UserSchema: Schema<IUserDocument> = new Schema({
   username: { type: String, required: true },
   points: { type: Number, required: true, default: 0 },
   createdAt: { type: Date, required: true, default: new Date() },
+  lastSeen: { type: Date, required: true, default: new Date() },
+  messageCount: { type: Number, required: true, default: 0 },
 });
 
 export const User: Model<IUserDocument> = model("User", UserSchema);
