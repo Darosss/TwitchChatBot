95416cd9f631d80eecddf66e67091191e117820c
Darosss <Daroing12@gmail.com>
2023-01-16 12:56:05 +0100

    fix: add points to user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -3,6 +3,7 @@ import { IUserDocument } from "./types";
 
 const UserSchema: Schema<IUserDocument> = new Schema({
   username: { type: String, required: true },
+  points: { type: Number, required: true, default: 0 },
   createdAt: { type: Date, required: true, default: new Date() },
 });
 
