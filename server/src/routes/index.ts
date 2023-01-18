97ba3f7063a7d47ddec0c408e1c8108081f2296c
Darosss <Daroing12@gmail.com>
2023-01-18 13:38:52 +0100

    fix: add users route to app

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import overlayRouter from "./overlay.route";
 import messagesRouter from "./messages.route";
+import usersRouter from "./users.route";
 
 const initRoutes = (app: any) => {
   app.use("/", overlayRouter);
+  app.use("/users", usersRouter);
   app.use("/messages", messagesRouter);
 };
 
