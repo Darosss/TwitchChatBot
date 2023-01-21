e5ef0117e83e391b884f3b5f35108df723d1d074
Darosss <Daroing12@gmail.com>
2023-01-21 21:33:49 +0100

    refactor: move out logic for redemption outside condition

--- frontend/src/components/Overlay/Overlay.tsx
+++ frontend/src/components/Overlay/Overlay.tsx
@@ -9,27 +9,31 @@ export default function Overlay() {
   const [redemptionInfo, setRedemptionInfo] = useState("");
   const [redemptionImg, setRedemptionImg] = useState("");
   useEffect(() => {
-    socket?.on("playRedemptionSound", (data) => {
+    socket?.on("onRedemption", (data) => {
       const { rewardTitle, userDisplayName, rewardImage } = data;
-      if (rewardTitle.includes("Alert sound")) {
-        setRedemptionImg(rewardImage?.url_4x);
-        setRedemptionInfo(`${userDisplayName} has redeemed - ${rewardTitle}`);
-        overlayRef.current?.classList.remove("overlay-hidden");
 
-        const audiotest = new Audio(
+      let redemptionAudio: HTMLAudioElement;
+
+      setRedemptionImg(rewardImage?.url_4x);
+      setRedemptionInfo(`${userDisplayName} has redeemed - ${rewardTitle}`);
+      overlayRef.current?.classList.remove("overlay-hidden");
+
+      setTimeout(() => {
+        redemptionAudio?.pause();
+        overlayRef.current?.classList.add("overlay-hidden");
+      }, Number(process.env.REACT_APP_REDEMPTION_ALERT_MAX_TIME!) * 1000);
+
+      if (rewardTitle.includes(process.env.REACT_APP_PREFIX_ALERT_SOUND!)) {
+        const redemptionAudio = new Audio(
           `/alert-sounds/${rewardTitle.split(":")[1].trim()}.mp3`
         );
-        audiotest.volume = 0.03;
-        audiotest.play();
-
-        audiotest.addEventListener("ended", function () {
-          overlayRef.current?.classList.add("overlay-hidden");
-        });
+        redemptionAudio.volume = 0.03;
+        redemptionAudio.play();
       }
     });
 
     return () => {
-      socket.off("playRedemptionSound");
+      socket.off("onRedemption");
     };
   }, [socket]);
 
