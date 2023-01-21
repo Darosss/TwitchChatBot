b5770fc2d2009581fbe1fa18ea25d3ddae0e9dd2
Darosss <Daroing12@gmail.com>
2023-01-21 14:02:23 +0100

    feat: add alert and sounds for redemptions

--- frontend/src/components/Overlay/Overlay.tsx
+++ frontend/src/components/Overlay/Overlay.tsx
@@ -1,9 +1,44 @@
 import "./style.css";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { SocketContext } from "../../Context/SocketContext";
 
 export default function Overlay() {
   const socket = useContext(SocketContext);
+  const overlayRef = useRef<HTMLDivElement>(null);
 
-  return <>"Overlay"</>;
+  const [redemptionInfo, setRedemptionInfo] = useState("");
+  const [redemptionImg, setRedemptionImg] = useState("");
+  useEffect(() => {
+    socket?.on("playRedemptionSound", (data) => {
+      const { rewardTitle, userDisplayName, rewardImage } = data;
+      if (rewardTitle.includes("Alert sound")) {
+        setRedemptionImg(rewardImage?.url_4x);
+        setRedemptionInfo(`${userDisplayName} has redeemed - ${rewardTitle}`);
+        overlayRef.current?.classList.remove("overlay-hidden");
+
+        const audiotest = new Audio(
+          `/alert-sounds/${rewardTitle.split(":")[1].trim()}.mp3`
+        );
+        audiotest.volume = 0.03;
+        audiotest.play();
+
+        audiotest.addEventListener("ended", function () {
+          overlayRef.current?.classList.add("overlay-hidden");
+        });
+      }
+    });
+
+    return () => {
+      socket.off("playRedemptionSound");
+    };
+  }, [socket]);
+
+  return (
+    <>
+      <div ref={overlayRef} id="overlay-redemption" className="overlay-hidden">
+        {redemptionInfo}
+        {redemptionImg ? <img alt="no" src={redemptionImg} /> : null}
+      </div>
+    </>
+  );
 }
