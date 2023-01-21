1bac0b31c3b5c5184886301cf38313746d500875
Darosss <Daroing12@gmail.com>
2023-01-21 13:59:48 +0100

    fix: add playRedemptionSound event
    add ISoundData interface

--- libs/types/index.d.ts
+++ libs/types/index.d.ts
@@ -2,6 +2,7 @@ interface ServerToClientEvents {
   noArg: () => void;
   withAck: (d: string, callback: (e: number) => void) => void;
   messageServer: (date: Date, username: string, message: string) => void;
+  playRedemptionSound: (data: ISoundData) => void;
 }
 
 interface ClientToServerEvents {
@@ -10,6 +11,17 @@ interface ClientToServerEvents {
 
 interface InterServerEvents {
   ping: () => void;
+}
+
+interface ISoundData {
+  rewardId: string;
+  userId: string;
+  userDisplayName: string;
+  redemptionDate: Date;
+  rewardTitle: string;
+  rewardCost: number;
+  rewardImage: { url_1x: string; url_2x: string; url_4x: string };
+  message: string;
 }
 
 interface SocketData {
