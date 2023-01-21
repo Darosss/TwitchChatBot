9125f905bb0825860b1925bc52e658e3aa0c22b0
Darosss <Daroing12@gmail.com>
2023-01-21 13:58:22 +0100

    refactor: move logic from server to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,32 @@
 import express from "express";
 import cors from "cors";
 import initRoutes from "./routes";
+import localSocket from "./local-socket";
+import { NextFunction, Request, Response } from "express";
+
+import http from "http";
 
 const expressApp = () => {
   const app = express();
+  const server = http.createServer(app);
+
   app.use(express.json());
+
   app.set("json spaces", 2);
   app.use(cors({ origin: "*", methods: ["POST", "GET"] }));
+  app.use(express.urlencoded({ extended: true }));
+
+  const localSocketIO = localSocket(server);
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    req.io = localSocketIO;
+
+    return next();
+  });
 
   initRoutes(app);
 
-  return app;
+  return { server, localSocketIO };
 };
 
 export default expressApp;
