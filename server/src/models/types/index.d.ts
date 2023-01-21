b1eb9c9926cbb9dc28996b98ab6e61b69c20c3bb
Darosss <Daroing12@gmail.com>
2023-01-21 21:34:37 +0100

    feat: add redemption interface

--- server/src/models/types/index.d.ts
+++ server/src/models/types/index.d.ts
@@ -13,9 +13,24 @@ export type IUserDocument = IUser & Document;
 
 export interface IMessage {
   _id: string | ObjectId;
-  message: String;
+  message: string;
   date: Date;
   owner: Types.ObjectId | string | IUser;
 }
 
 export type IMessageDocument = IMessage & Document;
+
+export interface IRedemption {
+  _id: string | ObjectId;
+  rewardId: string;
+  userId: string;
+  userName: string;
+  userDisplayName: string;
+  redemptionDate: Date;
+  rewardTitle: string;
+  rewardCost: number;
+  rewardImage: { url_1x: string; url_2x: string; url_4x: string };
+  message: string;
+}
+
+export type IRedemptionDocument = IRedeption & Document;
