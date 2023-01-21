9a010855ab63df8c83587e340b4841b8f7b0104d
Darosss <Daroing12@gmail.com>
2023-01-21 13:57:29 +0100

    feat: add new overlay routers

--- server/src/routes/overlay.route.ts
+++ server/src/routes/overlay.route.ts
@@ -1,8 +1,12 @@
 import Express, { Router } from "express";
-import overlay from "../controllers/overlay.controller";
+import {
+  overlay,
+  getTwitchAuthorizeUrl,
+} from "../controllers/overlay.controller";
 
 const overlayRouter = Router();
 
 overlayRouter.get("/", overlay);
+overlayRouter.get("/twitch-authorize-url", getTwitchAuthorizeUrl);
 
 export default overlayRouter;
