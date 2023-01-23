8e10fecd8713ace7c489010282989ed8c8060857
Darosss <Daroing12@gmail.com>
2023-01-23 19:32:28 +0100

    feat: add /messages/id route

--- server/src/routes/messages.route.ts
+++ server/src/routes/messages.route.ts
@@ -1,8 +1,13 @@
 import Express, { Router } from "express";
-import { getMessages } from "../controllers/messages.controller";
+import {
+  getMessages,
+  getUserMessages,
+} from "../controllers/messages.controller";
+import isParamObjectId from "../middlewares/isParamObjectId.middleware";
 
 const messagesRouter = Router();
 
 messagesRouter.get("/", getMessages);
+messagesRouter.get("/:id", isParamObjectId, getUserMessages);
 
 export default messagesRouter;
