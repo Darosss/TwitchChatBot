569fa086cbad26a98bfa32cd1c54a15644ecf2da
Darosss <Daroing12@gmail.com>
2023-01-23 19:26:45 +0100

    feat: add getUserMessages
    create IQueryMsgs interface
    add sort by date

--- server/src/controllers/messages.controller.ts
+++ server/src/controllers/messages.controller.ts
@@ -1,17 +1,23 @@
 import Express, { Request, Response } from "express";
 import { Message } from "../models/message.model";
 
+interface IQueryMsgs {
+  page?: number;
+  limit?: number;
+}
+
 const getMessages = async (req: Request, res: Response) => {
-  const { page = 1, limit = 50 } = req.query as unknown as {
-    page: number;
-    limit: number;
-  };
+  const { page = 1, limit = 50 } = req.query as unknown as IQueryMsgs;
 
   try {
     const messages = await Message.find()
       .limit(limit * 1)
+      .sort({ date: -1 })
       .skip((page - 1) * limit)
-      .populate("owner", { username: 1 })
+      .populate({
+        path: "owner",
+        select: "username",
+      })
       .select({ __v: 0 })
       .exec();
 
@@ -30,4 +36,33 @@ const getMessages = async (req: Request, res: Response) => {
   }
 };
 
-export { getMessages };
+const getUserMessages = async (req: Request, res: Response) => {
+  const { page = 1, limit = 50 } = req.query as unknown as IQueryMsgs;
+  const { id } = req.params;
+  try {
+    const messages = await Message.find({ owner: id })
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate({
+        path: "owner",
+        select: "username",
+      })
+      .select({ __v: 0 })
+      .exec();
+
+    const count = await Message.find({ owner: id }).countDocuments();
+
+    res.status(200).send({
+      messages,
+      totalPages: Math.ceil(count / limit),
+      messageCount: count,
+      currentPage: Number(page),
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(200).send({ message: "Couldn't get messages" });
+  }
+};
+
+export { getMessages, getUserMessages };
