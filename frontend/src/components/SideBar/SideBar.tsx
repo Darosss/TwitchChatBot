2f916fa4cab16fac8fd989b7b0795ee5ef04c9ac
Darosss <Daroing12@gmail.com>
2023-01-23 19:19:57 +0100

    fix: add new routes for user profile and messages

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -6,6 +6,7 @@ import Users from "../Users";
 import Overlay from "../Overlay";
 import MessagesList from "../MessagesList";
 import useFetch from "../../hooks/useFetch.hook";
+import UserProfile from "../UserProfile";
 
 export default function SideBar() {
   const { data: authUrlRes, error: authUrlError } = useFetch<string>(
@@ -38,6 +39,8 @@ export default function SideBar() {
       <Routes>
         <Route path="/" element={<Overlay />}></Route>
         <Route path="/messages" element={<MessagesList />}></Route>
+        <Route path="/messages/:userId" element={<MessagesList />}></Route>
+        <Route path="/user/:userId" element={<UserProfile />}></Route>
         <Route path="/users" element={<Users />}></Route>
         <Route path="/chat" element={<TwitchChat />}></Route>
       </Routes>
