e0fb95942850938d313f4b5ebcf863f0eaa89e88
Darosss <Daroing12@gmail.com>
2023-01-25 13:12:31 +0100

    fix: add first and latest messages route

--- server/src/routes/messages.route.ts
+++ server/src/routes/messages.route.ts
@@ -2,12 +2,18 @@ import Express, { Router } from "express";
 import {
   getMessages,
   getUserMessages,
+  getLatestAndFirstMsgs,
 } from "../controllers/messages.controller";
 import isParamObjectId from "../middlewares/isParamObjectId.middleware";
 
 const messagesRouter = Router();
 
 messagesRouter.get("/", getMessages);
+messagesRouter.get(
+  "/:id/latest-first-msgs",
+  isParamObjectId,
+  getLatestAndFirstMsgs
+);
 messagesRouter.get("/:id", isParamObjectId, getUserMessages);
 
 export default messagesRouter;
