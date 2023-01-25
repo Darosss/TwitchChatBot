2925c5a1c0218bf46b42725a402daf175f660eee
Darosss <Daroing12@gmail.com>
2023-01-25 13:12:56 +0100

    fix: add edit and get user profile

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -1,8 +1,15 @@
 import Express, { Router } from "express";
-import { getUsers } from "../controllers/users.controller";
+import {
+  getUsers,
+  getUsersProfile,
+  editUserProfile,
+} from "../controllers/users.controller";
+import isParamObjectId from "../middlewares/isParamObjectId.middleware";
 
 const usersRouter = Router();
 
 usersRouter.get("/", getUsers);
+usersRouter.get("/:id", isParamObjectId, getUsersProfile);
+usersRouter.post("/:id", isParamObjectId, editUserProfile);
 
 export default usersRouter;
