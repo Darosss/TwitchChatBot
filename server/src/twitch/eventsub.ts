dfcbdd04de6d5a14b28804f4e8a3638ee6829c8d
Darosss <Daroing12@gmail.com>
2023-01-26 18:20:29 +0100

    feat: add eventsub websocket twitch

---
+++ server/src/twitch/eventsub.ts
@@ -0,0 +1,64 @@
+import { ApiClient } from "@twurple/api";
+import { StaticAuthProvider } from "@twurple/auth";
+import { EventSubWsListener } from "@twurple/eventsub-ws";
+
+import { Server } from "socket.io";
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData,
+} from "../../../libs/types";
+import { Redemption } from "../models/redemption.model";
+
+const eventSub = async (
+  authProvider: StaticAuthProvider,
+  userId: string,
+  socket: Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+  >
+) => {
+  const apiClient = new ApiClient({ authProvider });
+
+  const listener = new EventSubWsListener({ apiClient });
+  await listener.start();
+
+  const redemptionSubscription =
+    await listener.subscribeToChannelRedemptionAddEvents(userId, async (e) => {
+      const {
+        rewardId,
+        userId,
+        userName,
+        userDisplayName,
+        redeemedAt,
+        rewardTitle,
+        rewardCost,
+      } = e;
+
+      const reward = await e.getReward();
+      const rewardData = {
+        rewardId: rewardId,
+        userId: userId,
+        userName: userName,
+        userDisplayName: userDisplayName,
+        redemptionDate: redeemedAt,
+        rewardTitle: rewardTitle,
+        rewardCost: rewardCost,
+        rewardImage: reward.getImageUrl(4),
+      };
+
+      try {
+        await new Redemption(rewardData).save();
+      } catch (err) {
+        console.log(err, "Couldn't save redemption");
+      }
+
+      socket.emit("onRedemption", rewardData);
+      console.log(`${e.userDisplayName} just took redemption!`);
+    });
+};
+
+export default eventSub;
