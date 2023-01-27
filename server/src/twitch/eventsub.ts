09495566072b3544ad13915c6a638ceda3ea3fb1
Darosss <Daroing12@gmail.com>
2023-01-27 14:56:48 +0100

    feat: add event on online/ofline/update stream

--- server/src/twitch/eventsub.ts
+++ server/src/twitch/eventsub.ts
@@ -9,6 +9,7 @@ import {
   SocketData,
 } from "../../../libs/types";
 import { Redemption } from "../models/redemption.model";
+import { TwitchSession } from "../models/twitch-session.model";
 
 const eventSub = async (
   apiClient: ApiClient,
@@ -56,6 +57,46 @@ const eventSub = async (
       socket.emit("onRedemption", rewardData);
       console.log(`${e.userDisplayName} just took redemption!`);
     });
+
+  const onlineSubscription = await listener.subscribeToStreamOnlineEvents(
+    userId,
+    (e) => {
+      console.log(`${e.broadcasterDisplayName} just went live!`);
+      e.getStream()
+        .then((stream) => {
+          console.log("swtream tag", stream.tags);
+          new TwitchSession({
+            sessionStart: e.startDate,
+            sessionTitles: stream.title,
+            categories: stream.gameName,
+            tags: stream.tags || "",
+          }).save((err, doc) => {
+            if (err) console.log("err save", err);
+            onUpdateStreamDetails(doc.id);
+            offlineSubscription(doc.id);
+          });
+        })
+        .catch((err) => console.log("Online subs err", err));
+    }
+  );
+
+  const offlineSubscription = async (sessionId: string) => {
+    return await listener.subscribeToStreamOfflineEvents(userId, async (e) => {
+      console.log(`${e.broadcasterDisplayName} just went offline`);
+      await TwitchSession.findByIdAndUpdate(sessionId, {
+        sessionEnd: new Date(),
+      });
+    });
+  };
+
+  const onUpdateStreamDetails = async (sessionId: string) => {
+    return await listener.subscribeToChannelUpdateEvents(userId, async (e) => {
+      console.log("Stream details has been updated");
+      await TwitchSession.findByIdAndUpdate(sessionId, {
+        $push: { categories: e.categoryName, sessionTitles: e.streamTitle },
+      });
+    });
+  };
 };
 
 export default eventSub;
