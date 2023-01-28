10641c2ca24a7f73eb5b9d3ac176318fa4dfa3a2
Darosss <Daroing12@gmail.com>
2023-01-28 14:53:39 +0100

    feat: add updateEveryUserTwitchDetails and ApiClient to constructor

--- server/src/chatbot/database-statistic.ts
+++ server/src/chatbot/database-statistic.ts
@@ -1,4 +1,5 @@
 import { ObjectId } from "mongoose";
+import { ApiClient } from "@twurple/api";
 import { Message } from "../models/message.model";
 import { IUser } from "../models/types";
 import { User } from "../models/user.model";
@@ -7,8 +8,34 @@ type userId = string | ObjectId;
 
 class BotStatisticDatabase {
   commandPrefix: string;
-  constructor() {
+  twitchApi: ApiClient;
+
+  constructor(twitchApi: ApiClient) {
+    this.twitchApi = twitchApi;
     this.commandPrefix = "--";
+    this.updateEveryUserTwitchDetails();
+  }
+
+  async updateEveryUserTwitchDetails() {
+    const broadcasterId = (await this.twitchApi.users.getMe()).id;
+    const users = await User.find({});
+    try {
+      for (let indx = 0; indx < users.length; indx++) {
+        const userTwitch = await this.twitchApi.users.getUserByName(
+          users[indx].username
+        );
+        const follower = await userTwitch?.getFollowTo(broadcasterId);
+
+        await User.findByIdAndUpdate(users[indx].id, {
+          twitchId: userTwitch?.id,
+          twitchName: userTwitch?.name,
+          follower: follower?.followDate,
+        });
+      }
+      console.log("Finished checking users in twitch details");
+    } catch (error) {
+      console.log("Couldn't save twitch details ");
+    }
   }
 
   async saveMessageToDatabase(senderId: userId, message: string) {
@@ -28,6 +55,7 @@ class BotStatisticDatabase {
 
   async isUserInDB(username: string) {
     const user = await User.findOne({ username: username });
+
     if (!user) {
       return await this.#createNewDBUser(username);
     } else {
@@ -42,6 +70,7 @@ class BotStatisticDatabase {
 
       return newUser;
     } catch (err) {
+      console.log("err", err);
       return false;
     }
   }
