27d6b5953217da99b4543c7f766a91e995647746
Darosss <Daroing12@gmail.com>
2023-01-28 15:02:53 +0100

    refactor: move twitch funcionality to own file

---
+++ server/src/twitch/initTwitchOnAuth.ts
@@ -0,0 +1,39 @@
+import { StaticAuthProvider } from "@twurple/auth";
+import eventSub from "./eventsub";
+import { ApiClient } from "@twurple/api";
+import * as ClientTmi from "../twitch-tmi";
+import BotStatisticDatabase from "../chatbot/database-statistic";
+import { Server } from "socket.io";
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData,
+} from "../../../libs/types";
+
+const initTwitchOnAuth = async (
+  authAccesToken: string,
+  socketIO: Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+  >
+) => {
+  const authProvider = new StaticAuthProvider(
+    process.env.CLIENT_ID!,
+    authAccesToken
+  );
+  const twitchApi = new ApiClient({ authProvider });
+
+  const botStatisticDatabase = new BotStatisticDatabase(twitchApi);
+  const TwitchTmi = ClientTmi.default(socketIO, botStatisticDatabase);
+  TwitchTmi.connect();
+
+  const authorizedUserId = (await twitchApi.users.getMe()).id;
+  if (authorizedUserId) {
+    eventSub(twitchApi, authorizedUserId, socketIO);
+  }
+};
+
+export default initTwitchOnAuth;
