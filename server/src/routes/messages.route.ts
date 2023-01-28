1bd032ae01f536a74c50de0332c951190dfa3ba5
Darosss <Daroing12@gmail.com>
2023-01-28 15:01:15 +0100

    fix: add twitch-session messages router

--- server/src/routes/messages.route.ts
+++ server/src/routes/messages.route.ts
@@ -3,12 +3,14 @@ import {
   getMessages,
   getUserMessages,
   getLatestAndFirstMsgs,
+  getSessionMessages,
 } from "../controllers/messages.controller";
 import isParamObjectId from "../middlewares/isParamObjectId.middleware";
 
 const messagesRouter = Router();
 
 messagesRouter.get("/", getMessages);
+messagesRouter.get("/twitch-session/:id", getSessionMessages);
 messagesRouter.get(
   "/:id/latest-first-msgs",
   isParamObjectId,
