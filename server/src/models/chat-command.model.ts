f97d3371c4ed3914ba6dee5b1db4114e368a47e5
Darosss <Daroing12@gmail.com>
2023-01-31 17:41:10 +0100

    feat: add chat command model

---
+++ server/src/models/chat-command.model.ts
@@ -0,0 +1,24 @@
+import { Model, model, Schema } from "mongoose";
+import { IChatCommandDocument } from "./types";
+
+const ChatCommandSchema: Schema<IChatCommandDocument> = new Schema({
+  name: { type: String, required: true },
+  enabled: { type: Boolean, default: true },
+  aliases: [String],
+  messages: [String],
+  privilage: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 10,
+  },
+  useCount: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export const ChatCommand: Model<IChatCommandDocument> = model(
+  "ChatCommands",
+  ChatCommandSchema
+);
