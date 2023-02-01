c09d143b352a349bffc8ea7ca7d7c2ce81cb82c3
Darosss <Daroing12@gmail.com>
2023-02-01 18:00:56 +0100

    fix: add chat-commands route to app

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import messagesRouter from "./messages.route";
 import usersRouter from "./users.route";
 import twitchSessionRouter from "./twitch-session.route";
 import redemptionsRouter from "./redemptions.route";
+import chatCommandsRouter from "./chat-commands.route";
 
 const initRoutes = (app: any) => {
   app.use("/", overlayRouter);
@@ -10,6 +11,7 @@ const initRoutes = (app: any) => {
   app.use("/messages", messagesRouter);
   app.use("/twitch-sessions", twitchSessionRouter);
   app.use("/redemptions", redemptionsRouter);
+  app.use("/chat-commands", chatCommandsRouter);
 };
 
 export default initRoutes;
