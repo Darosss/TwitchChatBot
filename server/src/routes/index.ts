3ace2b923e64c7398319dbe9b414a647f268a83f
Darosss <Daroing12@gmail.com>
2023-02-03 19:06:51 +0100

    fix: add configsRouter to app

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from "./users.route";
 import twitchSessionRouter from "./twitch-session.route";
 import redemptionsRouter from "./redemptions.route";
 import chatCommandsRouter from "./chat-commands.route";
+import configsRouter from "./configs.route";
 
 const initRoutes = (app: any) => {
   app.use("/", overlayRouter);
@@ -12,6 +13,7 @@ const initRoutes = (app: any) => {
   app.use("/twitch-sessions", twitchSessionRouter);
   app.use("/redemptions", redemptionsRouter);
   app.use("/chat-commands", chatCommandsRouter);
+  app.use("/configs", configsRouter);
 };
 
 export default initRoutes;
