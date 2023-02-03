cb714f3189eb594c53031d9eb31335e23b480d2e
Darosss <Daroing12@gmail.com>
2023-02-03 19:07:56 +0100

    fix: change permissionLevel to object and mark as capped collection

--- server/src/models/config.model.ts
+++ server/src/models/config.model.ts
@@ -1,21 +1,23 @@
 import { Model, model, Schema } from "mongoose";
 import { IConfigDocument } from "./types";
 
-const ConfigSchema: Schema<IConfigDocument> = new Schema({
-  commandsPrefix: { type: String, required: true, default: "--" },
-  timersIntervalDelay: { type: Number, required: true, default: 20 },
-  activeUserTimeDelay: { type: Number, required: true, default: 150 },
-  chatGamesIntervalDelay: { type: Number, required: true, default: 20 },
-  minActiveUsersThreshold: { type: Number, required: true, default: 3 },
-  permissionLevels: {
-    type: Map,
-    default: new Map([
-      ["broadcaster", 10],
-      ["mod", 8],
-      ["vip", 5],
-      ["all", 0],
-    ]),
+const ConfigSchema: Schema<IConfigDocument> = new Schema(
+  {
+    commandsPrefix: { type: String, required: true, default: "--" },
+    timersIntervalDelay: { type: Number, required: true, default: 20 },
+    activeUserTimeDelay: { type: Number, required: true, default: 150 },
+    chatGamesIntervalDelay: { type: Number, required: true, default: 20 },
+    minActiveUsersThreshold: { type: Number, required: true, default: 3 },
+    permissionLevels: {
+      broadcaster: { type: Number, default: 10 },
+      mod: { type: Number, default: 8 },
+      vip: { type: Number, default: 4 },
+      all: { type: Number, default: 0 },
+    },
   },
-});
+  {
+    capped: { size: 100000, max: 1 },
+  }
+);
 
 export const Config: Model<IConfigDocument> = model("Configs", ConfigSchema);
