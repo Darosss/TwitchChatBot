695bc82a15b14488303c583e3db860d3232f3311
Darosss <Daroing12@gmail.com>
2023-02-04 14:21:55 +0100

    refactor: a little improvement to hours, days

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -26,8 +26,9 @@ const daysAndHoursAgoFormat = (date: Date) => {
   const seconds = Math.floor(
     (new Date().getTime() - new Date(date).getTime()) / 1000
   );
-  const days = Math.floor(seconds / 60 / 60 / 24);
+
   const hours = Math.floor(seconds / 60 / 60);
+  const days = Math.floor(hours / 24);
 
   if (days > 0) {
     formated += `${days}d`;
