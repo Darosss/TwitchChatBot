922f2fec27cfe7402cb3f28962eeca903207f71d
Darosss <Daroing12@gmail.com>
2023-02-04 14:12:20 +0100

    fix: add days+time format date

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-const formatDate = (date: Date, format?: "date" | "time") => {
+const formatDate = (date: Date, format?: "date" | "time" | "days+time") => {
   let formatedDate = "";
 
   switch (format) {
@@ -10,11 +10,35 @@ const formatDate = (date: Date, format?: "date" | "time") => {
       formatedDate = date?.toString().split("T")[1].split(".")[0];
       break;
 
+    case "days+time":
+      formatedDate = daysAndHoursAgoFormat(date);
+      break;
+
     default:
       formatedDate = date?.toString().replace("T", " ").split(".")[0];
   }
 
   return String(formatedDate);
+};
+
+const daysAndHoursAgoFormat = (date: Date) => {
+  let formated = "";
+  const seconds = Math.floor(
+    (new Date().getTime() - new Date(date).getTime()) / 1000
+  );
+  const days = Math.floor(seconds / 60 / 60 / 24);
+  const hours = Math.floor(seconds / 60 / 60);
+
+  if (days > 0) {
+    formated += `${days}d`;
+    let leftHours = hours - days * 24;
+    if (leftHours > 0) formated += ` ${leftHours}h,`;
+  } else {
+    formated += `${hours}h`;
+  }
+
+  formated += ` ${date?.toString().split("T")[1].split(".")[0]}`;
+  return formated;
 };
 
 export default formatDate;
