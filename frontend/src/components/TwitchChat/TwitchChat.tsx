972dfd1783c166bcfe720f34a0deb90925f5e606
Darosss <Daroing12@gmail.com>
2023-02-06 20:28:20 +0100

    feat: add on join chat socket event
    add notifications with user details when user joins chat
    change size of chat and add resizable property
    and add some minor decorative css

--- frontend/src/components/TwitchChat/TwitchChat.tsx
+++ frontend/src/components/TwitchChat/TwitchChat.tsx
@@ -1,24 +1,45 @@
-import React, { MouseEvent, useContext, useEffect, useState } from "react";
+import React, {
+  MouseEvent,
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import { SocketContext } from "@context/SocketContext";
 import Message from "@components/Message";
 
 import "./style.css";
+import formatDate from "@utils/formatDate";
+import { Link } from "react-router-dom";
+import { IEventAndIUser } from "@libs/types";
 
 export default function TwitchChat() {
+  const LIMIT_NOTIFICATIONS = 5;
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
   const socket = useContext(SocketContext);
+  const notifications = useRef<HTMLDivElement>(null);
 
   const [messageToSend, setMessageToSend] = useState("");
   const [messages, setMessages] = useState<{
     [index: string]: { date: Date; username: string; message: string };
   }>();
 
+  const [userNotif, setUserNotif] = useState<IEventAndIUser[]>([]);
+
   const sendMessage = (e: MouseEvent<HTMLButtonElement>) => {
     console.log("SEND MESSAGE", messageToSend);
   };
 
+  const removeParentElement = (
+    e: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
+    (e.target as HTMLButtonElement).parentElement?.remove();
+  };
+
   useEffect(() => {
     socket?.on("messageServer", (date, username, message) => {
-      console.log("got message from server", date, username, message);
       setMessages((prevMessages) => {
         const newMessages = { ...prevMessages };
         newMessages[Math.random() * 100] = {
@@ -30,14 +51,29 @@ export default function TwitchChat() {
       });
     });
 
+    socket?.on("userJoinTwitchChat", (event, user) => {
+      setUserNotif((prevState) => {
+        prevState.unshift(Object.assign(user, event) as IEventAndIUser);
+        return prevState;
+      });
+
+      if (userNotif?.length > LIMIT_NOTIFICATIONS) {
+        setUserNotif((prevState) => {
+          prevState.pop();
+          return prevState;
+        });
+      }
+      forceUpdate();
+    });
+
     return () => {
       socket.off("messageServer");
+      socket.off("userJoinTwitchChat");
     };
   }, [socket]);
-
   return (
     <>
-      <div className="twitch-chat-header">
+      <div className="twitch-chat twitch-window">
         <div className="twitch-chat-title">STREAM CHAT</div>
         <div className="twitch-chat-messages">
           {messages
@@ -73,6 +109,95 @@ export default function TwitchChat() {
             SEND
           </button>
         </div>
+      </div>
+      <div className="twitch-notifications twitch-window" ref={notifications}>
+        {userNotif.map((notif) => {
+          return (
+            <div
+              className={`user-info ${
+                Math.floor(
+                  new Date(notif.eventDate).getTime() / 1000 -
+                    new Date(notif.createdAt).getTime() / 1000
+                ) < 1200
+                  ? "new-user"
+                  : "usual"
+              } `}
+              key={notif._id}
+            >
+              <button
+                id="button-close"
+                onClick={(e) => {
+                  removeParentElement(e);
+                }}
+              >
+                X
+              </button>
+              <div className="user-info-event">{notif.eventName}</div>
+              <div className="user-info-date">
+                {formatDate(notif.eventDate, "time")}
+              </div>
+              <div className="user-info-username">
+                <Link to={`../user/${notif._id}`} target="_blank">
+                  {notif.username}
+                </Link>
+              </div>
+              <div className="user-info-messages">
+                <img
+                  src="/icons/message.png"
+                  alt="msg"
+                  className="user-info-icon"
+                />
+                :<span>{notif.messageCount.toLocaleString()}</span>
+              </div>
+
+              <div className="user-info-points">
+                <img
+                  src="/icons/finger.png"
+                  alt="points"
+                  className="user-info-icon"
+                />
+                : <span>{notif.points.toLocaleString()}</span>
+              </div>
+              <div className="user-info-created-date">
+                <img
+                  src="/icons/calendar.png"
+                  alt="created"
+                  className="user-info-icon"
+                />
+                :<span> {formatDate(notif.createdAt, "days+time")}</span>
+              </div>
+              <div className="user-info-last-seen">
+                <img
+                  src="/icons/eyes.png"
+                  alt="seen"
+                  className="user-info-icon"
+                />
+                : <span>today, 15:12:12</span>
+              </div>
+              <div className="user-info-follower">
+                <img
+                  src="/icons/heart.png"
+                  alt="follow"
+                  className="user-info-icon"
+                />
+                :
+                {notif.follower ? (
+                  <span>{formatDate(notif.follower, "days+time")}</span>
+                ) : (
+                  <span>No</span>
+                )}
+              </div>
+              <div className="user-info-subsriber">
+                <img
+                  src="/icons/star.png"
+                  alt="sub"
+                  className="user-info-icon"
+                />
+                <span> 2020-20-20</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
