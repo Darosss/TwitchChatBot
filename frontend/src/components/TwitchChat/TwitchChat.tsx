5792a45c9c6b04b992110f747549883edb4f0fda
Darosss <Daroing12@gmail.com>
2023-02-07 14:25:20 +0100

    feat: add show hide windows and draggable

--- frontend/src/components/TwitchChat/TwitchChat.tsx
+++ frontend/src/components/TwitchChat/TwitchChat.tsx
@@ -12,6 +12,7 @@ import "./style.css";
 import formatDate from "@utils/formatDate";
 import { Link } from "react-router-dom";
 import { IEventAndIUser } from "@libs/types";
+import DragableParent from "@components/DragableParent";
 
 export default function TwitchChat() {
   const LIMIT_NOTIFICATIONS = 5;
@@ -21,6 +22,11 @@ export default function TwitchChat() {
   const socket = useContext(SocketContext);
 
   const [messageToSend, setMessageToSend] = useState("");
+
+  const [showChat, setShowChat] = useState(true);
+  const [showLastChatters, setShowLastChatters] = useState(true);
+  const [showNotifications, setShowNotifications] = useState(true);
+
   const [messages, setMessages] = useState<{
     [index: string]: { date: Date; username: string; message: string };
   }>();
@@ -95,147 +101,196 @@ export default function TwitchChat() {
 
   return (
     <div className="twitch-wrapper">
-      <div className="twitch-chat twitch-window">
-        <div className="twitch-chat-title">STREAM CHAT</div>
-        <div className="twitch-chat-messages">
-          {messages
-            ? [...Object.values(messages)]
-                .sort((a, b) => {
-                  return (
-                    (new Date(b.date) as unknown as number) -
-                    (new Date(a.date) as unknown as number)
-                  );
-                })
-                .map((message) => {
-                  return (
-                    <Message
-                      key={message.date + message.username}
-                      date={message.date}
-                      username={message.username}
-                      message={message.message}
-                    />
-                  );
-                })
-            : null}
-        </div>
-        <div className="twitch-chat-send-message">
-          <textarea
-            className="twitch-chat-textarea"
-            onChange={(e) => setMessageToSend(e.target.value)}
-            value={messageToSend}
-          />
+      {showChat ? (
+        <div className="twitch-chat twitch-window">
           <button
-            onClick={(e) => sendMessage(e)}
-            className="twitch-chat-btn-send-message"
+            onClick={(e) => setShowChat(!showChat)}
+            className="show-hide-twitch-btn"
           >
-            SEND
+            Hide
           </button>
+          <DragableParent />
+          <div className="twitch-chat-title">STREAM CHAT</div>
+          <div className="twitch-chat-messages">
+            {messages
+              ? [...Object.values(messages)]
+                  .sort((a, b) => {
+                    return (
+                      (new Date(b.date) as unknown as number) -
+                      (new Date(a.date) as unknown as number)
+                    );
+                  })
+                  .map((message) => {
+                    return (
+                      <Message
+                        key={message.date + message.username}
+                        date={message.date}
+                        username={message.username}
+                        message={message.message}
+                      />
+                    );
+                  })
+              : null}
+          </div>
+          <div className="twitch-chat-send-message">
+            <textarea
+              className="twitch-chat-textarea"
+              onChange={(e) => setMessageToSend(e.target.value)}
+              value={messageToSend}
+            />
+            <button
+              onClick={(e) => sendMessage(e)}
+              className="twitch-chat-btn-send-message"
+            >
+              SEND
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="twitch-last-chatters twitch-window">
-        {[...lastChatters.keys()].map((chatter) => {
-          return (
-            <div
-              className="user-chatter"
-              key={chatter + lastChatters.get(chatter)}
-            >
-              <div className="user-chatter-username">{chatter}</div>
-              <div className="user-chatter-last-seen">
-                {formatDate(lastChatters.get(chatter) || new Date(), "time")}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="twitch-notifications twitch-window">
-        {userNotif.map((notif) => {
-          return (
-            <div
-              className={`user-info ${
-                Math.floor(
-                  new Date(notif.eventDate).getTime() / 1000 -
-                    new Date(notif.createdAt).getTime() / 1000
-                ) < 1200
-                  ? "new-user"
-                  : "usual"
-              } `}
-              key={notif._id}
-            >
-              <button
-                id="button-close"
-                onClick={(e) => {
-                  removeParentElement(e);
-                }}
-              >
-                X
-              </button>
-              <div className="user-info-event">{notif.eventName}</div>
-              <div className="user-info-date">
-                {formatDate(notif.eventDate, "time")}
-              </div>
-              <div className="user-info-username">
-                <Link to={`../user/${notif._id}`} target="_blank">
-                  {notif.username}
-                </Link>
-              </div>
-              <div className="user-info-messages">
-                <img
-                  src="/icons/message.png"
-                  alt="msg"
-                  className="user-info-icon"
-                />
-                :<span>{notif.messageCount.toLocaleString()}</span>
-              </div>
+      ) : (
+        <button
+          onClick={(e) => setShowChat(!showChat)}
+          className="show-hide-twitch-btn"
+        >
+          Show chat
+        </button>
+      )}
 
-              <div className="user-info-points">
-                <img
-                  src="/icons/finger.png"
-                  alt="points"
-                  className="user-info-icon"
-                />
-                : <span>{notif.points.toLocaleString()}</span>
+      {showLastChatters ? (
+        <div className="twitch-last-chatters twitch-window">
+          <DragableParent />
+          <button
+            onClick={(e) => setShowLastChatters(!showLastChatters)}
+            className="show-hide-twitch-btn"
+          >
+            Hide
+          </button>
+          {[...lastChatters.keys()].map((chatter) => {
+            return (
+              <div
+                className="user-chatter"
+                key={chatter + lastChatters.get(chatter)}
+              >
+                <div className="user-chatter-username">{chatter}</div>
+                <div className="user-chatter-last-seen">
+                  {formatDate(lastChatters.get(chatter) || new Date(), "time")}
+                </div>
               </div>
-              <div className="user-info-created-date">
-                <img
-                  src="/icons/calendar.png"
-                  alt="created"
-                  className="user-info-icon"
-                />
-                :<span> {formatDate(notif.createdAt, "days+time")}</span>
+            );
+          })}
+        </div>
+      ) : (
+        <button
+          onClick={(e) => setShowLastChatters(!showLastChatters)}
+          className="show-hide-twitch-btn"
+        >
+          Show chatters
+        </button>
+      )}
+      {showNotifications ? (
+        <div className="twitch-notifications twitch-window">
+          <DragableParent />
+          <button
+            onClick={(e) => setShowNotifications(!showNotifications)}
+            className="show-hide-twitch-btn"
+          >
+            Hide
+          </button>
+          {userNotif.map((notif) => {
+            return (
+              <div
+                className={`user-info ${
+                  Math.floor(
+                    new Date(notif.eventDate).getTime() / 1000 -
+                      new Date(notif.createdAt).getTime() / 1000
+                  ) < 1200
+                    ? "new-user"
+                    : "usual"
+                } `}
+                key={notif._id}
+              >
+                <button
+                  id="button-close"
+                  onClick={(e) => {
+                    removeParentElement(e);
+                  }}
+                >
+                  X
+                </button>
+                <div className="user-info-event">{notif.eventName}</div>
+                <div className="user-info-date">
+                  {formatDate(notif.eventDate, "time")}
+                </div>
+                <div className="user-info-username">
+                  <Link to={`../user/${notif._id}`} target="_blank">
+                    {notif.username}
+                  </Link>
+                </div>
+                <div className="user-info-messages">
+                  <img
+                    src="/icons/message.png"
+                    alt="msg"
+                    className="user-info-icon"
+                  />
+                  :<span>{notif.messageCount.toLocaleString()}</span>
+                </div>
+
+                <div className="user-info-points">
+                  <img
+                    src="/icons/finger.png"
+                    alt="points"
+                    className="user-info-icon"
+                  />
+                  : <span>{notif.points.toLocaleString()}</span>
+                </div>
+                <div className="user-info-created-date">
+                  <img
+                    src="/icons/calendar.png"
+                    alt="created"
+                    className="user-info-icon"
+                  />
+                  :<span> {formatDate(notif.createdAt, "days+time")}</span>
+                </div>
+                <div className="user-info-last-seen">
+                  <img
+                    src="/icons/eyes.png"
+                    alt="seen"
+                    className="user-info-icon"
+                  />
+                  : <span>today, 15:12:12</span>
+                </div>
+                <div className="user-info-follower">
+                  <img
+                    src="/icons/heart.png"
+                    alt="follow"
+                    className="user-info-icon"
+                  />
+                  :
+                  {notif.follower ? (
+                    <span>{formatDate(notif.follower, "days+time")}</span>
+                  ) : (
+                    <span>No</span>
+                  )}
+                </div>
+                <div className="user-info-subsriber">
+                  <img
+                    src="/icons/star.png"
+                    alt="sub"
+                    className="user-info-icon"
+                  />
+                  <span> 2020-20-20</span>
+                </div>
               </div>
-              <div className="user-info-last-seen">
-                <img
-                  src="/icons/eyes.png"
-                  alt="seen"
-                  className="user-info-icon"
-                />
-                : <span>today, 15:12:12</span>
-              </div>
-              <div className="user-info-follower">
-                <img
-                  src="/icons/heart.png"
-                  alt="follow"
-                  className="user-info-icon"
-                />
-                :
-                {notif.follower ? (
-                  <span>{formatDate(notif.follower, "days+time")}</span>
-                ) : (
-                  <span>No</span>
-                )}
-              </div>
-              <div className="user-info-subsriber">
-                <img
-                  src="/icons/star.png"
-                  alt="sub"
-                  className="user-info-icon"
-                />
-                <span> 2020-20-20</span>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      ) : (
+        <button
+          onClick={(e) => setShowNotifications(!showNotifications)}
+          className="show-hide-twitch-btn"
+        >
+          Show notifications
+        </button>
+      )}
     </div>
   );
 }
