595e435a861678c19f08e51a7aa1a262c8ef08fa
Darosss <Daroing12@gmail.com>
2023-02-07 16:16:16 +0100

    refactor: move TwitchEvents to 3 separated components

---
+++ frontend/src/components/TwitchEvents/TwitchChat/TwitchChat.tsx
@@ -0,0 +1,83 @@
+import "./style.css";
+import React, { useContext, useEffect, useReducer, useState } from "react";
+
+import DragableParent from "@components/DragableParent";
+import Message from "@components/Message";
+import { SocketContext } from "@context/SocketContext";
+
+export default function TwitchChat() {
+  const socket = useContext(SocketContext);
+
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  const [messages, setMessages] = useState<{
+    [index: string]: { date: Date; username: string; message: string };
+  }>();
+
+  const [messageToSend, setMessageToSend] = useState("");
+
+  const sendMessage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log("SEND MESSAGE", messageToSend);
+  };
+
+  useEffect(() => {
+    socket?.on("messageServer", (date, username, message) => {
+      setMessages((prevMessages) => {
+        const newMessages = { ...prevMessages };
+        newMessages[Math.random() * 100] = {
+          date: date,
+          username: username,
+          message: message,
+        };
+        return newMessages;
+      });
+
+      forceUpdate();
+    });
+
+    return () => {
+      socket.off("messageServer");
+    };
+  }, [socket]);
+
+  return (
+    <div className="twitch-chat twitch-window">
+      <DragableParent />
+      <div className="twitch-chat-title">STREAM CHAT</div>
+      <div className="twitch-chat-messages">
+        {messages
+          ? [...Object.values(messages)]
+              .sort((a, b) => {
+                return (
+                  (new Date(b.date) as unknown as number) -
+                  (new Date(a.date) as unknown as number)
+                );
+              })
+              .map((message) => {
+                return (
+                  <Message
+                    key={message.date + message.username}
+                    date={message.date}
+                    username={message.username}
+                    message={message.message}
+                  />
+                );
+              })
+          : null}
+      </div>
+      <div className="twitch-chat-send-message">
+        <textarea
+          className="twitch-chat-textarea"
+          onChange={(e) => setMessageToSend(e.target.value)}
+          value={messageToSend}
+        />
+        <button
+          onClick={(e) => sendMessage(e)}
+          className="twitch-chat-btn-send-message"
+        >
+          SEND
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/TwitchEvents/TwitchChatters/index.ts
@@ -0,0 +1,3 @@
+import TwitchChatters from "./TwitchChatters";
+
+export default TwitchChatters;
