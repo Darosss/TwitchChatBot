e55feb46ce8df77044458f664221a1c80e1eba25
Darosss <Daroing12@gmail.com>
2023-02-10 23:32:13 +0100

    feat: add twitch-commands filter for model

---
+++ server/src/controllers/filters/commands.filter.ts
@@ -0,0 +1,61 @@
+import { IRequestCommandsQuery } from "@types";
+
+const filterCommandsByUrlParams = (params: IRequestCommandsQuery) => {
+  const {
+    search_name,
+    start_date,
+    end_date,
+    privilege,
+    description,
+    aliases,
+    messages,
+  } = params;
+
+  const filterCreated = {
+    ...(start_date &&
+      end_date === undefined && {
+        createdAt: { $gte: new Date(start_date) },
+      }),
+    ...(end_date &&
+      start_date === undefined && {
+        createdAt: { $lte: new Date(end_date) },
+      }),
+    ...(start_date &&
+      end_date && {
+        $and: [
+          { createdAt: { $gte: new Date(start_date) } },
+          { createdAt: { $lte: new Date(end_date) } },
+        ],
+      }),
+  };
+
+  const filterName = {
+    ...(search_name && { name: { $regex: search_name, $options: "i" } }),
+  };
+  const filterPrivilege = {
+    ...(privilege && { privilege: { $eq: privilege } }),
+  };
+  console.log(filterPrivilege);
+  const filterAliases = {
+    ...(aliases && { aliases: { $regex: aliases, $options: "i" } }),
+  };
+  const filterDescription = {
+    ...(description && { description: { $regex: description, $options: "i" } }),
+  };
+  const filterMessages = {
+    ...(messages && { messages: { $regex: messages, $options: "i" } }),
+  };
+  const searchFilter = {
+    $and: [
+      filterCreated,
+      filterName,
+      filterPrivilege,
+      filterAliases,
+      filterDescription,
+      filterMessages,
+    ],
+  };
+  return searchFilter;
+};
+
+export { filterCommandsByUrlParams };
