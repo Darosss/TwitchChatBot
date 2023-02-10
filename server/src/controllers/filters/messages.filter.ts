a8cc1c1d9edddd88139f590b1de6dbc3059702be
Darosss <Daroing12@gmail.com>
2023-02-10 13:50:44 +0100

    refactor: move filter to filters folder

---
+++ server/src/controllers/filters/messages.filter.ts
@@ -0,0 +1,28 @@
+import { User } from "@models/user.model";
+import { IRequestQueryMessage } from "@types";
+
+const filterMessagesByUrlParams = async (params: IRequestQueryMessage) => {
+  const { search_name, owner, start_date, end_date } = params;
+
+  const searchingUserId = (await User.findOne({ username: owner }))?.id;
+
+  const searchFilter = {
+    ...(search_name && { message: { $regex: search_name } }),
+    ...(owner && { owner: { $eq: searchingUserId } }),
+    ...(start_date &&
+      end_date === undefined && { date: { $gte: new Date(start_date) } }),
+    ...(end_date &&
+      start_date === undefined && { date: { $lte: new Date(end_date) } }),
+    ...(start_date &&
+      end_date && {
+        $and: [
+          { date: { $gte: new Date(start_date) } },
+          { date: { $lte: new Date(end_date) } },
+        ],
+      }),
+  };
+
+  return searchFilter;
+};
+
+export { filterMessagesByUrlParams };
