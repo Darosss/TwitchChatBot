31aeeab2effe16f1eebf6810ef1da9b0a87c0984
Darosss <Daroing12@gmail.com>
2023-02-11 14:34:06 +0100

    feat: add remove difference from set util

---
+++ server/src/utils/remove-difference-set.util.ts
@@ -0,0 +1,10 @@
+const removeDifferenceFromSet = (
+  originalSet: Set<string>,
+  toBeRemovedSet: Set<string>
+) => {
+  [...toBeRemovedSet].forEach(function (v) {
+    originalSet.delete(v);
+  });
+};
+
+export default removeDifferenceFromSet;
