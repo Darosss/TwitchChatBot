798ce031f771fd72b6ec5b88ab3bda01765f695d
Darosss <daroing12@gmail.com>
2023-02-16 15:21:32 +0100

    feat: add User service

---
+++ frontend/src/services/User.service.ts
@@ -0,0 +1,40 @@
+import useAxiosCustom, { IPagination } from "./Api.service";
+
+export interface IUser {
+  _id: string;
+  twitchId: string;
+  username: string;
+  createdAt: Date;
+  privileges: number;
+  points?: number;
+  lastSeen: Date;
+  messageCount?: number;
+  notes?: string[];
+  twitchName?: string;
+  twitchCreated?: Date;
+  userDisplayName?: string;
+  follower?: Date;
+}
+
+const getUsersList = () => {
+  return useAxiosCustom<IPagination<IUser>>({
+    url: `/users`,
+  });
+};
+
+const getUser = (userId: string) => {
+  return useAxiosCustom<IUser>({
+    url: `/users/${userId}`,
+  });
+};
+
+const editUser = (userId: string, data: Partial<IUser>) => {
+  return useAxiosCustom<IUser>({
+    url: `/users/${userId}`,
+    method: "POST",
+    bodyData: data,
+    manual: true,
+  });
+};
+
+export default { getUsersList, editUser, getUser };
