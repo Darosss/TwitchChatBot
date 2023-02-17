a8cad496981b78dad0a09995400a914e70d240bb
Darosss <daroing12@gmail.com>
2023-02-17 20:51:28 +0100

    feat(RedemptionService): add getMostActiveUsersByRedempiotns

--- server/src/services/Redemption/Redemption.service.ts
+++ server/src/services/Redemption/Redemption.service.ts
@@ -43,3 +43,48 @@ export const createRedemption = async (
     throw new Error("Failed to create redemption");
   }
 };
+
+export const getMostActiveUsersByRedemptions = async (
+  limit: number = 3,
+  startDate?: Date,
+  endDate?: Date
+) => {
+  const activeUsers = await Redemption.aggregate([
+    {
+      $match: {
+        ...(startDate &&
+          endDate && {
+            redemptionDate: { $gte: startDate, $lt: endDate },
+          }),
+      },
+    },
+    {
+      $group: {
+        _id: "$userId",
+        redemptionsCount: { $sum: 1 },
+        redemptionsCost: { $sum: "$rewardCost" },
+      },
+    },
+    { $sort: { redemptionsCost: -1 } },
+    { $limit: limit },
+    {
+      $lookup: {
+        from: "users",
+        localField: "_id",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    { $unwind: "$user" },
+    {
+      $project: {
+        _id: 1,
+        username: "$user.username",
+        redemptionsCount: 1,
+        redemptionsCost: 1,
+      },
+    },
+  ]);
+
+  return activeUsers;
+};
