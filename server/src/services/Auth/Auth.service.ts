9e3c871e16cf9b02c6f4157f1114ec90b1f0d464
Darosss <daroing12@gmail.com>
2023-02-17 15:57:02 +0100

    feat: add new functions for check valid auth

--- server/src/services/Auth/Auth.service.ts
+++ server/src/services/Auth/Auth.service.ts
@@ -1,6 +1,15 @@
 import { AuthToken } from "@models/auth.model";
 import { AuthCreateData } from "./types/Auth";
 
+export const getAuthToken = async () => {
+  try {
+    const auth = await AuthToken.findOne({});
+    return auth;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to find any auth");
+  }
+};
 export const createNewAuth = async (createData: AuthCreateData) => {
   try {
     const newAuth = await AuthToken.create(createData);
@@ -9,4 +18,28 @@ export const createNewAuth = async (createData: AuthCreateData) => {
     console.error(err);
     throw new Error("Failed to create new auth");
   }
+};
+
+export const checkIfAuthValid = async () => {
+  try {
+    const auth = await getAuthToken();
+    if (!auth) return false;
+    const { expiresIn, obtainmentTimestamp } = auth;
+
+    const newDate = new Date().getTime();
+    const currentSeconds = (newDate - obtainmentTimestamp) / 1000;
+
+    if (expiresIn > currentSeconds) return true;
+    return false;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to check if auth is valid");
+  }
+};
+
+export const createOrGetIfAuthValid = async (createData: AuthCreateData) => {
+  if (!(await checkIfAuthValid())) {
+    return await createNewAuth(createData);
+  }
+  return (await getAuthToken())!;
 };
