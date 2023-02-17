4a67c8994e933e3abb337e3e88fcc22a21b2586e
Darosss <daroing12@gmail.com>
2023-02-17 20:48:10 +0100

    feat: add new routes for current session and statistics

--- server/src/routes/twitch-session.route.ts
+++ server/src/routes/twitch-session.route.ts
@@ -1,9 +1,18 @@
 import Express, { Router } from "express";
-import { getTwitchSessionsList } from "@controllers/twitch-session.controller";
+import {
+  getCurrentSession,
+  getTwitchSessionsList,
+  getCurrentSessionStatistics,
+} from "@controllers/twitch-session.controller";
 import checkSearchParams from "@middlewares/checkSearchParams.middleware";
 
 const twitchSessionRouter = Router();
 
 twitchSessionRouter.get("/", checkSearchParams, getTwitchSessionsList);
+twitchSessionRouter.get("/current-session", getCurrentSession);
+twitchSessionRouter.get(
+  "/current-session/statistics",
+  getCurrentSessionStatistics
+);
 
 export default twitchSessionRouter;
