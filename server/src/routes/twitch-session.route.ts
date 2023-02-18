b9faf531404cbfb406c8b4d8b80610e7002f9c28
Darosss <daroing12@gmail.com>
2023-02-18 19:41:45 +0100

    feat(route): add /id and /id/statistic routes

--- server/src/routes/twitch-session.route.ts
+++ server/src/routes/twitch-session.route.ts
@@ -3,13 +3,22 @@ import {
   getCurrentSession,
   getTwitchSessionsList,
   getCurrentSessionStatistics,
+  getSessionStatisticsById,
+  getSessionById,
 } from "@controllers/twitch-session.controller";
 import checkSearchParams from "@middlewares/checkSearchParams.middleware";
+import isParamObjectId from "@middlewares/isParamObjectId.middleware";
 
 const twitchSessionRouter = Router();
 
 twitchSessionRouter.get("/", checkSearchParams, getTwitchSessionsList);
 twitchSessionRouter.get("/current-session", getCurrentSession);
+twitchSessionRouter.get("/:id", isParamObjectId, getSessionById);
+twitchSessionRouter.get(
+  "/:id/statistics",
+  isParamObjectId,
+  getSessionStatisticsById
+);
 twitchSessionRouter.get(
   "/current-session/statistics",
   getCurrentSessionStatistics
