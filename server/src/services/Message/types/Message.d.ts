e72b12f0437afa38c975dc1fdb7a7dbb07416d45
Darosss <daroing12@gmail.com>
2023-02-22 10:00:05 +0100

    refactor(service): change MessageCreateData to Omit

--- server/src/services/Message/types/Message.d.ts
+++ server/src/services/Message/types/Message.d.ts
@@ -1,11 +1,7 @@
 import { PopulateOption, PopulateOptions } from "mongoose";
 import { SortQuery, SelectQuery } from "@services/types";
 
-export interface MessageCreateData {
-  message: string;
-  date: Date;
-  owner: string;
-}
+export type MessageCreateData = Omit<IMessage, "_id">;
 
 export interface MessageFindOptions {
   select?: SelectQuery<IMessage> | {};
