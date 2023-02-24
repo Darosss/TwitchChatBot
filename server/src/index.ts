45dd949c00d4cc1610fdbc39ad9519f3f9f9872a
Darosss <daroing12@gmail.com>
2023-02-24 23:06:14 +0100

    refactor: move server logic into fn

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,22 @@
 import dotenv from "dotenv";
 import path from "node:path";
+import "module-alias/register";
 
-import initMongoDataBase, { initDefaultsDB } from "./mongoDBConn";
+import initMongoDataBase from "@configs/database";
 import expressApp from "./app";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-initMongoDataBase();
-initDefaultsDB();
+const startServer = async () => {
+  await initMongoDataBase();
 
-const server = expressApp();
+  const server = expressApp();
 
-server.listen(process.env.BACKEND_PORT, () => {
-  console.log("listening on *:", process.env.BACKEND_PORT);
+  server.listen(process.env.BACKEND_PORT, async () => {
+    console.log("listening on *:", process.env.BACKEND_PORT);
+  });
+};
+
+startServer().catch((error) => {
+  console.error(error);
 });
