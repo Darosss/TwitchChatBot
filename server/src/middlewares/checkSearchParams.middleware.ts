8983895e1eb5c4b1bdd0965499e7b36cbff71b8c
Darosss <daroing12@gmail.com>
2023-02-26 13:29:09 +0100

    feat(middleware): add AppError to checkSearchParams
    add query to number converssion'

--- server/src/middlewares/checkSearchParams.middleware.ts
+++ server/src/middlewares/checkSearchParams.middleware.ts
@@ -1,26 +1,27 @@
 import { IRequestQuery } from "@types";
+import { AppError } from "@utils/ErrorHandler.util";
 import { NextFunction, Request, Response } from "express";
 
-const checkSearchParams = (req: Request, res: Response, next: NextFunction) => {
-  const { page, limit } = req.query as unknown as IRequestQuery;
-
+const checkSearchParams = (
+  req: Request<{}, {}, {}, IRequestQuery>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { page, limit } = req.query;
   let message = ``;
 
   if (
     (page && limit && page > 0 && limit > 0) ||
-    page === undefined ||
-    limit === undefined
+    (page === undefined && limit === undefined)
   )
     return next();
 
-  if (page <= 0) message += `Page key must be > 0;`;
-  if (limit <= 0) message += `Limit key must be > 0;`;
+  if (Number(page) <= 0) message += `Page key must be > 0;`;
+  if (Number(limit) <= 0) message += `Limit key must be > 0;`;
 
   if (message.length < 1) message = "Probably page or limit aren't numbers";
 
-  res.status(400).send({
-    message: message,
-  });
+  throw new AppError(400, message);
 };
 
 export default checkSearchParams;
