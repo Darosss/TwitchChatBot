4ed1711b14fa64ae4621b9d29389771b97c9ab59
Darosss <daroing12@gmail.com>
2023-02-27 00:26:28 +0100

    feat(component): move nested routes to routes folder
    add layouts

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -4,18 +4,17 @@ import React from "react";
 import { SocketContext, socketConn } from "@context/SocketContext";
 import SideBar from "@components/SideBar";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import TwitchEvents from "@components/TwitchEvents";
-import Users from "@components/Users";
-import Overlay from "@components/Overlay";
-import MessagesList from "@components/MessagesList";
-import UserProfile from "@components/UserProfile";
-import TwitchSessions from "@components/TwitchSessions/";
-import RedemptionsList from "@components/RedemptionsList";
-import CommandsList from "@components/CommandsList";
-import ConfigsList from "@components/ConfigsList";
-import TriggersList from "@components/TriggersList";
-import TwitchSessionDetail from "@components/TwitchSessionDetail";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+
+import { MessageRoutes } from "@routes/message.routes";
+import { UserRoutes } from "@routes/user.routes";
+import { ConfigRoutes } from "@routes/configs.routes";
+import { TriggerRoutes } from "@routes/trigger.routes";
+import { StreamSessionRoutes } from "@routes/streamSession.routes";
+import { RedemptionRoutes } from "@routes/redemption.routes";
+import { CommandRoutes } from "@routes/command.routes";
+import { EventRoutes } from "@routes/events.routes";
+import { OverlayRoutes } from "@routes/overlay.routes";
 
 function App() {
   return (
@@ -29,134 +28,25 @@ function App() {
 
         <div className="main">
           <Routes>
-            <Route
-              path="/"
-              element={
-                <div className="main-overlay">
-                  <Overlay />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="messages"
-              element={
-                <div className="main-other">
-                  <MessagesList messages="all" />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="twitch-sessions"
-              element={
-                <div className="main-other">
-                  <TwitchSessions />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="twitch-sessions/:sessionId"
-              element={
-                <div className="main-other">
-                  <TwitchSessionDetail />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="messages/:userId"
-              element={
-                <div className="main-other">
-                  <MessagesList messages="user" />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="messages/twitch-session/:sessionId"
-              element={
-                <div className="main-other">
-                  <MessagesList messages="session" />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="redemptions"
-              element={
-                <div className="main-other">
-                  <RedemptionsList redemptions="all" />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="redemptions/:userId"
-              element={
-                <div className="main-other">
-                  <RedemptionsList redemptions="user" />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="redemptions/twitch-session/:sessionId"
-              element={
-                <div className="main-other">
-                  <RedemptionsList redemptions="session" />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="user/:userId"
-              element={
-                <div className="main-other">
-                  <UserProfile />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="users"
-              element={
-                <div className="main-other">
-                  <Users />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="events"
-              element={
-                <div className="main-other">
-                  <TwitchEvents />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="commands"
-              element={
-                <div className="main-other">
-                  <CommandsList />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="commands/:commandId"
-              element={
-                <div className="main-other">
-                  <CommandsList />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="configs"
-              element={
-                <div className="main-other">
-                  <ConfigsList />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="triggers"
-              element={
-                <div className="main-other">
-                  <TriggersList />
-                </div>
-              }
-            ></Route>
+            <Route path="/overlay" element={<OverlayLayout />}>
+              <Route index element={<OverlayRoutes />} />
+            </Route>
+
+            <Route path="/" element={<DefaultRouteLayout />}>
+              <Route index element={<>HOME </>} />
+              <Route path="users/*" element={<UserRoutes />} />
+              <Route path="messages/*" element={<MessageRoutes />} />
+              <Route path="commands/*" element={<CommandRoutes />} />
+              <Route path="events/*" element={<EventRoutes />} />
+              <Route path="redemptions/*" element={<RedemptionRoutes />} />
+              <Route
+                path="stream-sessions/*"
+                element={<StreamSessionRoutes />}
+              />
+              <Route path="triggers/*" element={<TriggerRoutes />} />
+              <Route path="configs/*" element={<ConfigRoutes />} />
+              <Route path="*" element={<>Not found</>} />
+            </Route>
           </Routes>
         </div>
       </BrowserRouter>
@@ -164,4 +54,18 @@ function App() {
   );
 }
 
+const DefaultRouteLayout = () => {
+  return (
+    <div className="main-other">
+      <Outlet />
+    </div>
+  );
+};
+const OverlayLayout = () => {
+  return (
+    <div className="main-overlay">
+      <Outlet />
+    </div>
+  );
+};
 export default App;
