86a858743f7dc997d8b63c240f448835d219cc02
Darosss <daroing12@gmail.com>
2023-02-28 19:09:31 +0100

    feat: add user messages and redemptions

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,5 +1,11 @@
 import Express, { NextFunction, Request, Response } from "express";
-import { IRequestQueryUser } from "@types";
+import {
+  IRequestParams,
+  IRequestQuery,
+  IRequestQueryMessage,
+  IRequestQueryUser,
+  IRequestRedemptionQuery,
+} from "@types";
 import { filterUsersByUrlParams } from "./filters/usersFilter";
 import {
   getUserById,
@@ -7,6 +13,12 @@ import {
   getUsers,
   updateUserById,
 } from "@services/users";
+
+import { filterMessagesByUrlParams } from "./filters/messagesFilter";
+import { getMessages, getMessagesCount } from "@services/messages";
+
+import { getRedemptions, getRedemptionsCount } from "@services/redemptions";
+import { filterRedemptionsByUrlParams } from "./filters/redemptionsFilter";
 
 const getUsersList = async (
   req: Request<{}, {}, {}, IRequestQueryUser>,
@@ -63,6 +75,106 @@ const editUserProfile = async (
   try {
     await updateUserById(id, { notes: notes });
     return res.status(200).send({ message: "Updated successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUserMessages = async (
+  req: Request<IRequestParams, {}, {}, IRequestQueryMessage>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const { page = 1, limit = 50 } = req.query;
+
+  const searchFilter = Object.assign(
+    { owner: id },
+    await filterMessagesByUrlParams(req.query)
+  );
+
+  try {
+    const messages = await getMessages(searchFilter, {
+      limit: limit,
+      skip: page,
+      sort: { date: -1 },
+      select: { __v: 0 },
+    });
+    const count = await getMessagesCount(searchFilter);
+
+    return res.status(200).send({
+      data: messages,
+      totalPages: Math.ceil(count / limit),
+      count: count,
+      currentPage: Number(page),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUserRedemptions = async (
+  req: Request<IRequestParams, {}, {}, IRequestRedemptionQuery>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const { page = 1, limit = 50 } = req.query;
+
+  const searchFilter = Object.assign(
+    { userId: id },
+    filterRedemptionsByUrlParams(req.query)
+  );
+
+  try {
+    const redemptions = await getRedemptions(searchFilter, {
+      limit: limit,
+      skip: page,
+      sort: { redemptionDate: -1 },
+    });
+
+    const count = await getRedemptionsCount(searchFilter);
+
+    return res.status(200).send({
+      data: redemptions,
+      totalPages: Math.ceil(count / limit),
+      count: count,
+      currentPage: Number(page),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getLatestEldestUserMessages = async (
+  req: Request<IRequestParams, {}, {}, IRequestQuery>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const { limit = 6 } = req.query;
+
+  try {
+    const firstMessages = await getMessages(
+      { owner: id },
+      {
+        limit: limit,
+        sort: { date: 1 },
+        select: { __v: 0 },
+      }
+    );
+    const latestMessages = await getMessages(
+      { owner: id },
+      {
+        limit: limit,
+        sort: { date: -1 },
+        select: { __v: 0 },
+      }
+    );
+
+    return res.status(200).send({
+      data: { firstMessages: firstMessages, latestMessages: latestMessages },
+    });
   } catch (err) {
     next(err);
   }
