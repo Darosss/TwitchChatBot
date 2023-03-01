58e02314143675da7dc2b4b7016dd65a1e909a77
Darosss <daroing12@gmail.com>
2023-03-01 12:21:18 +0100

    feat: add session messages and redemption routes

--- frontend/src/routes/StreamSessionRoute.tsx
+++ frontend/src/routes/StreamSessionRoute.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 
 import StreamSessionDetail from "@components/streamSessionDetail";
 import StreamSessions from "@components/streamSessions";
+import MessagesList from "@components/messagesList";
+import RedemptionsList from "@components/redemptionsList";
 
 export function StreamSessionRoutes() {
   return (
@@ -9,6 +11,15 @@ export function StreamSessionRoutes() {
       <Route>
         <Route index element={<StreamSessions />} />
         <Route path=":sessionId" element={<StreamSessionDetail />} />
+        <Route
+          path=":sessionId/messages"
+          element={<MessagesList messages="session" />}
+        />
+        <Route
+          path=":sessionId/redemptions"
+          element={<RedemptionsList redemptions="session" />}
+        />
+
         <Route path="*" element={<>Not found</>} />
       </Route>
     </Routes>
