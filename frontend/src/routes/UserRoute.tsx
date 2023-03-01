960e3e147e88ba7783b4ec69f78abbf24a3e1e2d
Darosss <daroing12@gmail.com>
2023-03-01 12:21:42 +0100

    feat: add user messages and redemptions routes

--- frontend/src/routes/UserRoute.tsx
+++ frontend/src/routes/UserRoute.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Users from "@components/users";
 import UserProfile from "@components/userProfile";
+import MessagesList from "@components/messagesList";
+import RedemptionsList from "@components/redemptionsList";
 
 export function UserRoutes() {
   return (
@@ -8,6 +10,14 @@ export function UserRoutes() {
       <Route>
         <Route index element={<Users />} />
         <Route path=":userId" element={<UserProfile />} />
+        <Route
+          path=":userId/messages"
+          element={<MessagesList messages="user" />}
+        />
+        <Route
+          path=":userId/redemptions"
+          element={<RedemptionsList redemptions="user" />}
+        />
         <Route path="*" element={<>Not found</>} />
       </Route>
     </Routes>
