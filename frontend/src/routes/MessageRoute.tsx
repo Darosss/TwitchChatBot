f7f312de35af565a7f8b7ed67b85eef7e8895577
Darosss <daroing12@gmail.com>
2023-03-01 12:20:55 +0100

    refactor: move user and session messages to other routes

--- frontend/src/routes/MessageRoute.tsx
+++ frontend/src/routes/MessageRoute.tsx
@@ -6,11 +6,7 @@ export function MessageRoutes() {
     <Routes>
       <Route>
         <Route index element={<MessagesList messages="all" />} />
-        <Route path=":userId" element={<MessagesList messages="user" />} />
-        <Route
-          path="stream-session/:sessionId"
-          element={<MessagesList messages="session" />}
-        />
+
         <Route path="*" element={<>Not found</>} />
       </Route>
     </Routes>
