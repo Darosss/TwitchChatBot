5ba0321f4a5ee4f20b60526b00160cd18da6bb05
Darosss <daroing12@gmail.com>
2023-03-01 12:23:07 +0100

    feat: move session and user routes to its own files

--- frontend/src/services/MessageService.ts
+++ frontend/src/services/MessageService.ts
@@ -8,40 +8,9 @@ export interface IMessage {
   owner: IUser;
   ownerUsername: string;
 }
-interface FirstAndLatestMsgs {
-  firstMessages: IMessage[];
-  latestMessages: IMessage[];
-}
-const getMessages = (
-  messages: "all" | "session" | "user",
-  sessionId: string | undefined,
-  userId: string | undefined
-) => {
-  const baseUrl = "/messages";
-  switch (messages) {
-    case "session":
-      return useAxiosCustom<IPagination<IMessage>>({
-        url: `${baseUrl}/stream-session/${sessionId}`,
-      });
-      break;
 
-    case "user":
-      return useAxiosCustom<IPagination<IMessage>>({
-        url: `${baseUrl}/${userId}`,
-      });
-      break;
-
-    default:
-      return useAxiosCustom<IPagination<IMessage>>({
-        url: baseUrl,
-      });
-  }
-};
-
-const getLatestAndFirstMsgs = (userId: string) => {
-  return useAxiosCustom<{ data: FirstAndLatestMsgs }>({
-    url: `/messages/${userId}/latest-first-msgs`,
+export const getMessages = () => {
+  return useAxiosCustom<IPagination<IMessage>>({
+    url: `/messages`,
   });
 };
-
-export default { getMessages, getLatestAndFirstMsgs };
