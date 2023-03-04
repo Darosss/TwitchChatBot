6e6d1e8a60f37aebb9c38d76d19e2241a23afa24
Darosss <daroing12@gmail.com>
2023-03-04 14:18:54 +0100

    feat(defaults): add configs defaults

---
+++ server/src/defaults/configsDefaults.ts
@@ -0,0 +1,22 @@
+import { IConfigDefaults } from "@models/types";
+
+export const configDefaults: IConfigDefaults = {
+  commandsPrefix: "--",
+  timersIntervalDelay: 20,
+  activeUserTimeDelay: 150,
+  chatGamesIntervalDelay: 20,
+  minActiveUsersThreshold: 3,
+  intervalCheckChatters: 300,
+  pointsIncrement: {
+    message: 1,
+    watch: 10,
+    watchMultipler: 2.0,
+  },
+  intervalCheckViewersPeek: 600,
+  permissionLevels: {
+    broadcaster: 10,
+    mod: 8,
+    vip: 4,
+    all: 0,
+  },
+};
