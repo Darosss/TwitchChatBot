b96c312e4ce513694b7d8ab066241e2cf73eb44e
Darosss <daroing12@gmail.com>
2023-03-04 14:28:52 +0100

    feat(model): add default Maps

--- server/src/models/streamSessionModel.ts
+++ server/src/models/streamSessionModel.ts
@@ -2,27 +2,32 @@ import { Model, model, Schema } from "mongoose";
 import { IStreamSessionDocument } from "./types";
 
 const StreamSessionSchema: Schema<IStreamSessionDocument> = new Schema({
-  sessionStart: { type: Date, required: true, default: new Date() },
+  sessionStart: { type: Date, required: true, default: Date.now },
   sessionEnd: { type: Date },
   sessionTitles: {
     type: Map,
     of: String,
+    default: new Map(),
   },
   categories: {
     type: Map,
     of: String,
+    default: new Map(),
   },
   tags: {
     type: Map,
     of: String,
+    default: new Map(),
   },
   viewers: {
     type: Map,
     of: Number,
+    default: new Map(),
   },
   watchers: {
     type: Map,
     of: Number,
+    default: new Map(),
   },
 });
 
