348a6c06fa99e25effb18c8995923e7c590a7242
Darosss <daroing12@gmail.com>
2023-03-06 18:19:54 +0100

    feat(model): add watchTime to user model

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -12,6 +12,7 @@ const UserSchema: Schema<IUserDocument> = new Schema(
     privileges: { type: Number, default: 0 },
     lastSeen: { type: Date, default: Date.now },
     points: { type: Number, default: 0 },
+    watchTime: { type: Number, default: 0 },
     messageCount: { type: Number, default: 0 },
   },
   { timestamps: true }
