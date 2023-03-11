5de987bb1f39b9bf4dc31d77574e5c7f4361f2ac
Darosss <daroing12@gmail.com>
2023-03-11 16:47:12 +0100

    feat: add layout grid system instead of own system

--- frontend/src/components/streamEvents/StreamEvents.tsx
+++ frontend/src/components/streamEvents/StreamEvents.tsx
@@ -1,131 +1,93 @@
 import "./style.css";
-import React, { useReducer } from "react";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import React, { useState } from "react";
+import ReactGridLayout, { Responsive, WidthProvider } from "react-grid-layout";
 
-import HiddenMenu from "@components/hiddenMenu";
-import WidgetWrapper from "@components/widgetWrapper";
-import useLocalStorage from "@hooks/useLocalStorageHook";
 import StreamChat from "./streamChat";
 import StreamChatters from "./streamChatters";
 import StreamNotifications from "./streamNotifications";
 import StreamStatistics from "./streamStatistics";
+import DrawerBar from "@components/drawer/DrawerBar";
 
-interface IWidget {
-  enabled: boolean;
-  size?: { width: string; height: string }; // TODO: add size later
-}
-
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
 export default function StreamEvents() {
-  const [, forceUpdate] = useReducer((x) => x + 1, 0);
-  const [chatWidget, setChatWidget] = useLocalStorage<IWidget>(
-    "stream-chat-widget-data",
-    { enabled: true }
-  );
-  const [chattersWidget, setChattersWidget] = useLocalStorage<IWidget>(
-    "stream-chatters-widget-data",
-    { enabled: true }
-  );
-  const [notificationsWidget, setNotificationsWidget] =
-    useLocalStorage<IWidget>("stream-notifications-widget-data", {
-      enabled: true,
-    });
-  const [statisticSession, setStatisticSession] = useLocalStorage<IWidget>(
-    "stream-statistic-widget-data",
-    {
-      enabled: true,
-    }
-  );
+  const [isEdit, setIsEdit] = useState(false);
+  const [layoutWidgets, setLayoutWidgets] = useState<ReactGridLayout.Layouts>({
+    lg: [
+      { i: "stream-chat", x: 0, y: 0, w: 3, h: 12, static: true },
+      { i: "stream-chatters", x: 3, y: 0, w: 2, h: 12, static: true },
+      { i: "stream-notifications", x: 0, y: 12, w: 4, h: 12, static: true },
+      { i: "stream-statistics", x: 11, y: 0, w: 7, h: 12, static: true },
+    ],
+  });
+
+  function toggleStaticMode() {
+    setIsEdit(!isEdit);
+    setLayoutWidgets((prevLayout) => ({
+      ...prevLayout,
+      lg: prevLayout.lg.map((item) => ({
+        ...item,
+        static: !item.static,
+      })),
+    }));
+  }
+
+  const onLayoutChange = (
+    layouts: ReactGridLayout.Layout[],
+    layout: ReactGridLayout.Layouts
+  ) => {
+    setLayoutWidgets(layout);
+  };
+
   return (
-    <div className="stream-wrapper">
-      <div className="menu-widgets">
-        <HiddenMenu>
-          <li>
-            <button
-              className={`stream-btn ${
-                chatWidget.enabled ? "active" : "not-active"
-              }`}
-              onClick={() => {
-                setChatWidget((prevLocalStorage) => {
-                  prevLocalStorage.enabled = !prevLocalStorage.enabled;
-                  return prevLocalStorage;
-                });
-                forceUpdate();
-              }}
-            >
-              Toggle chat
-            </button>
-          </li>
-          <li>
-            <button
-              className={`stream-btn ${
-                chattersWidget.enabled ? "active" : "not-active"
-              }`}
-              onClick={() => {
-                setChattersWidget((prevLocalStorage) => {
-                  prevLocalStorage.enabled = !prevLocalStorage.enabled;
-                  return prevLocalStorage;
-                });
-                forceUpdate();
-              }}
-            >
-              Toggle chatters
-            </button>
-          </li>
-          <li>
-            <button
-              className={`stream-btn ${
-                notificationsWidget.enabled ? "active" : "not-active"
-              }`}
-              onClick={() => {
-                setNotificationsWidget((prevLocalStorage) => {
-                  prevLocalStorage.enabled = !prevLocalStorage.enabled;
-                  return prevLocalStorage;
-                });
-                forceUpdate();
-              }}
-            >
-              Toggle notifications
-            </button>
-          </li>
-          <li>
-            <button
-              className={`stream-btn ${
-                statisticSession.enabled ? "active" : "not-active"
-              }`}
-              onClick={() => {
-                setStatisticSession((prevLocalStorage) => {
-                  prevLocalStorage.enabled = !prevLocalStorage.enabled;
-                  return prevLocalStorage;
-                });
-                forceUpdate();
-              }}
-            >
-              Toggle statistics
-            </button>
-          </li>
-        </HiddenMenu>
-      </div>
-
-      {chatWidget?.enabled ? (
-        <WidgetWrapper id="stream-window">
-          <StreamChat className="stream-window" />
-        </WidgetWrapper>
-      ) : null}
-      {chattersWidget?.enabled ? (
-        <WidgetWrapper id="stream-last-chatters">
-          <StreamChatters className="stream-window" />
-        </WidgetWrapper>
-      ) : null}
-      {notificationsWidget?.enabled ? (
-        <WidgetWrapper id="stream-notifications">
-          <StreamNotifications className="stream-window" />
-        </WidgetWrapper>
-      ) : null}
-
-      {statisticSession?.enabled ? (
-        <WidgetWrapper id="stream-statistics" horizontal={true}>
-          <StreamStatistics className="stream-window" />
-        </WidgetWrapper>
-      ) : null}
+    <div>
+      <DrawerBar direction={"top"} size={60} showBtnText="&#8595;">
+        <div className="widget-menu-drawer">
+          <div>
+            <button onClick={toggleStaticMode}>Toggle Edit</button>
+          </div>
+          <div>
+            Is edit:
+            <span style={{ color: !isEdit ? "red" : "green" }}>
+              {isEdit.toString()}
+            </span>
+          </div>
+          <div>
+            <button>Save</button>
+          </div>
+        </div>
+      </DrawerBar>
+      <ResponsiveReactGridLayout
+        onLayoutChange={onLayoutChange}
+        compactType={null}
+        layouts={layoutWidgets}
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        preventCollision={false}
+        rowHeight={30}
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        autoSize={true}
+        margin={{
+          lg: [10, 10],
+          md: [10, 10],
+          sm: [10, 10],
+          xs: [10, 10],
+          xxs: [10, 10],
+        }}
+      >
+        <div key="stream-chat">
+          <StreamChat />
+        </div>
+        <div key="stream-chatters">
+          <StreamChatters />
+        </div>
+        <div key="stream-notifications">
+          <StreamNotifications />
+        </div>
+        <div key="stream-statistics">
+          <StreamStatistics />
+        </div>
+      </ResponsiveReactGridLayout>
     </div>
   );
 }
