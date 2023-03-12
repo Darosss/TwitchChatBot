340033a5e1a1b2217624a688ece349fe377e5d70
Darosss <daroing12@gmail.com>
2023-03-12 18:37:51 +0100

    fix: add required props to StreamEvents

--- frontend/src/routes/EventsRoute.tsx
+++ frontend/src/routes/EventsRoute.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import StreamEvents from "@components/streamEvents";
+import { initialLayoutWidgets } from "src/layout/initialLayoutWidgets";
 
 export function EventRoutes() {
   return (
     <Routes>
       <Route>
-        <Route index element={<StreamEvents />} />
+        <Route
+          index
+          element={<StreamEvents initialLayouts={initialLayoutWidgets} />}
+        />
         <Route path="*" element={<>Not found</>} />
       </Route>
     </Routes>
