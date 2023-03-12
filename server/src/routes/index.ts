327f481989d4620bd464b96b74d94bf0b38401da
Darosss <daroing12@gmail.com>
2023-03-13 00:39:51 +0100

    feat: add message-categories route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,12 +6,14 @@ import redemptionsRouter from "./redemptionsRoute";
 import triggersRouter from "./triggersRoute";
 import usersRouter from "./usersRoute";
 import streamSessionsRouter from "./streamSessionsRoute";
+import messageCategoriesRouter from "./messageCategoriesRoute";
 
 const initRoutes = (app: any) => {
   app.use("/auth", auth);
   app.use("/chat-commands", chatCommandsRouter);
   app.use("/configs", configsRouter);
   app.use("/messages", messagesRouter);
+  app.use("/message-categories", messageCategoriesRouter);
   app.use("/users", usersRouter);
   app.use("/redemptions", redemptionsRouter);
   app.use("/stream-sessions", streamSessionsRouter);
