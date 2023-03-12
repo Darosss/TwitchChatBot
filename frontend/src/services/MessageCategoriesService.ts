5d109d88b6dd0daf1209175b82872f7b4040c211
Darosss <daroing12@gmail.com>
2023-03-13 00:44:25 +0100

    feat: add MessageCategoriesService

---
+++ frontend/src/services/MessageCategoriesService.ts
@@ -0,0 +1,49 @@
+import useAxiosCustom, { IPagination } from "./ApiService";
+
+export interface IMessageCategory {
+  _id: string;
+  category: string;
+  messages: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const getMessageCategories = () => {
+  return useAxiosCustom<IPagination<IMessageCategory>>({
+    url: `/message-categories`,
+  });
+};
+
+export const editMessageCategoryById = (
+  id: string,
+  data: Partial<IMessageCategory>
+) => {
+  return useAxiosCustom<IMessageCategory>({
+    url: `/message-categories/${id}`,
+    method: "POST",
+    bodyData: data,
+    manual: true,
+    urlParams: false,
+  });
+};
+
+export const createMessageCategory = (
+  data: Pick<IMessageCategory, "category" | "messages">
+) => {
+  return useAxiosCustom<IMessageCategory>({
+    url: `/message-categories/create`,
+    method: "POST",
+    bodyData: data,
+    manual: true,
+    urlParams: false,
+  });
+};
+
+export const deleteMessageCategoryById = (id: string) => {
+  return useAxiosCustom<any>({
+    url: `/message-categories/delete/${id}`,
+    method: "DELETE",
+    manual: true,
+    urlParams: false,
+  });
+};
