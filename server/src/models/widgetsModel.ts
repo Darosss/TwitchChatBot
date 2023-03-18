ba081b14ac9a31fcf86b8a55abe0e5f3e9499da1
Darosss <daroing12@gmail.com>
2023-03-18 21:55:30 +0100

    feat: use Map instead of Object in layout

--- server/src/models/widgetsModel.ts
+++ server/src/models/widgetsModel.ts
@@ -1,10 +1,23 @@
 import { Model, model, Schema } from "mongoose";
 import { IWidgetsDocument } from "./types";
 
+const layoutBreakpointSchema = new Schema({
+  i: String,
+  x: Number,
+  y: Number,
+  w: Number,
+  h: Number,
+  static: Boolean,
+});
+
 const WidgetsModel: Schema<IWidgetsDocument> = new Schema(
   {
     name: { type: String, required: true },
-    layout: Object,
+    layout: {
+      type: Map,
+      of: [layoutBreakpointSchema],
+      default: new Map(),
+    },
   },
   { timestamps: true }
 );
