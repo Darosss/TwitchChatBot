0af1d1cc3df2f1f7da67db76e10b52a9090110ba
Darosss <daroing12@gmail.com>
2023-03-19 11:08:20 +0100

    feat(component): add stream events list layouts

---
+++ frontend/src/components/streamEvents/streamEventsLayouts/StreamEventsLayouts.tsx
@@ -0,0 +1,54 @@
+import "./style.css";
+import React, { useState } from "react";
+import { createLayout, getWidgets } from "@services/WidgetsService";
+import { Link } from "react-router-dom";
+import { initialLayoutWidgets } from "src/layout/initialLayoutWidgets";
+
+export default function StreamNotifications() {
+  const { data, loading, error, refetchData } = getWidgets();
+
+  const [layoutName, setLayoutName] = useState<string>("");
+
+  const { refetchData: fetchCreateLayout } = createLayout({
+    name: layoutName,
+    layout: initialLayoutWidgets,
+  });
+
+  if (error) return <>There is an error. {error.response?.data.message}</>;
+  if (!data || loading) return <>Loading!</>;
+
+  const { data: layouts } = data;
+
+  const createNewLayout = () => {
+    fetchCreateLayout().then(() => refetchData());
+  };
+
+  return (
+    <>
+      <div className="events-layouts-header">Stream events layouts</div>
+      <div className="events-layouts-list">
+        <div className="events-layouts-btn">
+          <input
+            type="text"
+            placeholder="Name"
+            defaultValue={layoutName}
+            onChange={(e) => setLayoutName(e.target.value)}
+          ></input>
+          <button
+            onClick={() => createNewLayout()}
+            className="common-button primary-button"
+          >
+            Create
+          </button>
+        </div>
+        {layouts.map((layout, index) => {
+          return (
+            <div key={index}>
+              <Link to={`${layout._id}`}>{layout.name}</Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
