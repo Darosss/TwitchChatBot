4214bc04d68d02d70625458874c7bf5d890d1d51
Darosss <daroing12@gmail.com>
2023-03-25 22:56:36 +0100

    feat(model) add timer model

---
+++ server/src/models/timerModel.ts
@@ -0,0 +1,19 @@
+import { Model, model, Schema } from "mongoose";
+import { TimerDocument } from "./types";
+
+const TimerSchema: Schema<TimerDocument> = new Schema(
+  {
+    name: { type: String, required: true },
+    enabled: { type: Boolean, default: true },
+    points: { type: Number, default: 0 },
+    reqPoints: { type: Number, default: 20 },
+    nonFollowMulti: { type: Boolean, default: false },
+    nonSubMulti: { type: Boolean, default: false },
+    uses: { type: Number, default: 0 },
+    description: { type: String },
+    messages: [String],
+  },
+  { timestamps: true }
+);
+
+export const Timer: Model<TimerDocument> = model("Timers", TimerSchema);
