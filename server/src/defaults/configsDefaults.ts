50dc1af594aca9b46b5fbfeeac24b6cefc8fa726
Darosss <daroing12@gmail.com>
2023-03-26 12:54:29 +0200

    feat: add new configs to defaults

--- server/src/defaults/configsDefaults.ts
+++ server/src/defaults/configsDefaults.ts
@@ -1,23 +1,37 @@
 import { ConfigDefaults } from "./types";
 
 export const configDefaults: ConfigDefaults = {
-  commandsPrefix: "--",
-  timersIntervalDelay: 20,
-  activeUserTimeDelay: 150,
-  chatGamesIntervalDelay: 20,
-  minActiveUsersThreshold: 3,
-  intervalCheckChatters: 300,
-  pointsIncrement: {
-    message: 1,
-    watch: 10,
-    watchMultipler: 2.0,
+  commandsConfigs: {
+    commandsPrefix: "--",
   },
-  randomMessageChance: 10,
-  intervalCheckViewersPeek: 600,
-  permissionLevels: {
-    broadcaster: 10,
-    mod: 8,
-    vip: 4,
-    all: 0,
+  timersConfigs: {
+    timersIntervalDelay: 20,
+  },
+  chatGamesConfigs: {
+    activeUserTimeDelay: 150,
+    chatGamesIntervalDelay: 20,
+    minActiveUsersThreshold: 3,
+  },
+  triggersConfigs: {
+    randomMessageChance: 10,
+  },
+  pointsConfigs: {
+    pointsIncrement: {
+      message: 1,
+      watch: 10,
+      watchMultipler: 2.0,
+    },
+  },
+  loyaltyConfigs: {
+    intervalCheckChatters: 300,
+  },
+  headConfigs: {
+    intervalCheckViewersPeek: 600,
+    permissionLevels: {
+      broadcaster: 10,
+      mod: 8,
+      vip: 4,
+      all: 0,
+    },
   },
 };
