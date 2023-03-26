b90c169be45ddc326f9db0c26d82c04507f46592
Darosss <daroing12@gmail.com>
2023-03-26 12:52:45 +0200

    feat: add usage of new configs from backend\

--- frontend/src/services/ConfigService.ts
+++ frontend/src/services/ConfigService.ts
@@ -1,26 +1,54 @@
 import useAxiosCustom from "./ApiService";
 
-export interface Config {
-  _id: string;
-  commandsPrefix: string;
+export interface TimersConfigs {
   timersIntervalDelay: number;
+}
+
+export interface CommandsConfigs {
+  commandsPrefix: string;
+}
+
+export interface ChatGamesConfigs {
   activeUserTimeDelay: number;
   chatGamesIntervalDelay: number;
   minActiveUsersThreshold: number;
-  intervalCheckChatters: number;
+}
+
+export interface TriggersConfigs {
   randomMessageChance: number;
+}
+
+export interface PointsConfigs {
   pointsIncrement: {
     message: number;
     watch: number;
     watchMultipler: number;
   };
-  intervalCheckViewersPeek: number;
+}
+
+export interface LoyaltyConfigs {
+  intervalCheckChatters: number;
+}
+
+export interface HeadConfigs {
   permissionLevels: {
     broadcaster: number;
     mod: number;
     vip: number;
     all: number;
   };
+  intervalCheckViewersPeek: number;
+}
+
+export interface Config {
+  _id: string;
+  commandsConfigs: CommandsConfigs;
+  timersConfigs: TimersConfigs;
+  chatGamesConfigs: ChatGamesConfigs;
+  triggersConfigs: TriggersConfigs;
+  pointsConfigs: PointsConfigs;
+  loyaltyConfigs: LoyaltyConfigs;
+  headConfigs: HeadConfigs;
   createdAt: Date;
   updatedAt: Date;
 }
