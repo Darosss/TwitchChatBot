731b60f9a3b8c856b79a874ed6b2aae895968d26
Darosss <daroing12@gmail.com>
2023-03-27 13:38:25 +0200

    feat(route): add moods router

---
+++ server/src/routes/moodsRoute.ts
@@ -0,0 +1,21 @@
+import isParamObjectId from "@middlewares/isParamObjectIdMiddleware";
+import Express, { Router } from "express";
+
+import {
+  getMoodsList,
+  addNewMood,
+  // getMoodById,
+  editMoodById,
+  deleteMood,
+} from "@controllers/moodsController";
+import checkSearchParams from "@middlewares/checkSearchParamsMiddleware";
+
+const moodsRouter = Router();
+
+moodsRouter.get("/", checkSearchParams, getMoodsList);
+moodsRouter.post("/create", addNewMood);
+// moodsRouter.get("/:id", isParamObjectId, getMoodById);
+moodsRouter.post("/:id", isParamObjectId, editMoodById);
+moodsRouter.delete("/delete/:id", isParamObjectId, deleteMood);
+
+export default moodsRouter;
