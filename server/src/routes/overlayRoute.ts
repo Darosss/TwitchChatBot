949221b05c8b44c8bc6e9e932d2c940e7839e85f
Darosss <daroing12@gmail.com>
2023-03-31 16:34:22 +0200

    feat(route): add path to getOverlayById

--- server/src/routes/overlayRoute.ts
+++ server/src/routes/overlayRoute.ts
@@ -4,11 +4,13 @@ import {
   addNewOverlay,
   editOverlayById,
   removeOverlayById,
+  getOverlayById,
 } from "@controllers/overlaysController";
 const overlayRouter = Router();
 
 overlayRouter.get("/", getOverlaysList);
 overlayRouter.post("/create", addNewOverlay);
+overlayRouter.get("/:id", getOverlayById);
 overlayRouter.post("/:id", editOverlayById);
 overlayRouter.delete("/delete/:id", removeOverlayById);
 
