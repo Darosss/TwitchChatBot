e36da2713b48bf52ddd5d5c9120cdd95a77abc4f
Darosss <daroing12@gmail.com>
2023-03-31 16:31:30 +0200

    feat: add OverlaysList and Overlay routes

--- frontend/src/routes/OverlayRoute.tsx
+++ frontend/src/routes/OverlayRoute.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Overlay from "@components/overlay";
+import OverlaysList from "@components/overlay/overlaysList";
 
 export function OverlayRoutes() {
   return (
     <Routes>
       <Route>
-        <Route index element={<Overlay />} />
+        <Route index element={<OverlaysList />} />
+        <Route path=":overlayId" element={<Overlay />} />
 
         <Route path="*" element={<>Not found</>} />
       </Route>
