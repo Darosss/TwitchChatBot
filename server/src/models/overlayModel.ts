784dd3371289546fe3784392d27d2daa544de977
Darosss <daroing12@gmail.com>
2023-03-31 16:33:50 +0200

    feat(model): add overlayModel

---
+++ server/src/models/overlayModel.ts
@@ -0,0 +1,31 @@
+import { Model, model, Schema } from "mongoose";
+import { OverlayDocument } from "./types";
+import { nameField } from "@utils/commonSchemaFieldsUtil";
+
+const layoutBreakpointSchema = new Schema({
+  i: String,
+  x: Number,
+  y: Number,
+  w: Number,
+  h: Number,
+  static: Boolean,
+});
+
+const OverlaySchema: Schema<OverlayDocument> = new Schema(
+  {
+    ...nameField,
+    layout: {
+      type: Map,
+      of: [layoutBreakpointSchema],
+      default: new Map(),
+    },
+    toolbox: {
+      type: Map,
+      of: [layoutBreakpointSchema],
+      default: new Map(),
+    },
+  },
+  { timestamps: true }
+);
+
+export const Overlay: Model<OverlayDocument> = model("Overlays", OverlaySchema);
