b0ffa701d158b0857481f630523597512f487c3e
Darosss <daroing12@gmail.com>
2023-03-31 16:33:02 +0200

    feat(controller): add overlaysController

---
+++ server/src/controllers/overlaysController.ts
@@ -0,0 +1,118 @@
+import Express, { NextFunction, Request, Response } from "express";
+import {
+  createOverlay,
+  deleteOverlayById,
+  getOverlayById as getOverlayByIdService,
+  getOverlays,
+  getOverlaysCount,
+  updateOverlayById,
+} from "@services/overlays";
+import { RequestParams, RequestSearch } from "@types";
+import { OverlayCreateData, OverlayUpdateData } from "@services/overlays/types";
+
+export const getOverlaysList = async (
+  req: Request<{}, {}, {}, RequestSearch>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { page = 1, limit = 25 } = req.query;
+
+  const searchFilter = {}; // filterCommandsByUrlParams(req.query);
+  try {
+    const overlays = await getOverlays(searchFilter, {
+      limit: limit,
+      skip: page,
+      sort: { createdAt: -1 },
+    });
+
+    const count = await getOverlaysCount(searchFilter);
+    return res.status(200).send({
+      data: overlays,
+      totalPages: Math.ceil(count / limit),
+      count: count,
+      currentPage: Number(page),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getOverlayById = async (
+  req: Request<RequestParams, {}, {}, {}>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const overlay = await getOverlayByIdService(id, {});
+
+    return res.status(200).send({
+      data: overlay,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const addNewOverlay = async (
+  req: Request<RequestParams, {}, OverlayCreateData, {}>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, layout, toolbox } = req.body;
+
+  try {
+    const newOverlay = await createOverlay({
+      name: name,
+      layout: layout,
+      toolbox: toolbox,
+    });
+
+    return res
+      .status(201)
+      .send({ message: "Added successfully", overlay: newOverlay });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const editOverlayById = async (
+  req: Request<RequestParams, {}, OverlayUpdateData, {}>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const { name, layout, toolbox } = req.body;
+
+  try {
+    const updatedOverlay = await updateOverlayById(id, {
+      name: name,
+      layout: layout,
+      toolbox: toolbox,
+    });
+
+    return res.status(200).send({
+      message: "Updated successfully",
+      overlay: updatedOverlay,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const removeOverlayById = async (
+  req: Request<RequestParams, {}, {}, {}>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const deletedOverlay = await deleteOverlayById(id);
+
+    return res.status(200).send({ message: "Overlay deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
