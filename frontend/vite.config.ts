6ab854eccda098bc80142d18cf7d5e83e4b2a5c4
Darosss <daroing12@gmail.com>
2023-04-03 16:29:46 +0200

    chore(config): add global variables for sass

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,13 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "./src/_variables.scss" as *;`,
+      },
+    },
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   envDir: "../",
 });
