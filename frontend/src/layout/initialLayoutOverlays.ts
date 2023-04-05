710cb056afe8c2d1afecbd67870771e84de8e84e
Darosss <daroing12@gmail.com>
2023-04-05 16:33:56 +0200

    feat: add music-player to layout

--- frontend/src/layout/initialLayoutOverlays.ts
+++ frontend/src/layout/initialLayoutOverlays.ts
@@ -1,12 +1,30 @@
 import ReactGridLayout from "react-grid-layout";
 
 export const initialLayoutOverlays: ReactGridLayout.Layouts = {
-  ulg: [{ i: "overlay-redemptions", x: 0, y: 0, w: 6, h: 5, static: true }],
-  lg: [{ i: "overlay-redemptions", x: 0, y: 0, w: 3, h: 5, static: true }],
-  md: [{ i: "overlay-redemptions", x: 0, y: 0, w: 2, h: 10, static: true }],
-  sm: [{ i: "overlay-redemptions", x: 0, y: 0, w: 2, h: 8, static: true }],
-  xs: [{ i: "overlay-redemptions", x: 0, y: 0, w: 2, h: 6, static: true }],
-  xxs: [{ i: "overlay-redemptions", x: 0, y: 0, w: 3, h: 5, static: true }],
+  ulg: [
+    { i: "overlay-redemptions", x: 0, y: 0, w: 6, h: 5, static: true },
+    { i: "overlay-music-player", x: 6, y: 0, w: 6, h: 5, static: true },
+  ],
+  lg: [
+    { i: "overlay-redemptions", x: 0, y: 0, w: 3, h: 5, static: true },
+    { i: "overlay-music-player", x: 6, y: 0, w: 6, h: 5, static: true },
+  ],
+  md: [
+    { i: "overlay-redemptions", x: 0, y: 0, w: 2, h: 10, static: true },
+    { i: "overlay-music-player", x: 6, y: 0, w: 6, h: 5, static: true },
+  ],
+  sm: [
+    { i: "overlay-redemptions", x: 0, y: 0, w: 2, h: 8, static: true },
+    { i: "overlay-music-player", x: 6, y: 0, w: 6, h: 5, static: true },
+  ],
+  xs: [
+    { i: "overlay-redemptions", x: 0, y: 0, w: 2, h: 6, static: true },
+    { i: "overlay-music-player", x: 6, y: 0, w: 6, h: 5, static: true },
+  ],
+  xxs: [
+    { i: "overlay-redemptions", x: 0, y: 0, w: 3, h: 5, static: true },
+    { i: "overlay-music-player", x: 6, y: 0, w: 6, h: 5, static: true },
+  ],
 };
 
 export const initialToolboxOverlays: ReactGridLayout.Layouts = {
--- frontend/src/layout/initialLayoutWidgets.ts
+++ frontend/src/layout/initialLayoutWidgets.ts
@@ -8,14 +8,16 @@ export const initialLayoutWidgets: ReactGridLayout.Layouts = {
     { i: "stream-statistics", x: 11, y: 0, w: 7, h: 15, static: true },
     { i: "messages-window", x: 4, y: 0, w: 2, h: 10, static: true },
     { i: "stream-modes", x: 6, y: 0, w: 3, h: 10, static: true },
+    { i: "music-player", x: 10, y: 0, w: 2, h: 10, static: true },
   ],
   lg: [
     { i: "stream-chat", x: 0, y: 0, w: 3, h: 12, static: true },
     { i: "stream-chatters", x: 0, y: 12, w: 3, h: 10, static: true },
-    { i: "stream-notifications", x: 8, y: 0, w: 4, h: 10, static: true },
+    { i: "stream-notifications", x: 8, y: 0, w: 2, h: 10, static: true },
     { i: "stream-statistics", x: 4, y: 10, w: 8, h: 12, static: true },
     { i: "messages-window", x: 3, y: 0, w: 2, h: 10, static: true },
     { i: "stream-modes", x: 5, y: 0, w: 3, h: 10, static: true },
+    { i: "music-player", x: 10, y: 0, w: 2, h: 10, static: true },
   ],
   md: [
     { i: "stream-chat", x: 0, y: 0, w: 2, h: 10, static: true },
@@ -24,6 +26,7 @@ export const initialLayoutWidgets: ReactGridLayout.Layouts = {
     { i: "stream-statistics", x: 0, y: 10, w: 6, h: 8, static: true },
     { i: "messages-window", x: 4, y: 0, w: 2, h: 10, static: true },
     { i: "stream-modes", x: 2, y: 0, w: 2, h: 10, static: true },
+    { i: "music-player", x: 6, y: 0, w: 3, h: 10, static: true },
   ],
   sm: [
     { i: "stream-chat", x: 0, y: 0, w: 2, h: 8, static: true },
@@ -32,6 +35,7 @@ export const initialLayoutWidgets: ReactGridLayout.Layouts = {
     { i: "stream-statistics", x: 0, y: 8, w: 2, h: 8, static: true },
     { i: "messages-window", x: 3, y: 0, w: 1, h: 8, static: true },
     { i: "stream-modes", x: 6, y: 0, w: 2, h: 8, static: true },
+    { i: "music-player", x: 6, y: 0, w: 3, h: 10, static: true },
   ],
   xs: [
     { i: "stream-chat", x: 0, y: 0, w: 2, h: 6, static: true },
@@ -40,6 +44,7 @@ export const initialLayoutWidgets: ReactGridLayout.Layouts = {
     { i: "stream-statistics", x: 0, y: 12, w: 1, h: 1, static: true },
     { i: "messages-window", x: 3, y: 0, w: 2, h: 4, static: true },
     { i: "stream-modes", x: 0, y: 6, w: 2, h: 3, static: true },
+    { i: "music-player", x: 6, y: 0, w: 3, h: 10, static: true },
   ],
   xxs: [
     { i: "stream-chat", x: 0, y: 0, w: 3, h: 5, static: true },
@@ -48,6 +53,7 @@ export const initialLayoutWidgets: ReactGridLayout.Layouts = {
     { i: "stream-statistics", x: 2, y: 0, w: 7, h: 5, static: true },
     { i: "messages-window", x: 3, y: 0, w: 3, h: 4, static: true },
     { i: "stream-modes", x: 5, y: 0, w: 3, h: 10, static: true },
+    { i: "music-player", x: 6, y: 0, w: 3, h: 10, static: true },
   ],
 };
 
