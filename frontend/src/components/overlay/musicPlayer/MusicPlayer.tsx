d471a13bb0e2bc31dc0052a5f445d198322c97e4
Darosss <daroing12@gmail.com>
2023-04-05 16:34:59 +0200

    feat: add MusicPlayer component overlay

---
+++ frontend/src/components/overlay/musicPlayer/MusicPlayer.tsx
@@ -0,0 +1,94 @@
+import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
+
+import { SocketContext } from "@context/SocketContext";
+
+export default function MusicPlayer() {
+  const socket = useContext(SocketContext);
+  const [songName, setSongName] = useState("");
+  const [songDuration, setSongDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  // Memoize the AudioContext instance
+
+  const remainingTime = (currentTime: string) => {};
+
+  useEffect(() => {
+    let source: AudioBufferSourceNode | null = null;
+    let timer: NodeJS.Timer | undefined;
+
+    socket.on("audio", (data) => {
+      setSongName(data.name);
+      setSongDuration(data.duration);
+
+      clearInterval(timer);
+      let currTime = data.currentTime;
+      console.log(currTime, "Piosenka teraz gra od ");
+      timer = setInterval(() => {
+        setCurrentTime(currTime);
+        currTime++;
+        if (currTime >= data.duration) {
+          setCurrentTime(0);
+          clearInterval(timer);
+        }
+      }, 1000);
+
+      const audioCtx = new AudioContext();
+      audioCtx.decodeAudioData(data.audioBuffer, (buffer) => {
+        if (source) {
+          console.log("SOURCE ALREADY IS SO STOP ");
+          source.stop();
+        }
+
+        console.log("Create new source ");
+        source = new AudioBufferSourceNode(audioCtx, {
+          buffer: buffer,
+        });
+
+        const gainNode = audioCtx.createGain();
+        source.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
+        gainNode.gain.value = 0.12;
+
+        source.start(0, data.currentTime);
+        // const source = new AudioBufferSourceNode(memoizedAudioCtx, {
+        //   buffer: buffer,
+        // });
+
+        // const gainNode = memoizedAudioCtx?.createGain();
+        // source.connect(gainNode);
+        // gainNode.connect(memoizedAudioCtx?.destination);
+
+        // gainNode.gain.value = 0.02;
+
+        // source.start(0, data.currentTime);
+      });
+    });
+
+    socket.on("audioStop", () => {
+      clearInterval(timer);
+      if (source) {
+        console.log("SOURCE ALREADY IS SO STOP ");
+        source.stop();
+      }
+    });
+
+    return () => {
+      socket.off("audio");
+      socket.off("audioStop");
+    };
+  }, []);
+
+  return (
+    <div className="music-player-wrapper">
+      <div>
+        Name: <span>{songName}</span>
+      </div>
+      <div>
+        Duration:
+        <span>
+          {" " + currentTime} / {Math.round(songDuration)} sec
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/overlay/musicPlayer/index.ts
@@ -0,0 +1,5 @@
+import "./style.scss";
+
+import MusicPlayer from "./MusicPlayer";
+
+export default MusicPlayer;
