2ce754db64136e3466d6295d217a8ea9c41bb883
Darosss <daroing12@gmail.com>
2023-04-05 16:30:14 +0200

    feat: add usage of MusicStreamHandler
    this commit provide streaming music through socket

--- server/src/socketIO/index.ts
+++ server/src/socketIO/index.ts
@@ -6,6 +6,9 @@ import {
   SocketData,
 } from "@libs/types";
 
+import path from "node:path";
+import MusicStreamHandler from "./MusicStreamHandler";
+
 const localSocket = (httpServer: any) => {
   const io = new Server<
     ClientToServerEvents,
@@ -16,8 +19,32 @@ const localSocket = (httpServer: any) => {
     cors: { origin: "*", methods: ["GET", "POST"] },
   });
 
-  io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+  const musicPath = path.resolve(__dirname, "../data/music");
+
+  const musicStreamHandler = new MusicStreamHandler(io, musicPath);
+  musicStreamHandler.init();
+
+  io.on("connection", async (socket) => {
+    musicStreamHandler.addJoinedClientAsListener(socket.id);
+
+    socket.on("musicPause", () => {
+      musicStreamHandler.pausePlayer();
+    });
+    socket.on("musicStop", () => {});
+    socket.on("musicPlay", () => {
+      musicStreamHandler.resumePlayer();
+    });
+
+    socket.on("musicNext", () => {
+      musicStreamHandler.nextSong();
+    });
+
+    socket.on("getAudioInfo", () => {
+      const audioInfo = musicStreamHandler.getAudioInfo();
+      if (!audioInfo) return;
+
+      io.to(socket.id).emit("getAudioInfo", audioInfo);
+    });
   });
 
   return io;
