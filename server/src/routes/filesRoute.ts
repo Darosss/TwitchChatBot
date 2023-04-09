e431b08a97c82c26aea6388f8716c38b14fd3364
Darosss <daroing12@gmail.com>
2023-04-09 12:39:18 +0200

    feat(route): add filesRoute

---
+++ server/src/routes/filesRoute.ts
@@ -0,0 +1,15 @@
+import Express, { Router } from "express";
+import {
+  uploadMp3File,
+  getFoldersList,
+  getFolderMp3Files,
+  deleteMp3File,
+} from "@controllers/filesController";
+const filesRouter = Router();
+
+filesRouter.get("/folder-list", getFoldersList);
+filesRouter.get("/audio/:folder", getFolderMp3Files);
+filesRouter.post("/upload/audio-mp3", uploadMp3File);
+filesRouter.delete("/delete/audio/:folder/:fileName", deleteMp3File);
+
+export default filesRouter;
