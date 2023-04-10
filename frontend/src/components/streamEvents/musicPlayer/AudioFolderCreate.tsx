8a570f168569f13597457eb27b1a0b2ece577623
Darosss <daroing12@gmail.com>
2023-04-10 10:53:58 +0200

    feat: add AudioFolderCreate and styles

---
+++ frontend/src/components/streamEvents/musicPlayer/AudioFolderCreate.tsx
@@ -0,0 +1,72 @@
+import {
+  createAudioFolder,
+  deleteAudioFolder,
+  getFoldersList,
+} from "@services/FilesService";
+import { addNotification } from "@utils/getNotificationValues";
+import { handleActionOnChangeState } from "@utils/handleDeleteApi";
+import React, { useContext, useEffect, useState } from "react";
+
+export default function AudioFolderCreate() {
+  const [folderName, setFolderName] = useState("");
+  const [folderToDelete, setFolderToDelete] = useState<string | null>(null);
+
+  const {
+    data: foldersData,
+    loading: foldersLoad,
+    error: foldersError,
+    refetchData: refetchFolders,
+  } = getFoldersList();
+
+  const { refetchData: fetchCreateFolder } = createAudioFolder(folderName);
+
+  const { refetchData: fetchDeleteFolder } = deleteAudioFolder(
+    folderToDelete || ""
+  );
+
+  useEffect(() => {
+    handleActionOnChangeState(folderToDelete, setFolderToDelete, () => {
+      fetchDeleteFolder().then(() => {
+        refetchFolders();
+        addNotification("Deleted", "Folder deleted successfully", "danger");
+        setFolderToDelete(null);
+      });
+    });
+  }, [folderToDelete]);
+
+  const handleCreateButton = () => {
+    fetchCreateFolder();
+    refetchFolders();
+  };
+
+  return (
+    <div className="folders-create-wrapper">
+      <div>
+        <input type="text" onChange={(e) => setFolderName(e.target.value)} />
+        <button
+          className="primary-button common-button"
+          onClick={() => handleCreateButton()}
+        >
+          Create folder
+        </button>
+      </div>
+      <div className="folders-create-list">
+        {foldersData?.data.map((folder, index) => {
+          return (
+            <div key={index} className="folder-file">
+              <div>
+                <button
+                  onClick={() => setFolderToDelete(folder)}
+                  className="common-button danger-button"
+                >
+                  x
+                </button>
+              </div>
+              <div> {folder} </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
