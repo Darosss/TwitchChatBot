21f69f2263dd838af9a2de8448df8dea667f13f3
Darosss <daroing12@gmail.com>
2023-04-10 10:54:39 +0200

    feat: add deleteAudioFolder route

--- server/src/routes/filesRoute.ts
+++ server/src/routes/filesRoute.ts
@@ -4,12 +4,16 @@ import {
   getFoldersList,
   getFolderMp3Files,
   deleteMp3File,
+  createAudioFolder,
+  deleteAudioFolder,
 } from "@controllers/filesController";
 const filesRouter = Router();
 
 filesRouter.get("/folder-list", getFoldersList);
 filesRouter.get("/audio/:folder", getFolderMp3Files);
 filesRouter.post("/upload/audio-mp3/:folder", uploadMp3File);
+filesRouter.post("/create/audio/:folder", createAudioFolder);
 filesRouter.delete("/delete/audio/:folder/:fileName", deleteMp3File);
+filesRouter.delete("/delete/folder/audio/:folder", deleteAudioFolder);
 
 export default filesRouter;
