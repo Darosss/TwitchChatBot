c2dee051fd5e33183a9eaa52dbcc80cd19861555
Darosss <daroing12@gmail.com>
2023-04-12 00:17:33 +0200

    feat: add isDraggable, isResizable by default to layout

--- frontend/src/layout/initialLayoutOverlays.ts
+++ frontend/src/layout/initialLayoutOverlays.ts
@@ -1,29 +1,119 @@
 import ReactGridLayout from "react-grid-layout";
 
+const defaultsLayoutOpts = {
+  isDraggable: false,
+  isResizable: false,
+  static: false,
+};
+
 export const initialLayoutOverlays: ReactGridLayout.Layouts = {
   ulg: [
-    { i: "overlay-redemptions", x: 0, y: 15, w: 20, h: 25, static: true },
-    { i: "overlay-music-player", x: 6, y: 0, w: 25, h: 5, static: true },
+    {
+      i: "overlay-redemptions",
+      x: 0,
+      y: 15,
+      w: 20,
+      h: 25,
+      ...defaultsLayoutOpts,
+    },
+    {
+      i: "overlay-music-player",
+      x: 6,
+      y: 0,
+      w: 25,
+      h: 5,
+      ...defaultsLayoutOpts,
+    },
   ],
   lg: [
-    { i: "overlay-redemptions", x: 0, y: 15, w: 20, h: 25, static: true },
-    { i: "overlay-music-player", x: 6, y: 0, w: 25, h: 5, static: true },
+    {
+      i: "overlay-redemptions",
+      x: 0,
+      y: 15,
+      w: 20,
+      h: 25,
+      ...defaultsLayoutOpts,
+    },
+    {
+      i: "overlay-music-player",
+      x: 6,
+      y: 0,
+      w: 25,
+      h: 5,
+      ...defaultsLayoutOpts,
+    },
   ],
   md: [
-    { i: "overlay-redemptions", x: 0, y: 15, w: 20, h: 25, static: true },
-    { i: "overlay-music-player", x: 6, y: 0, w: 25, h: 5, static: true },
+    {
+      i: "overlay-redemptions",
+      x: 0,
+      y: 15,
+      w: 20,
+      h: 25,
+      ...defaultsLayoutOpts,
+    },
+    {
+      i: "overlay-music-player",
+      x: 6,
+      y: 0,
+      w: 25,
+      h: 5,
+      ...defaultsLayoutOpts,
+    },
   ],
   sm: [
-    { i: "overlay-redemptions", x: 0, y: 15, w: 20, h: 25, static: true },
-    { i: "overlay-music-player", x: 6, y: 0, w: 25, h: 5, static: true },
+    {
+      i: "overlay-redemptions",
+      x: 0,
+      y: 15,
+      w: 20,
+      h: 25,
+      ...defaultsLayoutOpts,
+    },
+    {
+      i: "overlay-music-player",
+      x: 6,
+      y: 0,
+      w: 25,
+      h: 5,
+      ...defaultsLayoutOpts,
+    },
   ],
   xs: [
-    { i: "overlay-redemptions", x: 0, y: 15, w: 20, h: 25, static: true },
-    { i: "overlay-music-player", x: 6, y: 0, w: 25, h: 5, static: true },
+    {
+      i: "overlay-redemptions",
+      x: 0,
+      y: 15,
+      w: 20,
+      h: 25,
+      ...defaultsLayoutOpts,
+    },
+    {
+      i: "overlay-music-player",
+      x: 6,
+      y: 0,
+      w: 25,
+      h: 5,
+      ...defaultsLayoutOpts,
+    },
   ],
   xxs: [
-    { i: "overlay-redemptions", x: 0, y: 15, w: 20, h: 25, static: true },
-    { i: "overlay-music-player", x: 0, y: 5, w: 15, h: 5, static: true },
+    {
+      i: "overlay-redemptions",
+      x: 0,
+      y: 15,
+      w: 20,
+      h: 25,
+      ...defaultsLayoutOpts,
+    },
+    {
+      i: "overlay-music-player",
+      x: 0,
+      y: 5,
+      w: 15,
+      h: 5,
+      ...defaultsLayoutOpts,
+    },
   ],
 };
 
