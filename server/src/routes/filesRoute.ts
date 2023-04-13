03d9cfbea8dfa8cc1c257fc3a8add2bd83ad69a0
Darosss <daroing12@gmail.com>
2023-04-13 13:52:22 +0200

    feat: add new path for alertSounds

--- server/src/routes/filesRoute.ts
+++ server/src/routes/filesRoute.ts
@@ -6,13 +6,19 @@ import {
   deleteMp3File,
   createAudioFolder,
   deleteAudioFolder,
+  getAlertSoundsList,
+  uploadAlertSound,
+  deleteAlertSound,
 } from "@controllers/filesController";
 const filesRouter = Router();
 
 filesRouter.get("/folder-list", getFoldersList);
+filesRouter.get("/audio/alertSounds", getAlertSoundsList);
 filesRouter.get("/audio/:folder", getFolderMp3Files);
+filesRouter.post("/upload/alertSounds", uploadAlertSound);
 filesRouter.post("/upload/audio-mp3/:folder", uploadMp3File);
 filesRouter.post("/create/audio/:folder", createAudioFolder);
+filesRouter.delete("/delete/alertSounds/:fileName", deleteAlertSound);
 filesRouter.delete("/delete/audio/:folder/:fileName", deleteMp3File);
 filesRouter.delete("/delete/folder/audio/:folder", deleteAudioFolder);
 
