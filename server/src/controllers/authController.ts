83f97401cea4e468a7318840e8caafe77a6849db
Darosss <daroing12@gmail.com>
2023-04-15 00:50:39 +0200

    refactor: remove emit forceReconnect

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -2,7 +2,6 @@ import { getTwitchAuthUrl } from "../auth/auth";
 import Express, { Request, Response } from "express";
 
 export const afterTwitchAuthorization = async (req: Request, res: Response) => {
-  req.io.emit("forceReconnect");
   res.status(200).send("Logged in successfully! You can close this page");
 };
 
