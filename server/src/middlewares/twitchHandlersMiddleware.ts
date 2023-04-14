5fad541c3433094d3a3987d30b7b2ea568a41283
Darosss <daroing12@gmail.com>
2023-04-15 00:12:36 +0200

    refactor: change to work as middleware

--- server/src/middlewares/twitchHandlersMiddleware.ts
+++ server/src/middlewares/twitchHandlersMiddleware.ts
@@ -1,38 +1,27 @@
-import { RefreshingAuthProvider } from "@twurple/auth";
+import { NextFunction, Request, Response } from "express";
 import { ApiClient } from "@twurple/api";
-import * as ClientTmi from "./twitchClientTmi";
-import StreamHandler from "../StreamHandler";
-import { Server } from "socket.io";
-import {
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData,
-} from "@libs/types";
-import { AuthorizationTwitch } from "@types";
-import { createNewAuth, createOrGetIfAuthValid } from "@services/auth";
+import { createNewAuth, getAuthToken, removeAuthToken } from "@services/auth";
 import { getConfigs } from "@services/configs";
+import { RefreshingAuthProvider } from "@twurple/auth";
+import ClientTmiHandler from "../stream/TwitchTmiHandler";
+import StreamHandler from "../stream/StreamHandler";
+import { getTwitchAuthUrl } from "../auth/auth";
+import { logger } from "@utils/loggerUtil";
 
-const initTwitchOnAuth = async (
-  authAccesToken: AuthorizationTwitch,
-  socketIO: Server<
-    ClientToServerEvents,
-    ServerToClientEvents,
-    InterServerEvents,
-    SocketData
-  >
+const authorizationTwitch = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
+  console.log(";hgehehehe");
   const clientId = process.env.CLIENT_ID!;
   const clientSecret = process.env.CLIENT_SECRET!;
 
-  const newAuthToken = await createOrGetIfAuthValid({
-    accessToken: authAccesToken.access_token,
-    refreshToken: authAccesToken.refresh_token,
-    expiresIn: authAccesToken.expires_in,
-    obtainmentTimestamp: new Date().getTime(),
-    scope: authAccesToken.scope,
-  });
-
+  const tokenDB = await getAuthToken();
+  if (!tokenDB) {
+    const authUrl = getTwitchAuthUrl();
+    return res.redirect(authUrl.toString());
+  }
   const authProvider = new RefreshingAuthProvider(
     {
       clientId,
@@ -41,30 +30,44 @@ const initTwitchOnAuth = async (
         await createNewAuth(newTokenData);
       },
     },
-    newAuthToken
+    tokenDB
   );
 
-  const twitchApi = new ApiClient({ authProvider });
-  const authorizedUser = await twitchApi.users.getMe();
+  try {
+    const twitchApi = new ApiClient({ authProvider });
+    const authorizedUser = await twitchApi.users.getMe();
+    const configDB = await getConfigs();
 
-  if (!authorizedUser) return;
+    if (!authorizedUser || !configDB) {
+      throw Error("Something went wrong, try login again");
+    }
 
-  const configDB = await getConfigs();
-  if (!configDB) return;
+    const clientTmi = ClientTmiHandler.getInstance({
+      userToListen: authorizedUser.name,
+      username: process.env.bot_username!,
+      password: process.env.bot_password!,
+    });
 
-  const twitchTmi = ClientTmi.default({
-    userToListen: authorizedUser.name,
-  });
-
-  const streamHandler = new StreamHandler({
-    twitchApi: twitchApi,
-    config: configDB,
-    authorizedUser: authorizedUser,
-    socketIO: socketIO,
-    clientTmi: twitchTmi,
-  });
-
-  twitchTmi.connect();
+    const streamHandler = StreamHandler.getInstance({
+      twitchApi: twitchApi,
+      config: configDB,
+      authorizedUser: authorizedUser,
+      socketIO: req.io,
+      clientTmi: clientTmi,
+    });
+    return next();
+  } catch (err) {
+    await removeAuthToken();
+    if (err instanceof Error) {
+      res
+        .status(400)
+        .send({ message: "Something went wrong. Please log in again" });
+      logger.error(`Error occured while using ApiClient ${err}`);
+    } else {
+      res.status(500).send({ message: "Interfnal server error" });
+      logger.error(`An unknown error occured while using ApiClient ${err}`);
+    }
+  }
 };
 
-export default initTwitchOnAuth;
+export default authorizationTwitch;
