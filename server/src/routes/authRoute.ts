32a678d1d6a8794892f3d06ed0bd41b79404f03b
Darosss <daroing12@gmail.com>
2023-04-15 00:14:37 +0200

    feat: add usage of new middlewares

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,9 +1,16 @@
 import Express, { Router } from "express";
 import { overlay, getTwitchAuthorizeUrl } from "@controllers/authController";
+import authorizationTwitch from "@middlewares/authorizationTwitchMiddleware";
+import twitchHandlersMiddleware from "@middlewares/twitchHandlersMiddleware";
 
 const auth = Router();
 
-auth.get("/twitch/callback", overlay);
+auth.get(
+  "/twitch/callback",
+  authorizationTwitch,
+  twitchHandlersMiddleware,
+  overlay
+);
 auth.get("/authorize-url", getTwitchAuthorizeUrl);
 
 export default auth;
