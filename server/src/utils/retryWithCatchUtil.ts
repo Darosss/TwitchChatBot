8ccf65bece1fb86b65b7b289a80d102bb4c1e13d
Darosss <daroing12@gmail.com>
2023-04-15 00:14:18 +0200

    feat: add messages and logger

--- server/src/utils/retryWithCatchUtil.ts
+++ server/src/utils/retryWithCatchUtil.ts
@@ -1,8 +1,19 @@
+import { logger } from "./loggerUtil";
+
+interface RetryWitchCatchOptions {
+  retryLimit: number;
+  retryInterval: number;
+  errorMessage?: string;
+}
+
 const retryWithCatch = async <T = unknown>(
   apiCall: () => Promise<T>,
-  retryLimit: number = 3,
-  retryInterval: number = 1000
+  options: RetryWitchCatchOptions = {
+    retryLimit: 3,
+    retryInterval: 1000,
+  }
 ): Promise<T> => {
+  const { retryLimit, retryInterval, errorMessage } = options;
   let retries = 0;
   while (retries < retryLimit) {
     try {
@@ -10,18 +21,20 @@ const retryWithCatch = async <T = unknown>(
       return result;
     } catch (error) {
       retries++;
-      console.error(
-        `API call failed. Retrying in ${
-          retryInterval * retries
-        }ms... (${retries}/${retryLimit})`,
-        error
-      );
+      const errorMsg = `API call failed. Retrying in ${
+        retryInterval * retries
+      }ms... (${retries}/${retryLimit})`;
+
+      logger.error(`${errorMsg} - ${error}`);
+      console.error(errorMsg, error);
       await new Promise((resolve) =>
         setTimeout(resolve, retryInterval * retries)
       );
     }
   }
-  throw new Error(`API call failed after ${retryLimit} retries.`);
+  throw new Error(
+    `API call failed after ${retryLimit} retries. ${errorMessage || ""}`
+  );
 };
 
 export default retryWithCatch;
