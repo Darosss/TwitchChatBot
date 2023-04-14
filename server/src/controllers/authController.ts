4a9061995db8beb84eaf67f1fdac72170db63abd
Darosss <daroing12@gmail.com>
2023-04-15 00:02:08 +0200

    refactor: move auth logic to middleware and link to auth

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,63 +1,12 @@
-import { AuthorizationTwitch } from "@types";
-import retryWithCatch from "@utils/retryWithCatchUtil";
+import { getTwitchAuthUrl } from "../auth/auth";
 import Express, { Request, Response } from "express";
 
-import initTwitchOnAuth from "../stream/twitch/initTwitchOnAuth";
-
 export const overlay = async (req: Request, res: Response) => {
-  const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env;
-
-  const { code, scope, state } = req.query as unknown as {
-    code: string;
-    scope: string;
-    state: string;
-  };
-  const authRes = await retryWithCatch(() =>
-    fetch("https://id.twitch.tv/oauth2/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-      },
-      body: new URLSearchParams({
-        client_id: CLIENT_ID!,
-        client_secret: CLIENT_SECRET!,
-        code: code,
-        grant_type: "authorization_code",
-        redirect_uri: REDIRECT_URI!,
-      }).toString(),
-    })
-  );
-
-  //GET auth respons with secret / access token
-  if (authRes) {
-    const authTwitchJson = (await authRes.json()) as AuthorizationTwitch;
-
-    initTwitchOnAuth(authTwitchJson, req.io).then(() => {
-      req.io.emit("forceReconnect");
-      res.redirect(process.env.REDIRECT_AFTER_AUTH!);
-    });
-  }
+  req.io.emit("forceReconnect");
+  res.status(200).send("Logged in successfully! You can close this page");
 };
 
 export const getTwitchAuthorizeUrl = (req: Request, res: Response) => {
-  const scopes = [
-    "channel:manage:polls",
-    "channel:read:polls",
-    "channel:read:redemptions",
-    "channel:manage:redemptions",
-    "channel:read:vips",
-    "moderation:read",
-    "moderator:read:chatters",
-  ];
-  const response_type = "code";
-  const state = "c3ab8aa609ea11e793ae92361f002671";
-
-  const authUrl = new URL(`https://id.twitch.tv/oauth2/authorize`);
-  authUrl.searchParams.append("response_type", response_type);
-  authUrl.searchParams.append("client_id", process.env.CLIENT_ID!);
-  authUrl.searchParams.append("redirect_uri", process.env.REDIRECT_URI!);
-  authUrl.searchParams.append("scope", scopes.join(" "));
-  authUrl.searchParams.append("state", state);
-
+  const authUrl = getTwitchAuthUrl();
   res.status(200).send({ data: authUrl });
 };
