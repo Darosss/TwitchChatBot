ca6cce0825e74772a122623143efe0db9444586c
Darosss <daroing12@gmail.com>
2023-04-15 00:11:45 +0200

    feat: add runOnceMiddleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,9 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, {
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from "express";
 import cors from "cors";
 import initRoutes from "./routes";
 import localSocket from "./socketIO";
@@ -7,13 +12,13 @@ import {
   errorResponder,
   invalidPathHandler,
 } from "@middlewares/errorHandlersMiddleware";
+import twitchHandlersMiddleware from "@middlewares/twitchHandlersMiddleware";
 
 const expressApp = () => {
   const app = express();
   const server = http.createServer(app);
 
   app.use(express.json());
-
   app.set("json spaces", 2);
   app.use(
     cors({
@@ -26,20 +31,30 @@ const expressApp = () => {
   app.use(express.urlencoded({ extended: true }));
 
   const localSocketIO = localSocket(server);
-
   app.use((req: Request, res: Response, next: NextFunction) => {
     req.io = localSocketIO;
-
     return next();
   });
+  app.use(runOnceMiddleware(twitchHandlersMiddleware));
 
   initRoutes(app);
 
   app.use(errorResponder);
-
   app.use(invalidPathHandler);
-
   return server;
 };
+
+function runOnceMiddleware(middleware: RequestHandler): RequestHandler {
+  let hasRun = false;
+
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!hasRun) {
+      middleware(req, res, next);
+      hasRun = true;
+    } else {
+      next();
+    }
+  };
+}
 
 export default expressApp;
