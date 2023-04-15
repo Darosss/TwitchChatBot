9cc2b32d6d612b2e013f4b23f4245dd0d28f7f29
Darosss <daroing12@gmail.com>
2023-04-15 15:22:06 +0200

    feat: add envFilePath

--- server/src/configs/globalPaths.ts
+++ server/src/configs/globalPaths.ts
@@ -1,4 +1,5 @@
 import path from "path";
 
+export const envFilePath = path.resolve(__dirname, "../../../.env");
 export const musicPath = path.resolve(__dirname, "../public/music");
 export const alertSoundsPath = path.resolve(__dirname, "../public/alertSounds");
