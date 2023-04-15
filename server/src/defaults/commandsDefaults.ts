05d1c033b0b5639f9eff9fb0a3941423b3a956ef
Darosss <daroing12@gmail.com>
2023-04-15 15:23:25 +0200

    feat: add commandsDefaults file

---
+++ server/src/defaults/commandsDefaults.ts
@@ -0,0 +1,28 @@
+import { ChatCommandCreateData } from "@services/chatCommands/types";
+
+export const getDefaultChatCommands = () => {
+  const defaultCommands: ChatCommandCreateData[] = [
+    {
+      name: "messages",
+      messages: ["@$user{username}, your messages: $user{messageCount}"],
+      aliases: ["messages", "msgs", "msg"],
+      description: "Send information about user's message count",
+    },
+    {
+      name: "points",
+      messages: ["@$user{username}, your points: $user{points}"],
+      aliases: ["pts", "points"],
+      description: "Send information about user's points",
+    },
+    {
+      name: "example",
+      messages: [
+        "This is example command message 1",
+        "This is example command message 2",
+      ],
+      aliases: ["example", "exampleCommand"],
+      description: "Example command description",
+    },
+  ];
+  return defaultCommands;
+};
