1eecb850b70782244a8e153a3a2e869a184b307a
Darosss <daroing12@gmail.com>
2023-04-17 00:29:52 +0200

    feat: add all env variables to envVariables

---
+++ server/src/configs/envVariables.ts
@@ -0,0 +1,13 @@
+export const {
+  HOST_FRONTEND_URL: hostFrontendURL,
+  LOCAL_FRONTEND_URL: localFrontendURL,
+  BACKEND_PORT: backendPort,
+  CLIENT_ID: clientId,
+  CLIENT_SECRET: clientSecret,
+  REDIRECT_URL: redirectUrl,
+  DATABASE_CONNECT_URL: databaseConnectURL,
+  BOT_USERNAME: botUsername,
+  BOT_PASSWORD: botPassword,
+  BOT_ID: botId,
+  NODE_ENV: nodeEnv,
+} = process.env;
