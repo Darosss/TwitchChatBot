2bdb3af63aff906d8f0d4304fa5b87acc617c236
Darosss <daroing12@gmail.com>
2023-04-20 11:02:41 +0200

    feat(component): add home compoonent

---
+++ frontend/src/components/home/Home.tsx
@@ -0,0 +1,108 @@
+import ChangeTheme from "@components/changeTheme";
+import { HelmetTitle } from "@components/componentWithTitle";
+import Message, { MessageProps } from "@components/message";
+import { routes } from "@routes/routesList";
+import React, { useEffect, useReducer, useState } from "react";
+import { Link } from "react-router-dom";
+
+const chatMessages = [
+  "WELCOME HOME!",
+  "Wassup, bro?",
+  "How's goin...",
+  "Nice weather today, don't you think so?",
+  "Hope you all right",
+  "Go and check your triggers :D",
+  "I have feeling that timers today will be happier",
+  "I hope you all right!",
+];
+
+export default function Home() {
+  return (
+    <div className="home-wrapper">
+      <HelmetTitle title="Home" />
+      <div className="home-nav">
+        <h1> Twitch ChatBot </h1>
+        <ChangeTheme />
+      </div>
+      <div className="home-content">
+        <div className="home-content-tiles">
+          {routes.map((route) => {
+            return (
+              <div>
+                <Link to={route.path} className="common-button primary-button">
+                  {route.label}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <ChatBackground />
+      </div>
+      <div className="home-footer">
+        <div>Footer</div>
+        <div>Footer</div>
+        <div>Footer</div>
+      </div>
+    </div>
+  );
+}
+
+function ChatBackground() {
+  const [messages, setMessages] = useState<MessageProps[]>([]);
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  useEffect(() => {
+    let chatTimeout: NodeJS.Timeout;
+    let chatMessageIndex = 1;
+
+    const getRandomDelay = () => {
+      const min = 500;
+      const max = 1000;
+      const randomDelay = Math.floor(Math.random() * (max - min + 1)) + min;
+      return randomDelay;
+    };
+
+    const getRandomFromChatMessages = () => {
+      console.log("test", Math.floor(Math.random() * chatMessages.length));
+      return chatMessages[Math.floor(Math.random() * chatMessages.length)];
+    };
+
+    const timeoutChatMessage = () => {
+      chatTimeout = setTimeout(() => {
+        if (chatMessageIndex > 100) return;
+        chatMessageIndex++;
+        setMessages((prevState) => {
+          prevState.push({
+            date: new Date(),
+            username: `Chat bot`,
+            message: getRandomFromChatMessages(),
+          });
+          return prevState;
+        });
+        forceUpdate();
+        timeoutChatMessage();
+      }, chatMessageIndex * getRandomDelay());
+    };
+
+    timeoutChatMessage();
+
+    return () => clearTimeout(chatTimeout);
+  }, []);
+
+  return (
+    <div className="home-chat-bg prevent-select">
+      {messages.map((message, index) => {
+        return (
+          <div key={index} className="typewriter">
+            <Message
+              date={message.date}
+              username={message.username}
+              message={message.message}
+              tooltip={false}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ frontend/src/components/home/index.ts
@@ -0,0 +1,4 @@
+import "./style.scss";
+import Home from "./Home";
+
+export default Home;
