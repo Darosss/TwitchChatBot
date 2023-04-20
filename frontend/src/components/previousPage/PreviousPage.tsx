018aaf8b7f597158287ec917b6ad6322ea75e5cb
Darosss <daroing12@gmail.com>
2023-04-20 11:13:05 +0200

    style: adjust size of previous button

--- frontend/src/components/previousPage/PreviousPage.tsx
+++ frontend/src/components/previousPage/PreviousPage.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function PreviousPage() {
   return (
-    <>
+    <div className="previous-page-wrapper">
       <Link className="common-button primary-button" to="../">
         &#8592;Back
       </Link>
-    </>
+    </div>
   );
 }
