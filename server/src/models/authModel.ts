962b44af51b58ea03e71f3dac1fde1d67b199aa4
Darosss <daroing12@gmail.com>
2023-04-24 18:21:49 +0200

    feat(model): add ivAccessToken and ivRefreshToken to AuthSchema

--- server/src/models/authModel.ts
+++ server/src/models/authModel.ts
@@ -1,10 +1,14 @@
 import { Model, model, Schema } from "mongoose";
 import { AuthDocument } from "./types";
+import { encryptionKey } from "@configs/envVariables";
+import { encryptToken } from "@utils/tokenUtil";
 
 const AuthSchema: Schema<AuthDocument> = new Schema(
   {
     accessToken: { type: String, required: true },
+    ivAccessToken: { type: Buffer },
     refreshToken: { type: String, required: true },
+    ivRefreshToken: { type: Buffer },
     expiresIn: { type: Number, require: true, default: 0 },
     obtainmentTimestamp: { type: Number, required: true, default: 0 },
     scope: [String],
