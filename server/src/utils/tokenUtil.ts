e9aa80cd6b5a9cf52643f9db426d07cdc49e4072
Darosss <daroing12@gmail.com>
2023-04-24 18:20:16 +0200

    feat: add iv to decryptToken as parameter

--- server/src/utils/tokenUtil.ts
+++ server/src/utils/tokenUtil.ts
@@ -17,16 +17,14 @@ export const encryptToken = (
   encrypted += cipher.final("hex");
   return { iv: iv, encrypted: encrypted };
 };
+
 export const decryptToken = (
   encryptedToken: string,
+  iv: Buffer,
   encryptionKey: string
 ): string => {
-  const decipher = crypto.createDecipheriv(
-    algorithm,
-    Buffer.from(encryptionKey),
-    iv
-  );
-  let decrypted = decipher.update(encryptedToken, "binary", "utf8");
+  const decipher = crypto.createDecipheriv(algorithm, encryptionKey, iv);
+  let decrypted = decipher.update(encryptedToken, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
 };
