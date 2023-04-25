09ac53b2816da6737fe510c91efbddfe541e1c1b
Darosss <daroing12@gmail.com>
2023-04-25 11:57:51 +0200

    feat: add update prefixes, suffixes and chances

--- frontend/src/components/modesList/affixes/Affixes.tsx
+++ frontend/src/components/modesList/affixes/Affixes.tsx
@@ -22,22 +22,25 @@ export default function Affixes() {
   const [editingAffix, setEditingAffix] = useState("");
   const [affixIdDelete, setAffixIdDelete] = useState<string | null>(null);
 
-  const [name, setName] = useState("");
   const [createName, setCreateName] = useState("");
+  const [name, setName] = useState("");
+  const [prefixChance, setPrefixChance] = useState<number>(5);
+  const [suffixChance, setSuffixChance] = useState<number>(5);
+  const [prefixes, setPrefixes] = useState<string[]>([""]);
+  const [suffixes, setSuffixes] = useState<string[]>([""]);
 
   const { data: affixesData, loading, error, refetchData } = getAffixes();
 
   const { refetchData: fetchEditAffix } = editAffix(editingAffix, {
     name: name,
+    prefixes: prefixes,
+    suffixes: suffixes,
+    prefixChance: prefixChance,
+    suffixChance: suffixChance,
   });
 
   const { refetchData: fetchCreateAffix } = createAffix({
     name: createName,
-    prefixChance: 5,
-    suffixChance: 5,
-    prefixes: [""],
-    suffixes: [""],
-    //TODO:ADD states
   });
 
   const { refetchData: fetchDeleteAffix } = deleteAffix(
@@ -85,6 +88,10 @@ export default function Affixes() {
   const handleOnEdit = (affix: Affix) => {
     setEditingAffix(affix._id);
     setName(affix.name);
+    setPrefixChance(affix.prefixChance);
+    setSuffixChance(affix.suffixChance);
+    setPrefixes(affix.prefixes);
+    setSuffixes(affix.suffixes);
     setShowModal(true);
   };
 
@@ -154,6 +161,53 @@ export default function Affixes() {
               value={name}
               onChange={(e) => {
                 setName(e.target.value);
+              }}
+            />
+          </div>
+          <div>PrefixChance</div>
+          <div>
+            <input
+              className="triggers-list-input"
+              type="number"
+              value={prefixChance}
+              min={0}
+              max={100}
+              onChange={(e) => {
+                setPrefixChance(e.target.valueAsNumber);
+              }}
+            />
+          </div>
+          <div>Prefixes</div>
+          <div>
+            <textarea
+              className="triggers-list-input"
+              value={prefixes.join("\n")}
+              onChange={(e) => {
+                setPrefixes(e.target.value.split("\n"));
+              }}
+            />
+          </div>
+          <div>SuffixChance</div>
+          <div>
+            <input
+              className="triggers-list-input"
+              type="number"
+              value={suffixChance}
+              min={0}
+              max={100}
+              onChange={(e) => {
+                setSuffixChance(e.target.valueAsNumber);
+              }}
+            />
+          </div>
+
+          <div>Suffixes</div>
+          <div>
+            <textarea
+              className="triggers-list-input"
+              value={suffixes.join("\n")}
+              onChange={(e) => {
+                setSuffixes(e.target.value.split("\n"));
               }}
             />
           </div>
