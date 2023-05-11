9343e40a311d514c4a75f55f35a56327e9278896
Darosss <daroing12@gmail.com>
2023-05-12 00:13:58 +0200

    refactor: move types into sepaarated files

---
+++ server/src/socketIO/types/serverToClient.ts
@@ -0,0 +1,40 @@
+import {
+  EventAndUser,
+  RewardData,
+  CustomRewardData,
+  AudioStreamData,
+  AudioStreamDataInfo,
+  AudioYTData,
+  AudioYTDataInfo,
+} from "./dataTypes";
+
+export interface ServerToClientEvents
+  extends ServerToClientYoutubeEvents,
+    ServerToClientMusicLocalEvents {
+  noArg: () => void;
+  withAck: (callback: (e: number) => void) => void;
+  messageServer: (date: Date, username: string, message: string) => void;
+  userJoinTwitchChat: (eventAndUser: EventAndUser) => void;
+  onRedemption: (data: RewardData, alertSound: Buffer) => void;
+
+  forceReconnect: () => void;
+  getCustomRewards: (data: CustomRewardData[]) => void;
+  sendLoggedUserInfo: (username: string) => void;
+}
+
+export interface ServerToClientMusicLocalEvents {
+  audio: (data: AudioStreamData) => void;
+  audioStop: () => void;
+  getAudioInfo: (data: AudioStreamDataInfo) => void;
+  changeVolume: (volume: number) => void;
+}
+
+export interface ServerToClientYoutubeEvents {
+  changeYTVolume: (volume: number) => void;
+  musicYTNext: () => void;
+  musicYTPause: () => void;
+  musicYTStop: () => void;
+  musicYTPlay: () => void;
+  audioYT: (data: AudioYTData) => void;
+  getAudioYTInfo: (data: AudioYTDataInfo) => void;
+}
