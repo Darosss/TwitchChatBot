716baf14a3bcc1e8e13860285d84ffbabd05b1fd
Darosss <daroing12@gmail.com>
2023-05-13 02:00:11 +0200

    chore: add eslint to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   css: {
     preprocessorOptions: {
@@ -12,7 +12,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslint()],
   envDir: "../",
 
   optimizeDeps: {
