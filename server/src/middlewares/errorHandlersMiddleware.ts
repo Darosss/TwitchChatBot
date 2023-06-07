0266a7b78d9660bef5a4c8dac2af3e02f2904357
Darosss <daroing12@gmail.com>
2023-06-07 23:13:34 +0200

    refactor: remove res.header + replace res.status to res.statusCode

--- server/src/middlewares/errorHandlersMiddleware.ts
+++ server/src/middlewares/errorHandlersMiddleware.ts
@@ -1,16 +1,14 @@
 import { AppError } from "@utils/ErrorHandlerUtil";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-export const errorResponder = (error: AppError, req: Request, res: Response) => {
-  res.header("Content-Type", "application/json");
-
+export const errorResponder = (error: AppError, req: Request, res: Response, next: NextFunction) => {
   const status = error.statusCode || 400;
   res.status(status).send({ message: error.message, status: status });
 };
 
-export const invalidPathHandler = (req: Request, res: Response) => {
+export const invalidPathHandler = (req: Request, res: Response, next: NextFunction) => {
   const statusCode = 404;
 
-  res.status(statusCode);
+  res.statusCode = statusCode;
   res.send({ message: "Invalid path", status: statusCode });
 };
