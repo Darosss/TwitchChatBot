e67ca7353e0ca2ece867fd71daf5f4c823921b0d
Darosss <daroing12@gmail.com>
2023-06-25 23:38:39 +0200

    feat: add youtube progress bar

--- frontend/src/components/overlay/youtubePlayerVideo/YoutubePlayerVideo.tsx
+++ frontend/src/components/overlay/youtubePlayerVideo/YoutubePlayerVideo.tsx
@@ -2,10 +2,17 @@ import React, { useCallback, useContext, useEffect, useState } from "react";
 
 import { SocketContext } from "@context/socket";
 import YouTube, { YouTubeEvent, YouTubeProps } from "react-youtube";
+import SongProgress from "../SongProgress";
+
+let progressTimer: NodeJS.Timer; // as global because clear interval doesnt work
+//FIXME: later
+
 export default function MusicPlayer() {
   const socket = useContext(SocketContext);
   const [songName, setSongName] = useState("");
   const [player, setPlayer] = React.useState<YouTubeEvent | null>(null);
+  const [duration, setDuration] = useState<number>(0);
+  const [currentTime, setCurrentTime] = useState<number>(0);
 
   const handleYTPause = useCallback(() => {
     if (player) player.target.pauseVideo();
@@ -38,6 +45,9 @@ export default function MusicPlayer() {
 
   useEffect(() => {
     socket.on("audioYT", (data) => {
+      setSongInterval();
+      setCurrentTime(data.currentTime);
+      setDuration(data.duration);
       handleYTResume(data.id, data.currentTime, data.volume);
       setSongName(data.name);
     });
@@ -45,6 +55,8 @@ export default function MusicPlayer() {
     return () => {
       socket.off("audioYT");
     };
+    //FIXME: later
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [handleYTResume, socket]);
 
   useEffect(() => {
@@ -57,10 +69,31 @@ export default function MusicPlayer() {
     };
   }, [handleYTChangeVolume, socket]);
 
+  const countSongTime = () => setCurrentTime((prev) => prev + 1);
+
+  const setSongInterval = () => {
+    clearInterval(progressTimer);
+    progressTimer = setInterval(() => {
+      countSongTime();
+    }, 1000);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(progressTimer);
+    };
+    // Disable eslint - it just clearInterval
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const onPlayerReady: YouTubeProps["onReady"] = (event) => {
     setPlayer(event);
 
     socket.emit("getAudioYTData", (isPlaying, cb) => {
+      setSongInterval();
+      setCurrentTime(cb.currentTime);
+      setDuration(cb.duration);
+
       if (!isPlaying) return;
       event.target.loadVideoById(cb.id, cb.currentTime);
       event.target.setVolume(cb.volume);
@@ -68,9 +101,10 @@ export default function MusicPlayer() {
     });
   };
   const onPlayerEndSong: YouTubeProps["onEnd"] = (event) => {};
+
   const opts: YouTubeProps["opts"] = {
-    height: "100%",
-    width: "100%",
+    height: "0%",
+    width: "0%",
     playerVars: {
       autoplay: 0,
       enablejsapi: 1,
@@ -79,10 +113,12 @@ export default function MusicPlayer() {
   };
 
   return (
-    <div className="youtube-player-wrapper">
+    <div className={`youtube-player-wrapper ${songName ? "" : "hidden"}`}>
       <div className="youtube-song-details">
         <div className="youtube-current-song">{songName} </div>
-        <div className="youtube-song-progress">progress soon </div>
+        <div className="youtube-song-progress">
+          <SongProgress songDuration={duration} currentTime={currentTime} />
+        </div>
       </div>
 
       <div className="youtube-player">
