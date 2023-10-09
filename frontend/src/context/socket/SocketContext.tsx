026b984c6327bede717fec18e6781fdcb1016249
Darosss <daroing12@gmail.com>
2023-10-10 00:03:56 +0200

    refactor: move socket logic into SocketContext

--- frontend/src/context/socket/SocketContext.tsx
+++ frontend/src/context/socket/SocketContext.tsx
@@ -1,41 +1,53 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import { io, Socket } from "socket.io-client";
-import { ServerToClientEvents, ClientToServerEvents } from "./types";
-import { viteBackendUrl } from "src/configs/envVariables";
-export const socketConn = io(viteBackendUrl) as Socket<
+import {
   ServerToClientEvents,
-  ClientToServerEvents
->;
+  ClientToServerEvents,
+  SocketContexType,
+} from "./types";
+import { viteBackendUrl } from "src/configs/envVariables";
+import { getSocketEmitsFunctions } from "./emits";
+import { getSocketEventsFunctions } from "./events";
 
-export const SocketContext = React.createContext<
-  Socket<ServerToClientEvents, ClientToServerEvents>
->({} as Socket<ServerToClientEvents, ClientToServerEvents>);
-
-socketConn.on("forceReconnect", () => {
-  socketConn.disconnect();
-
-  socketConn.connect();
-});
-
-export const socketEmitRefreshTriggers = () => {
-  socketConn.emit("refreshTriggers");
-};
-
-export const socketEmitRefreshTimers = () => {
-  socketConn.emit("refreshTimers");
-};
-
-export const socketEmitRefreshCommands = () => {
-  socketConn.emit("refreshCommands");
-};
+export const SocketContext = React.createContext<SocketContexType | null>(null);
 
 export const SocketContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element => {
+  const socketConn = io(viteBackendUrl) as Socket<
+    ServerToClientEvents,
+    ClientToServerEvents
+  >;
+  const emits = useMemo<SocketContexType["emits"]>(() => {
+    if (!socketConn) return;
+
+    return getSocketEmitsFunctions(socketConn);
+  }, [socketConn]);
+
+  const events = useMemo<SocketContexType["events"]>(() => {
+    if (!socketConn) return;
+
+    return getSocketEventsFunctions(socketConn);
+  }, [socketConn]);
+
+  useEffect(() => {
+    if (!socketConn || !events) return;
+
+    events.forceReconnect.on(() => {
+      socketConn.disconnect();
+
+      socketConn.connect();
+    });
+
+    return () => {
+      events.forceReconnect.off();
+    };
+  }, [socketConn, events]);
+
   return (
-    <SocketContext.Provider value={socketConn}>
+    <SocketContext.Provider value={{ emits, events }}>
       {children}
     </SocketContext.Provider>
   );
