8fa9a361208a68fe51c99cadacc28311cf94d6ec
Darosss <daroing12@gmail.com>
2023-10-12 21:01:06 +0200

    feat: add index.ts to routes folder

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,3 +1,14 @@
-import { routes } from "./routesList";
+export { routes } from "./routesList";
 
-export { routes };
+export { CommandRoutes } from "./CommandRoute";
+export { ConfigRoutes } from "./ConfigsRoute";
+export { EventRoutes } from "./EventsRoute";
+export { MessageCategoriesRoute } from "./MessageCategoriesRoute";
+export { MessageRoutes } from "./MessageRoute";
+export { ModesRoutes } from "./ModeRoute";
+export { OverlayRoutes } from "./OverlayRoute";
+export { RedemptionRoutes } from "./RedemptionRoute";
+export { StreamSessionRoutes } from "./StreamSessionRoute";
+export { TimersRoute } from "./TimersRoute";
+export { TriggerRoutes } from "./TriggerRoute";
+export { UserRoutes } from "./UserRoute";
