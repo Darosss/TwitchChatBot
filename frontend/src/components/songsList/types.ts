99af869bf0dcc048943fad2f2be48afe5d9a85b8
Darosss <daroing12@gmail.com>
2023-10-24 11:51:23 +0200

    feat(songsList): add SET_WHO_ADDED to DispatchAction

--- frontend/src/components/songsList/types.ts
+++ frontend/src/components/songsList/types.ts
@@ -6,4 +6,5 @@ export type DispatchAction =
   | { type: "SET_CUSTOM_TITLE"; payload: SongCustomTitle }
   | { type: "SET_DURATION"; payload: number }
   | { type: "SET_CUSTOM_ID"; payload: string }
+  | { type: "SET_WHO_ADDED"; payload: string }
   | { type: "SET_STATE"; payload: SongCreateData };
