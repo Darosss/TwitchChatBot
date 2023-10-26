895c9a5dbad224fc4f2593cded91b84d0a3d740e
Darosss <daroing12@gmail.com>
2023-10-26 14:12:57 +0200

    refactor(routes): move routes into AllRoutes.tsx

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,29 +1,9 @@
 import React from "react";
-
 import { SocketContextProvider } from "@context";
-import SideBar from "@components/sideBar";
-
-import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import { ReactNotifications } from "react-notifications-component";
 import { HelmetProvider } from "react-helmet-async";
-import Home from "@components/home";
-import OverlayList from "@components/overlay/overlaysList";
-import ComponentWithTitle from "@components/componentWithTitle";
-import {
-  OverlayRoutes,
-  UserRoutes,
-  MessageRoutes,
-  MessageCategoriesRoute,
-  CommandRoutes,
-  EventRoutes,
-  ModesRoutes,
-  RedemptionRoutes,
-  StreamSessionRoutes,
-  TimersRoute,
-  TriggerRoutes,
-  ConfigRoutes,
-  SongsRoutes,
-} from "@routes";
+import { AllRoutes } from "@routes";
 
 function App() {
   return (
@@ -33,44 +13,7 @@ function App() {
           <div className="main">
             <ReactNotifications />
             <Routes>
-              <Route element={<OverlayLayout />}>
-                <Route path="/overlay/*" element={<OverlayRoutes />} />
-              </Route>
-              <Route element={<HomeLayout />}>
-                <Route path="/" element={<Home />} />
-              </Route>
-
-              <Route element={<DefaultRouteLayout />}>
-                <Route
-                  path="/overlay"
-                  element={
-                    <ComponentWithTitle
-                      title="Overlays"
-                      component={<OverlayList />}
-                    />
-                  }
-                />
-
-                <Route path="/users/*" element={<UserRoutes />} />
-                <Route path="/messages/*" element={<MessageRoutes />} />
-                <Route
-                  path="/message-categories/*"
-                  element={<MessageCategoriesRoute />}
-                />
-                <Route path="/commands/*" element={<CommandRoutes />} />
-                <Route path="/events/*" element={<EventRoutes />} />
-                <Route path="/modes/*" element={<ModesRoutes />} />
-                <Route path="/redemptions/*" element={<RedemptionRoutes />} />
-                <Route
-                  path="/stream-sessions/*"
-                  element={<StreamSessionRoutes />}
-                />
-                <Route path="/songs/*" element={<SongsRoutes />} />
-                <Route path="/timers/*" element={<TimersRoute />} />
-                <Route path="/triggers/*" element={<TriggerRoutes />} />
-                <Route path="/configs/*" element={<ConfigRoutes />} />
-                <Route path="/*" element={<>Not found</>} />
-              </Route>
+              <AllRoutes />
             </Routes>
           </div>
         </BrowserRouter>
@@ -79,29 +22,4 @@ function App() {
   );
 }
 
-const DefaultRouteLayout = () => {
-  return (
-    <div className="main-other">
-      <SideBar />
-
-      <Outlet />
-    </div>
-  );
-};
-
-const HomeLayout = () => {
-  return (
-    <div className="main-other">
-      <Outlet />
-    </div>
-  );
-};
-
-const OverlayLayout = () => {
-  return (
-    <div className="main-overlay">
-      <Outlet />
-    </div>
-  );
-};
 export default App;
--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,15 +1,3 @@
 export { routes } from "./routesList";
 
-export { CommandRoutes } from "./CommandRoute";
-export { ConfigRoutes } from "./ConfigsRoute";
-export { EventRoutes } from "./EventsRoute";
-export { MessageCategoriesRoute } from "./MessageCategoriesRoute";
-export { MessageRoutes } from "./MessageRoute";
-export { ModesRoutes } from "./ModeRoute";
-export { OverlayRoutes } from "./OverlayRoute";
-export { RedemptionRoutes } from "./RedemptionRoute";
-export { StreamSessionRoutes } from "./StreamSessionRoute";
-export { TimersRoute } from "./TimersRoute";
-export { TriggerRoutes } from "./TriggerRoute";
-export { UserRoutes } from "./UserRoute";
-export { SongsRoutes } from "./SongsRoute";
+export { AllRoutes } from "./AllRoutes";
