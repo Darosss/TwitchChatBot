7e4db1d649ce97b5fe155381af2873420926a55f
Darosss <daroing12@gmail.com>
2023-11-02 23:04:14 +0100

    refactor(checkSearchParams): add defaults page and limit

--- server/src/middlewares/checkSearchParamsMiddleware.ts
+++ server/src/middlewares/checkSearchParamsMiddleware.ts
@@ -1,18 +1,13 @@
 import { RequestQuery } from "@types";
-import { AppError } from "@utils";
 import { NextFunction, Request, Response } from "express";
 
 export const checkSearchParams = (req: Request<{}, {}, {}, RequestQuery>, res: Response, next: NextFunction) => {
   const { page, limit } = req.query;
-  let message = ``;
 
   if ((page && limit && Number(page) > 0 && Number(limit) > 0) || (page === undefined && limit === undefined))
     return next();
 
-  if (Number(page) <= 0) message += `Page key must be > 0;`;
-  if (Number(limit) <= 0) message += `Limit key must be > 0;`;
-
-  if (message.length < 1) message = "Probably page or limit aren't numbers";
-
-  throw new AppError(400, message);
+  req.query.page = "1";
+  req.query.limit = "20";
+  return next();
 };
