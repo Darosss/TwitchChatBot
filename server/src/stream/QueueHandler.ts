ddd7fbb56eebbc7d6b0e755c2b616efd3c6e1502
Darosss <daroing12@gmail.com>
2023-11-04 18:33:40 +0100

    feat(stream): add QueueHandler

---
+++ server/src/stream/QueueHandler.ts
@@ -0,0 +1,56 @@
+class QueueHandler<T> {
+  private items: T[];
+  private interval: NodeJS.Timeout | null = null;
+  constructor() {
+    this.items = [];
+  }
+
+  private clearInterval() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+
+  protected startInterval(cb: (item: T) => void) {
+    if (this.interval) return;
+    this.interval = setInterval(async () => {
+      const item = this.peek();
+
+      if (!item) return this.clearInterval();
+
+      cb(item);
+
+      if (item) this.dequeue();
+    }, 2500);
+  }
+
+  public enqueue(item: T) {
+    this.items.push(item);
+    console.log(`Item ${JSON.stringify(item)} inserted`);
+  }
+
+  public dequeue() {
+    if (this.isEmpty()) {
+      return console.log("No items in queue");
+    }
+    return this.items.shift();
+  }
+
+  public peek() {
+    if (this.isEmpty()) {
+      return console.log("No items in queue");
+    }
+    return this.items[0];
+  }
+
+  public isEmpty() {
+    return this.items.length == 0;
+  }
+
+  public printQueue() {
+    console.log(JSON.stringify(this.items));
+  }
+}
+
+export default QueueHandler;
