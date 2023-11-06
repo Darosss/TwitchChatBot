031e0e2b67e8c0d1a53d807db93f3fc8f624bbd7
Darosss <daroing12@gmail.com>
2023-11-07 00:53:06 +0100

    feat(defaults/achievements): add new ACHIEVEMENTS enum
    add DOTS, QUESTION_MARKS, COMMAS, DICTATOR, MONKEY, LONG_MESSAGES, SHORT_MESSAGES, XD, POLISH_SWEARING, KAPPA, LUL
    
    add mostly chat messages ACHIEVEMENTS

--- server/src/defaults/achievements/types.ts
+++ server/src/defaults/achievements/types.ts
@@ -1,4 +1,25 @@
 export enum ACHIEVEMENTS {
   WATCH_TIME = "WATCH TIME",
-  CHAT_MESSAGES = "CHAT MESSAGES"
+  CHAT_MESSAGES = "CHAT MESSAGES",
+  DOTS = "DOT COUNTS",
+  QUESTION_MARKS = "QUESTION MARKS",
+  COMMAS = "COMMA COMMA",
+  DICTATOR = "DICTATOR",
+  MONKEY = "MONKEY",
+  LONG_MESSAGES = "WORD SMITH",
+  SHORT_MESSAGES = "ENTER",
+  XD = "XD",
+  POLISH_SWEARING = "MIENSO",
+  KAPPA = "KAPPA",
+  LUL = "LUL"
+}
+
+//sorry for swearing LUL
+export enum POLISH_SWEARING {
+  KURWA = "kurwa",
+  PIERDOLE = "pierdole",
+  CHUJ = "chuj",
+  PIZDA = "pizda",
+  JEBAC = "jebac",
+  JEBAC2 = "jebać"
 }
