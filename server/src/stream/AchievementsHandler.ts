a03a50993a3b2e0f9717846ca717c213fe4dab79
Darosss <daroing12@gmail.com>
2023-11-07 00:37:50 +0100

    feat(AchievementsHandler): add check messages achievements method

--- server/src/stream/AchievementsHandler.ts
+++ server/src/stream/AchievementsHandler.ts
@@ -14,7 +14,20 @@ import {
   GetDataForObtainAchievementEmitReturnData,
   UpdateAchievementUserProgressProgressesArgs
 } from "@services";
+import { ACHIEVEMENTS, POLISH_SWEARING } from "@defaults";
 import { achievementsLogger } from "@utils";
+
+interface CheckMessageForAchievement {
+  message: string;
+  date: Date;
+  userId: string;
+  username: string;
+}
+
+interface CheckMessageForAchievementWithCondition extends Pick<CheckMessageForAchievement, "userId" | "username"> {
+  condition: boolean;
+  achievementName: ACHIEVEMENTS;
+}
 
 interface UpdateAchievementUserProgressOpts extends UpdateAchievementUserProgressProgressesArgs {
   username: string;
@@ -67,6 +80,52 @@ class AchievementsHandler extends QueueHandler<ObtainAchievementData> {
 
   private emitObtainAchievement({ achievementName, stage, username }: ObtainAchievementData) {
     this.socketIO.emit("obtainAchievement", { achievementName: achievementName, stage, username });
+  }
+
+  public async checkMessageForAchievements({ message, ...data }: CheckMessageForAchievement) {
+    const checkMessageForConditionWithData = async (condition: boolean, achievementName: ACHIEVEMENTS) =>
+      await this.checkMessageForAchievementWithCondition({ ...data, condition, achievementName });
+
+    const lowerCaseMessage = message.toLowerCase();
+    await checkMessageForConditionWithData(true, ACHIEVEMENTS.CHAT_MESSAGES);
+    await checkMessageForConditionWithData(message.includes("."), ACHIEVEMENTS.DOTS);
+    await checkMessageForConditionWithData(message.includes("?"), ACHIEVEMENTS.QUESTION_MARKS);
+    await checkMessageForConditionWithData(message.includes("!"), ACHIEVEMENTS.DICTATOR);
+    await checkMessageForConditionWithData(message.includes(","), ACHIEVEMENTS.COMMAS);
+    await checkMessageForConditionWithData(message.includes("@"), ACHIEVEMENTS.MONKEY);
+    await checkMessageForConditionWithData(message.length > 25, ACHIEVEMENTS.LONG_MESSAGES);
+    await checkMessageForConditionWithData(message.length < 4, ACHIEVEMENTS.SHORT_MESSAGES);
+    await checkMessageForConditionWithData(message.includes("xd"), ACHIEVEMENTS.XD);
+    await checkMessageForConditionWithData(message.includes("Kappa"), ACHIEVEMENTS.KAPPA);
+    await checkMessageForConditionWithData(message.includes("LUL"), ACHIEVEMENTS.LUL);
+    await checkMessageForConditionWithData(
+      this.isMessageContaingPolishSwearing(lowerCaseMessage),
+      ACHIEVEMENTS.POLISH_SWEARING
+    );
+  }
+
+  private isMessageContaingPolishSwearing(lowercaseMessage: string) {
+    for (const swearing in POLISH_SWEARING) {
+      console.log(swearing.toLowerCase(), lowercaseMessage, "c do chuja");
+      if (lowercaseMessage.includes(swearing.toLowerCase())) return true;
+    }
+    return false;
+  }
+
+  private async checkMessageForAchievementWithCondition({
+    condition,
+    achievementName,
+    userId,
+    username
+  }: CheckMessageForAchievementWithCondition) {
+    if (!condition) return;
+
+    await this.updateAchievementUserProgressAndAddToQueue({
+      achievementName,
+      userId,
+      username,
+      progress: { increment: true, value: 1 }
+    });
   }
 }
 
