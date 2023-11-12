4fb6252280edb80d32f7f79cbfd2db9fb39fd1fe
Darosss <daroing12@gmail.com>
2023-11-12 21:21:07 +0100

    feat(controllers/filter): add filterAchievementStagesByUrlParams

--- server/src/controllers/filters/achievementsFilter.ts
+++ server/src/controllers/filters/achievementsFilter.ts
@@ -1,4 +1,4 @@
-import { RequestSearch } from "@types";
+import { RequestAchievementStageQuery, RequestSearch } from "@types";
 
 export const filterAchievementsByUrlParams = (params: RequestSearch) => {
   const { search_name } = params;
@@ -8,6 +8,23 @@ export const filterAchievementsByUrlParams = (params: RequestSearch) => {
 
   const searchFilter = {
     $and: [filterName]
+  };
+  return searchFilter;
+};
+export const filterAchievementStagesByUrlParams = (params: RequestAchievementStageQuery) => {
+  const { search_name, sound, stageName } = params;
+  const filterName = {
+    ...(search_name && { name: { $regex: search_name, $options: "i" } })
+  };
+
+  const stageNameFilter = {
+    ...(stageName && { "stageData.name": { $regex: stageName, $options: "i" } })
+  };
+  const soundFilter = {
+    ...(sound && { "stageData.sound": { $regex: sound, $options: "i" } })
+  };
+  const searchFilter = {
+    $and: [filterName, stageNameFilter, soundFilter]
   };
   return searchFilter;
 };
