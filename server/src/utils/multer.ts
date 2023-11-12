e058e7e3e102dea3cf4bb9f9b4de72ffffd22f29
Darosss <daroing12@gmail.com>
2023-11-12 20:39:29 +0100

    feat(utils/multer): add filterImage

--- server/src/utils/multer.ts
+++ server/src/utils/multer.ts
@@ -7,3 +7,10 @@ export const filterMp3: multer.Options["fileFilter"] = (req, file, cb) => {
     cb(new AppError(400, "File isn't a mp3 extension"));
   }
 };
+
+export const filterImage: multer.Options["fileFilter"] = (req, file, cb) => {
+  if (file.mimetype.includes("image")) cb(null, true);
+  else {
+    cb(new AppError(400, "File isn't a image[.png, .jpeg, .jpg] extension"));
+  }
+};
