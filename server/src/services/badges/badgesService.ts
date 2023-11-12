7dc2b5a5248c41924a6333489fcca03098f89fba
Darosss <daroing12@gmail.com>
2023-11-12 21:07:49 +0100

    feat(services/badges): add basic crud services
    add getBadges, updateBadgeById, deleteBadgeById, deleteBadgeImage

--- server/src/services/badges/badgesService.ts
+++ server/src/services/badges/badgesService.ts
@@ -1,7 +1,28 @@
 import { Badge, BadgeDocument } from "@models";
 import { AppError, checkExistResource, handleAppError, logger } from "@utils";
-import { BadgeCreateData, BadgeFindOptions } from "./types";
-import { FilterQuery } from "mongoose";
+import { BadgeCreateData, BadgeFindOptions, BadgeUpdateData, ManyBadgesFindOptions } from "./types";
+import { FilterQuery, UpdateQuery } from "mongoose";
+import { getAchievementStages } from "@services";
+import { promises as fsPromises } from "fs";
+import path from "path";
+import { badgesPath } from "@configs";
+
+export const getBadges = async (filter: FilterQuery<BadgeDocument> = {}, affixFindOptions: ManyBadgesFindOptions) => {
+  const { limit = 50, skip = 1, sort = { createdAt: -1 }, select = { __v: 0 } } = affixFindOptions;
+
+  try {
+    const affix = await Badge.find(filter)
+      .limit(limit * 1)
+      .skip((skip - 1) * limit)
+      .select(select)
+      .sort(sort);
+
+    return affix;
+  } catch (err) {
+    logger.error(`Error occured while getting badges. ${err}`);
+    handleAppError(err);
+  }
+};
 
 export const getBadgesCount = async (filter: FilterQuery<BadgeDocument> = {}) => {
   return await Badge.countDocuments(filter);
@@ -32,6 +53,66 @@ export const getBadgeById = async (id: string, streamSessionFindOptions: BadgeFi
     return streamSession;
   } catch (err) {
     logger.error(`Error occured while getting stream session by id(${id}). ${err}`);
+    handleAppError(err);
+  }
+};
+
+export const updateBadgeById = async (id: string, updateData: UpdateQuery<BadgeUpdateData>) => {
+  try {
+    const updatedBadge = await Badge.findByIdAndUpdate(id, updateData, {
+      new: true
+    });
+
+    const badge = checkExistResource(updatedBadge, `Badge with id(${id})`);
+
+    return badge;
+  } catch (err) {
+    logger.error(`Error occured while editing badge by id(${id}). ${err}`);
+    handleAppError(err);
+  }
+};
+
+export const deleteBadgeById = async (id: string) => {
+  try {
+    const countBadgesInStages = (await getAchievementStages({ "stageData.badge": id }, {})) || [];
+
+    if (countBadgesInStages.length > 0) {
+      throw new AppError(
+        409,
+        `Badge with id(${id}) is used in stage(s): [${countBadgesInStages
+          .map(({ name }) => name)
+          .join(", ")}], cannot delete`
+      );
+    }
+
+    await Badge.findByIdAndDelete(id);
+
+    return { message: "Successfully deleted badge" };
+  } catch (err) {
+    logger.error(`Error occured while deleting badge by id(${id}). ${err}`);
+    handleAppError(err);
+  }
+};
+
+export const deleteBadgeImage = async (badgeName: string) => {
+  try {
+    const foundContainingBadges = (await getBadges({ imageUrl: { $regex: badgeName, $options: "i" } }, {})) || [];
+    if (foundContainingBadges.length > 0) {
+      throw new AppError(
+        409,
+        `Badge image with name: (${badgeName}) is used in badge(s): [${foundContainingBadges
+          .map(({ name }) => name)
+          .join(", ")}], cannot delete`
+      );
+    }
+
+    const filePath = path.join(badgesPath, badgeName);
+
+    await fsPromises.unlink(filePath);
+
+    return `Badge image ${badgeName} deleted successfully`;
+  } catch (err) {
+    logger.error(`Error occured while deleting badge image by name(${badgeName}). ${err}`);
     handleAppError(err);
   }
 };
