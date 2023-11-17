8293a79d4b6476a955b6170c98b14679844fe0b6
Darosss <daroing12@gmail.com>
2023-11-17 09:53:30 +0100

    refactor(getDateFromSecondsToYMDHMS): change to show only visible times + add suffixes

--- frontend/src/utils/getDateFromSecondsToYMDHMS.ts
+++ frontend/src/utils/getDateFromSecondsToYMDHMS.ts
@@ -1,26 +1,21 @@
 import moment from "moment";
 
-export const getDateFromSecondsToYMDHMS = (
-  time: number,
-  separatorYMD = " ",
-  separatorHMS = ":"
-) => {
+export const getDateFromSecondsToYMDHMS = (time: number, separator = " ") => {
   const duration = moment.duration(time, "seconds");
 
-  const seconds = Math.floor(duration.seconds()).toString().padStart(2, "0");
-  const minutes = Math.floor(duration.minutes()).toString().padStart(2, "0");
+  const seconds = Math.floor(duration.seconds());
+  const minutes = Math.floor(duration.minutes());
   const hours = Math.floor(duration.hours());
   const days = Math.floor(duration.days());
   const months = duration.months();
   const years = duration.years();
   const timeArray = [
-    years > 0 ? `${years.toFixed(0)}y${separatorYMD}` : "",
-    months > 0 ? `${months.toFixed(0)}m${separatorYMD}` : "",
-    days > 0 ? `${days.toFixed(0)}d${separatorYMD}` : "",
-    hours > 0 ? `${hours}${separatorHMS}` : "",
-    `${minutes}${separatorHMS}`,
-    seconds,
+    years > 0 ? `${years.toFixed(0)}y${separator}` : "",
+    months > 0 ? `${months.toFixed(0)}m${separator}` : "",
+    days > 0 ? `${days.toFixed(0)}d${separator}` : "",
+    hours > 0 ? `${hours}h${separator}` : "",
+    minutes > 0 ? `${minutes}m${separator}` : "",
+    seconds > 0 ? `${seconds}s` : "",
   ].filter(Boolean);
-
   return timeArray.join("");
 };
