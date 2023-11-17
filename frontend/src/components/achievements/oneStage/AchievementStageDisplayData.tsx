7bd4e237ca85d34cbba7d6eaca11bbd6ba310fa8
Darosss <daroing12@gmail.com>
2023-11-17 13:58:29 +0100

    feat(frontend/component/achievements): add EmulateStageButton
    add EmulateStageButton that handle emulate as points and emulate as time

--- frontend/src/components/achievements/oneStage/AchievementStageDisplayData.tsx
+++ frontend/src/components/achievements/oneStage/AchievementStageDisplayData.tsx
@@ -2,6 +2,8 @@ import { viteBackendUrl } from "src/configs/envVariables";
 import { useAchievementStageContext } from "./Context";
 import { TableDataWrapper } from "@components/tableWrapper";
 import { getDateFromSecondsToYMDHMS } from "@utils";
+import { useSocketContext } from "@socket";
+import { AchievementStageData } from "@services";
 
 export default function AchievementStageDisplayData() {
   const {
@@ -12,7 +14,14 @@ export default function AchievementStageDisplayData() {
     <>
       {state.stageData.map((data, index) => (
         <tr key={index} className="stage-data-content">
-          <td>{index + 1}</td>
+          <td className="stage-data-content-nr-emulate">
+            <div>
+              <div>
+                <EmulateStageButton stageData={data} />
+              </div>
+              <div>{index + 1}</div>
+            </div>
+          </td>
           <td>
             <TableDataWrapper>
               <div>Name</div>
@@ -58,6 +67,44 @@ export default function AchievementStageDisplayData() {
           </td>
         </tr>
       ))}
+    </>
+  );
+}
+
+interface EmulateStageButtonProps {
+  stageData: AchievementStageData;
+}
+function EmulateStageButton({ stageData }: EmulateStageButtonProps) {
+  const {
+    emits: { emulateAchievement },
+  } = useSocketContext();
+
+  const handleOnClickEmulate = (isTime = false) =>
+    emulateAchievement({
+      id: Math.random().toString(),
+      achievement: {
+        name: "Emited achievement",
+        isTime: isTime,
+      },
+      username: "Emited username",
+      stage: [stageData, new Date().getTime()],
+    });
+  return (
+    <>
+      <div>
+        <button
+          className="common-button tertiary-button"
+          onClick={() => handleOnClickEmulate()}
+        >
+          Emulate
+        </button>
+        <button
+          className="common-button tertiary-button"
+          onClick={() => handleOnClickEmulate(true)}
+        >
+          Emulate as time
+        </button>
+      </div>
     </>
   );
 }
