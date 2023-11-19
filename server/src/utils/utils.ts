2e7d52229d5d9b7ddbd34490a07a1469dd825c13
Darosss <daroing12@gmail.com>
2023-11-19 23:50:40 +0100

    feat(utils): add getDateFromSecondsToYMDHMS + generateRandomWord

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -43,3 +43,28 @@ export const isValidUrl = (urlString: string) => {
 export const getBaseLog = (x: number, y: number) => {
   return Math.log(y) / Math.log(x);
 };
+
+//NOTE: there is same function in /frontend folder
+export const getDateFromSecondsToYMDHMS = (time: number, separator = " ") => {
+  const duration = moment.duration(time, "seconds");
+
+  const seconds = Math.floor(duration.seconds());
+  const minutes = Math.floor(duration.minutes());
+  const hours = Math.floor(duration.hours());
+  const days = Math.floor(duration.days());
+  const months = duration.months();
+  const years = duration.years();
+  const timeArray = [
+    years > 0 ? `${years.toFixed(0)}y${separator}` : "",
+    months > 0 ? `${months.toFixed(0)}m${separator}` : "",
+    days > 0 ? `${days.toFixed(0)}d${separator}` : "",
+    hours > 0 ? `${hours}h${separator}` : "",
+    minutes > 0 ? `${minutes}m${separator}` : "",
+    seconds > 0 ? `${seconds}s` : ""
+  ].filter(Boolean);
+  return timeArray.join("");
+};
+
+export const generateRandomWord = () => {
+  return (Math.random() + 1).toString(36).substring(7);
+};
