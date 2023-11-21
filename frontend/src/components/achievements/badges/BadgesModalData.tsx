c8870436c6c2d3718c294234f971910882fe5597
Darosss <daroing12@gmail.com>
2023-11-21 21:01:32 +0100

    feat(BadgesModalData): update compoennt to work with new imagesUrls
    - destructure data from badgeImagesResponseData
    - add display all of sizes images as preview
    - add update each of sizes onClickBadge

--- frontend/src/components/achievements/badges/BadgesModalData.tsx
+++ frontend/src/components/achievements/badges/BadgesModalData.tsx
@@ -1,7 +1,8 @@
 import ModalDataWrapper from "@components/modalDataWrapper";
 import AvailableBadgeImages from "./AvailableBadgeImages";
 import { useBadgeContextEditCreateData } from "./ContextEditCreateData";
-import { useGetBadgesImages } from "@services";
+import { BadgeModelImagesUrls, useGetBadgesImages } from "@services";
+import { viteBackendUrl } from "src/configs/envVariables";
 
 export default function BadgeModalData() {
   const {
@@ -13,10 +14,12 @@ export default function BadgeModalData() {
     error,
     refetchData,
   } = useGetBadgesImages();
+
   if (loading) return <>Loading</>;
   if (error) return <>There is an error. {error.response?.data.message}</>;
   if (!badgeImagesResponseData) return null;
 
+  const { data } = badgeImagesResponseData;
   return (
     <ModalDataWrapper>
       <div>Name</div>
@@ -39,20 +42,49 @@ export default function BadgeModalData() {
           }
         />
       </div>
-      <div>Image </div>
+      <div className="badges-modal-data-images">
+        {Object.keys(state.imagesUrls).map((key, index) => {
+          const keyAsKeyOfBadgeImagesUrlsState =
+            key as keyof BadgeModelImagesUrls;
+          return (
+            <div key={index}>
+              {key}
+              {state.imagesUrls[keyAsKeyOfBadgeImagesUrlsState] ? (
+                <img
+                  src={`${viteBackendUrl}\\${state.imagesUrls[keyAsKeyOfBadgeImagesUrlsState]}`}
+                  alt={key}
+                />
+              ) : null}
+            </div>
+          );
+        })}
+      </div>
 
       <div>
         <AvailableBadgeImages
           className="available-badge-images-modal-edit"
           onClickRefresh={refetchData}
-          badgePaths={badgeImagesResponseData.data}
-          onClickBadge={({ basePath, badgeName }) =>
+          badgesData={data}
+          onClickBadge={({ basePath, badgeName, badgeExtension }) => {
+            const updatePayloadData: Partial<BadgeModelImagesUrls> = {};
+            Object.keys(state.imagesUrls).forEach((key) => {
+              const foundSize = data.availableSizes.find((size) =>
+                key.includes(String(size))
+              );
+              updatePayloadData[
+                key as keyof BadgeModelImagesUrls
+              ] = `${basePath}\\${badgeName}${data.separatorSizes}${foundSize}${badgeExtension}`;
+            });
+
             dispatch({
-              type: "SET_IMAGE_URL",
-              payload: `${basePath}\\${badgeName}`,
-            })
-          }
-          currentImgPath={state.imageUrl}
+              type: "SET_IMAGES_URLS",
+              payload: {
+                ...state.imagesUrls,
+                ...updatePayloadData,
+              },
+            });
+          }}
+          currentImgPath={state.imagesUrls.x128}
         />
       </div>
     </ModalDataWrapper>
