5a3b6899239433ee9b2e912525d50c7c12e0c765
Darosss <daroing12@gmail.com>
2023-11-27 23:51:39 +0100

    feat(defaults/achievements): add new default achievements
    - add new default achievements connected with: https://github.com/Darosss/TwitchChatBot/commit/1282a973e13377141cda8958d89a07b6c1df63a4

--- server/src/defaults/achievements/achievements.ts
+++ server/src/defaults/achievements/achievements.ts
@@ -30,6 +30,90 @@ export const getDefaultAchievementsData = (stagesId: string, tagId: string): Ach
       isTime: true
     },
     {
+      name: ACHIEVEMENTS.BOUGHT_SUBSCRIBTIONS,
+      description: "Achievements for bought subscriptions for channel",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SUBSCRIBTIONS_TIER_1,
+      description: "Achievements for subscribes channel with at least tier 1 (tiers 2 and 3 are counted)",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SUBSCRIBTIONS_TIER_2,
+      description: "Achievements for subscribes channel with at least tier 2(tiers 3 are counted)",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SUBSCRIBTIONS_TIER_3,
+      description: "Achievements for subscribes channel with tier 3",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SENT_SUBSCRIBTIONS_GIFTS_CUMULATIVE,
+      description: "Achievements for gifting a subscribes to other users cumulative(counting all tier 1, 2 and 3)",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SENT_SUBSCRIBTIONS_GIFTS_TIER_1,
+      description: "Achievements for gifting a subscribes to other users cumulative(only for tier 1 subs)",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SENT_SUBSCRIBTIONS_GIFTS_TIER_2,
+      description: "Achievements for gifting a subscribes to other users cumulative(only for tier 2 subs)",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SENT_SUBSCRIBTIONS_GIFTS_TIER_3,
+      description: "Achievements for gifting a subscribes to other users cumulative(only for tier 3 subs)",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
+      name: ACHIEVEMENTS.SENT_SUBSCRIBTIONS_GIFTS_AS_PACK_TIER_1,
+      description:
+        "Achievements for gifting a subscribes choosen amounts at once to other users fe. 20 at once (only for tier 1 subs, max 100)",
+      stages: stagesId,
+      tag: tagId
+    },
+    {
+      name: ACHIEVEMENTS.SENT_SUBSCRIBTIONS_GIFTS_AS_PACK_TIER_2,
+      description:
+        "Achievements for gifting a subscribes choosen amounts at once to other users fe. 20 at once (only for tier 2 subs, max 100)",
+      stages: stagesId,
+      tag: tagId
+    },
+    {
+      name: ACHIEVEMENTS.SENT_SUBSCRIBTIONS_GIFTS_AS_PACK_TIER_3,
+      description:
+        "Achievements for gifting a subscribes choosen amounts at once to other users fe. 20 at once (only for tier 3 subs, max 40)",
+      stages: stagesId,
+      tag: tagId
+    },
+    {
+      name: ACHIEVEMENTS.RECEIVED_SUBSCRIBTIONS_GIFTS,
+      description: "Achievements for getting a subscribes gifts from other users",
+      stages: stagesId,
+      tag: tagId,
+      showProgress: true
+    },
+    {
       name: ACHIEVEMENTS.COMMANDS_COUNT,
       description: "Achievements for use of command",
       stages: stagesId,
