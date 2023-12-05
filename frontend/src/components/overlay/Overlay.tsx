3bc331243e3d0acc23ecdc2911c8807cdbe98c3b
Darosss <daroing12@gmail.com>
2023-12-05 18:41:11 +0100

    feat(components/overlay): add usage of Context, css editor, socket
    - replace useStates with data from new created context from https://github.com/Darosss/TwitchChatBot/commit/64cb1951421360cc50fed520162367bbad25e378
    - add on socket event refreshOverlayLayoutEvent fetchRefreshData
    - add StyleCSSEditor to render when editor === true
    - add emit refreshOverlayLayout onEdit props of ReactGrid

--- frontend/src/components/overlay/Overlay.tsx
+++ frontend/src/components/overlay/Overlay.tsx
@@ -1,25 +1,20 @@
 import React, { useEffect, useState } from "react";
-import ReactGridLayout from "react-grid-layout";
-
 import Redemptions from "./redemptions";
-import {
-  initialLayoutOverlays,
-  initialToolboxOverlays,
-  overlaysKeys,
-} from "src/layout/initialLayoutOverlays";
-import { useParams } from "react-router-dom";
+import { overlaysKeys } from "src/layout/initialLayoutOverlays";
 import { getInitialCurrentBreakpoint } from "@utils";
 import ReactGrid from "@components/reactGrid";
-import { useEditOverlayById, useGetOverlayById } from "@services";
 import MusicPlayer from "./musicPlayer";
 import YoutubePlayerVideo from "./youtubePlayerVideo";
 import { HelmetTitle } from "@components/componentWithTitle";
 import Achievements from "./achievements";
 import Chat from "./chat";
 import PreviewImageSelector from "./PreviewImageSelector";
+import { useOverlayDataContext } from "./OverlayDataContext";
+import StyleCSSEditor from "./StyleCSSEditor";
+import { useSocketContext } from "@socket";
 
 const components = new Map([
-  [overlaysKeys.overlayMusicPlayer, Redemptions],
+  [overlaysKeys.overlayRedemptions, Redemptions],
   [overlaysKeys.overlayMusicPlayer, MusicPlayer],
   [overlaysKeys.overlayYoutubeMusicPlayer, YoutubePlayerVideo],
   [overlaysKeys.overlayAchievements, Achievements],
@@ -28,47 +23,60 @@ const components = new Map([
 
 export default function Overlay(params: { editor?: boolean }) {
   const { editor = false } = params;
-  const { overlayId } = useParams();
-  const [layoutOverlay, setLayoutOverlay] = useState<ReactGridLayout.Layouts>(
-    initialLayoutOverlays
-  );
-  const [toolbox, setToolbox] = useState<ReactGridLayout.Layouts>(
-    initialToolboxOverlays
-  );
+  const {
+    emits: { refreshOverlayLayout },
+    events: { refreshOverlayLayout: refreshOverlayLayoutEvent },
+  } = useSocketContext();
+  const {
+    layoutState,
+    toolboxState,
+    baseData,
+    fetchEditOverlay,
+    fetchRefreshData,
+    isEditorState: [, setIsEditor],
+  } = useOverlayDataContext();
+
   const [currentBreakpoint, setCurrentBreakpoint] = useState(
     getInitialCurrentBreakpoint()
   );
 
-  const { data, error } = useGetOverlayById(overlayId || "");
-  const { refetchData: fetchEditOverlay } = useEditOverlayById(
-    overlayId || "",
-    {
-      layout: layoutOverlay,
-      toolbox: toolbox,
-    }
-  );
+  useEffect(() => {
+    setIsEditor(editor);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editor]);
 
   useEffect(() => {
-    if (!data) return;
-    const { data: layoutData } = data;
+    refreshOverlayLayoutEvent.on((id) => {
+      if (baseData._id !== id) return;
+      fetchRefreshData();
+    });
 
-    setLayoutOverlay(layoutData.layout);
-    setToolbox(layoutData.toolbox);
-  }, [data]);
-  if (error) return <>There is an error. {error.response?.data.message}</>;
+    return () => {
+      refreshOverlayLayoutEvent.off();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [refreshOverlayLayoutEvent, baseData]);
+
   return (
     <div>
-      <HelmetTitle title={"Overlay " + data?.data.name || "Overlay"} />
-      {editor ? <PreviewImageSelector /> : null}
+      <HelmetTitle title={"Overlay " + baseData.name} />
+      {editor ? (
+        <>
+          <StyleCSSEditor />
+          <PreviewImageSelector />
+        </>
+      ) : null}
       <ReactGrid
-        layoutName={data?.data.name || ""}
-        layoutState={[layoutOverlay, setLayoutOverlay]}
-        toolboxState={[toolbox, setToolbox]}
+        layoutName={baseData.name}
+        layoutState={layoutState}
+        toolboxState={toolboxState}
         currentBreakpointState={[currentBreakpoint, setCurrentBreakpoint]}
         componentsMap={components}
-        onEdit={fetchEditOverlay}
+        onEdit={() =>
+          fetchEditOverlay().then(() => refreshOverlayLayout(baseData._id))
+        }
         showDrawer={editor}
-      ></ReactGrid>
+      />
     </div>
   );
 }
