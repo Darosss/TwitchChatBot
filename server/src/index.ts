9164f6061669d399dfa7fca817747a427d27c097
Darosss <daroing12@gmail.com>
2023-12-08 11:39:24 +0100

    feat: add usage of newSocket and init from initializeHandlers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,19 @@
 import "module-alias/register";
+import "./discord";
 import { initMongoDataBase, backendPort } from "@configs";
-
 import expressApp from "./app";
 import { logger } from "@utils";
-import "./discord";
+import { newSocket } from "./socketIO";
+import init from "./stream/initializeHandlers";
 
 const startServer = async () => {
   await initMongoDataBase();
 
   const server = expressApp();
+
+  const socketIO = newSocket(server);
+
+  await init(socketIO);
 
   server.listen(backendPort, async () => {
     console.log("listening on *:", backendPort);
