dfc1d2aaba3ffc1512164fa79875fb540d7acd9d
Darosss <daroing12@gmail.com>
2023-12-13 11:44:44 +0100

    feat(discord/commands): add needed administrator permission for ping command

--- server/src/discord/commands/ping.ts
+++ server/src/discord/commands/ping.ts
@@ -1,7 +1,10 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import { CommandData } from "./types";
 
 export const ping: CommandData = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   execute: async (interaction: CommandInteraction) => interaction.reply("Pong!")
 };
