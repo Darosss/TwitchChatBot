8085f9d68edc9edb46b8fcaefb9a4e8153da2ff6
Darosss <daroing12@gmail.com>
2023-12-18 17:33:12 +0100

    feat(server): add discord invite link route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { afterTwitchAuthorization, getTwitchAuthorizeUrl } from "@controllers";
+import { afterTwitchAuthorization, getDiscordInviteUrl, getTwitchAuthorizeUrl } from "@controllers";
 import { authorizationTwitch } from "@middlewares";
 
 const auth = Router();
 
 auth.get("/twitch/callback", authorizationTwitch, afterTwitchAuthorization);
 auth.get("/authorize-url", getTwitchAuthorizeUrl);
+auth.get("/discord/invite", getDiscordInviteUrl);
 
 export default auth;
