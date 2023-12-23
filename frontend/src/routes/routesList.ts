415df73e2a7f4aeef342194566df5cf3fb9be1e9
Darosss <daroing12@gmail.com>
2023-12-23 18:58:25 +0100

    feat(routes): add route types
    - add description to existing routes

--- frontend/src/routes/routesList.ts
+++ frontend/src/routes/routesList.ts
@@ -1,17 +1,95 @@
-export const routes = [
-  { path: "/", label: "Home" },
-  { path: "/achievements", label: "Achievements" },
-  { path: "/overlay", label: "Overlay" },
-  { path: "/messages", label: "Messages" },
-  { path: "/message-categories", label: "Message categories" },
-  { path: "/users", label: "Users" },
-  { path: "/events", label: "Events" },
-  { path: "/modes", label: "Modes" },
-  { path: "/stream-sessions", label: "Sessions" },
-  { path: "/songs", label: "Songs" },
-  { path: "/redemptions", label: "Redemptions" },
-  { path: "/commands", label: "Commands" },
-  { path: "/triggers", label: "Triggers" },
-  { path: "/timers", label: "Timers" },
-  { path: "/configs", label: "Configs" },
+type RoutesType = {
+  path: string;
+  label: string;
+  description: string;
+};
+
+export const routes: RoutesType[] = [
+  { path: "/", label: "Home", description: "Home site" },
+  {
+    path: "/achievements",
+    label: "Achievements",
+    description: `A place where you can modify, edit, create achievements. 
+These are obtainable moslty by watching stream, chatting on stream and more`,
+  },
+  {
+    path: "/overlay",
+    label: "Overlay",
+    description: `A place where you can create and modify your own overlays.
+Choose from: chat, rewards, music player, achievements box and more`,
+  },
+  {
+    path: "/messages",
+    label: "Messages",
+    description: "All messages from your stream sessions.",
+  },
+  {
+    path: "/message-categories",
+    label: "Message categories",
+    description:
+      "Message categories - which can be send by bot depends on % set in configs. ",
+  },
+  {
+    path: "/users",
+    label: "Users",
+    description: "Check all users that visited or were on your stream",
+  },
+  {
+    path: "/events",
+    label: "Events",
+    description: `Events window - a virtual stream deck where you can: 
+- manage played music
+- turn on/off modes(affixes, moods, tags)
+- add custom alert sounds
+- check last chatters
+- send messages as bot
+- check stream evenets like (who join channel, who left)
+- and more...`,
+  },
+  {
+    path: "/modes",
+    label: "Modes",
+    description: `Manage default or created by your own affixes, tags, moods`,
+  },
+  {
+    path: "/stream-sessions",
+    label: "Sessions",
+    description:
+      "Here you can check stream session that you already finished or are currently active",
+  },
+  {
+    path: "/songs",
+    label: "Songs",
+    description: "A whole songs collected by playing through music yt player",
+  },
+  {
+    path: "/redemptions",
+    label: "Redemptions",
+    description: "Contains all redemptions from twitch channel points",
+  },
+  {
+    path: "/commands",
+    label: "Commands",
+    description: "Modify, create, delete your own chat commands",
+  },
+  {
+    path: "/triggers",
+    label: "Triggers",
+    description: "Modify, create, delete your own chat triggers",
+  },
+  {
+    path: "/timers",
+    label: "Timers",
+    description: "Modify, create, delete your own chat timers",
+  },
+  {
+    path: "/configs",
+    label: "Configs",
+    description: `Change bot configs fe.:
+- checking interval of modes
+- permission levels
+- mode suffix, prefix chances
+- song request
+- and more`,
+  },
 ];
