9c7080c05f3b3256d4135876f0dd8779bf1c8ae0
darosss <darosss12@proton.me>
2025-04-16 11:54:33 +0200

    feat(server): improve globalPaths: - improve paths: add rootFolder + musicFolderName + youtube music path + add usage of them in rest

--- server/src/configs/globalPaths.ts
+++ server/src/configs/globalPaths.ts
@@ -1,17 +1,25 @@
 import path from "path";
 import { ensureDirectoryExists } from "@utils";
 
+export const musicFolderName = "music";
+export const rootFolder = path.resolve(__dirname, "../");
 export const envFilePath = path.resolve(__dirname, "../../../.env");
 
-export const publicPath = path.resolve(__dirname, "../public");
+export const publicEndpointName = "public";
+export const publicEndpointPath = path.join(rootFolder, publicEndpointName);
+export const achievementsPath = path.join(publicEndpointPath, "achievements");
 
-export const musicPath = path.join(publicPath, "music");
-export const alertSoundsPath = path.join(publicPath, "alertSounds");
-export const achievementsPath = path.join(publicPath, "achievements");
 export const badgesPath = path.join(achievementsPath, "badges");
 export const achievementsStagesSoundsPath = path.join(achievementsPath, "stagesSounds");
 
+export const musicPath = path.join(publicEndpointPath, musicFolderName);
+export const alertSoundsFolderName = "alertSounds";
+export const alertSoundsPath = path.join(publicEndpointPath, alertSoundsFolderName);
+export const ytMusicFolderName = "youtube";
+export const ytMusicPath = path.join(musicPath, ytMusicFolderName);
+
 ensureDirectoryExists(musicPath);
+ensureDirectoryExists(ytMusicPath);
 ensureDirectoryExists(alertSoundsPath);
 ensureDirectoryExists(achievementsPath);
 ensureDirectoryExists(badgesPath);
